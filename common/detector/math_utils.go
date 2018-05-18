19628c35cf5ac55521c4492ccbb2573d2c58b28e
makki_d <d.makiuchi@gmail.com>
2018-05-18 10:32:40 +0900

    gofmt

--- common/detector/math_utils.go
+++ common/detector/math_utils.go
@@ -8,20 +8,19 @@ func MathUtils_Round(d float64) int {
 	if d < 0.0 {
 		return int(d - 0.5)
 	}
-
 	return int(d + 0.5)
 }
 
 func MathUtils_DistanceFloat(aX, aY, bX, bY float64) float64 {
 	xDiff := aX - bX
 	yDiff := aY - bY
-	return math.Sqrt(xDiff * xDiff + yDiff * yDiff)
+	return math.Sqrt(xDiff*xDiff + yDiff*yDiff)
 }
 
 func MathUtils_DistanceInt(aX, aY, bX, bY int) float64 {
 	xDiff := aX - bX
 	yDiff := aY - bY
-	return math.Sqrt(float64(xDiff * xDiff + yDiff * yDiff))
+	return math.Sqrt(float64(xDiff*xDiff + yDiff*yDiff))
 }
 
 func MathUtils_Sum(arr []int) int {
