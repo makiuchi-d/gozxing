0570545fbe1cbf72b829b85fb07a505ff147cbdc
makki_d <d.makiuchi@gmail.com>
2018-05-19 14:40:58 +0900

    implement BitMatrix

---
+++ common/bit_matrix.go
@@ -0,0 +1,350 @@
+package common
+
+import (
+	"errors"
+)
+
+type BitMatrix struct {
+	width   int
+	height  int
+	rowSize int
+	bits    []uint32
+}
+
+func NewSquareBitMatrix(dimension int) (*BitMatrix, error) {
+	return NewBitMatrix(dimension, dimension)
+}
+
+func NewBitMatrix(width, height int) (*BitMatrix, error) {
+	if width < 1 || height < 1 {
+		return nil, errors.New("IllegalArgumentException: Both dimensions must be greater than 0")
+	}
+	rowSize := (width + 31) / 32
+	bits := make([]uint32, rowSize*height)
+	return &BitMatrix{width, height, rowSize, bits}, nil
+}
+
+func ParseImageToBitMatrix(image [][]bool) (*BitMatrix, error) {
+	var width, height int
+	height = len(image)
+	if height > 0 {
+		width = len(image[0])
+	}
+	bits, e := NewBitMatrix(width, height)
+	if e != nil {
+		return nil, e
+	}
+	for i := 0; i < height; i++ {
+		imageI := image[i]
+		for j := 0; j < width; j++ {
+			if imageI[j] {
+				bits.Set(j, i)
+			}
+		}
+	}
+	return bits, nil
+}
+
+func ParseStringToBitMatrix(stringRepresentation, setString, unsetString string) (*BitMatrix, error) {
+	if stringRepresentation == "" {
+		return nil, errors.New("IllegalArgumentException")
+	}
+
+	bits := make([]bool, len(stringRepresentation))
+	bitsPos := 0
+	rowStartPos := 0
+	rowLength := -1
+	nRows := 0
+	pos := 0
+	for pos < len(stringRepresentation) {
+		if c := stringRepresentation[pos]; c == '\n' || c == '\r' {
+			if bitsPos > rowStartPos {
+				if rowLength == -1 {
+					rowLength = bitsPos - rowStartPos
+				} else if bitsPos-rowStartPos != rowLength {
+					return nil, errors.New("IllegalArgumentException: row length do not match")
+				}
+				rowStartPos = bitsPos
+				nRows++
+			}
+			pos++
+		} else if stringRepresentation[pos:pos+len(setString)] == setString {
+			pos += len(setString)
+			bits[bitsPos] = true
+			bitsPos++
+		} else if stringRepresentation[pos:pos+len(unsetString)] == unsetString {
+			pos += len(unsetString)
+			bits[bitsPos] = false
+			bitsPos++
+		} else {
+			return nil, errors.New(
+				"IllegalArgumentException: illegal character encountered: " + stringRepresentation[pos:])
+		}
+	}
+
+	if bitsPos > rowStartPos {
+		if rowLength == -1 {
+			rowLength = bitsPos - rowStartPos
+		} else if bitsPos-rowStartPos != rowLength {
+			return nil, errors.New("IllegalArgumentException: row length do not match")
+		}
+		nRows++
+	}
+	matrix, e := NewBitMatrix(rowLength, nRows)
+	if e != nil {
+		return nil, e
+	}
+	for i := 0; i < bitsPos; i++ {
+		if bits[i] {
+			matrix.Set(i%rowLength, i/rowLength)
+		}
+	}
+	return matrix, nil
+}
+
+func (b *BitMatrix) Get(x, y int) bool {
+	offset := (y * b.rowSize) + (x / 32)
+	return ((b.bits[offset] >> uint(x%32)) & 1) != 0
+}
+
+func (b *BitMatrix) Set(x, y int) {
+	offset := (y * b.rowSize) + (x / 32)
+	b.bits[offset] |= 1 << uint(x%32)
+}
+
+func (b *BitMatrix) Unset(x, y int) {
+	offset := (y * b.rowSize) + (x / 32)
+	b.bits[offset] &= ^(1 << uint(x%32))
+}
+
+func (b *BitMatrix) Flip(x, y int) {
+	offset := (y * b.rowSize) + (x / 32)
+	b.bits[offset] ^= 1 << uint(x%32)
+}
+
+func (b *BitMatrix) Xor(mask *BitMatrix) error {
+	if b.width != mask.GetWidth() || b.height != mask.GetHeight() ||
+		b.rowSize != mask.GetRowSize() {
+		return errors.New("IllegalArgumentException: input matrix dimensions do not match")
+	}
+	/*
+		rowArray := NewBitArray(b.width/32 + 1)
+		for y := 0; y < b.height; y++ {
+			offset := y * b.rowSize
+			row := mask.GetRow(y, rowArray).GetBitArray()
+			for x := 0; x < b.rowSize; x++ {
+				b.bits[offset+x] ^= row[x]
+			}
+		}
+	*/
+	for y := 0; y < b.height; y++ {
+		bOffset := y * b.rowSize
+		mOffset := y * mask.rowSize
+		for x := 0; x < b.rowSize; x++ {
+			b.bits[bOffset+x] ^= mask.bits[mOffset+x]
+		}
+	}
+
+	return nil
+}
+
+func (b *BitMatrix) Clear() {
+	max := len(b.bits)
+	for i := 0; i < max; i++ {
+		b.bits[i] = 0
+	}
+}
+
+func (b *BitMatrix) SetRegion(left, top, width, height int) error {
+	if top < 0 || left < 0 {
+		return errors.New("IllegalArgumentException: Left and top must be nonnegative")
+	}
+	if height < 1 || width < 1 {
+		return errors.New("IllegalArgumentException: Height and width must be at least 1")
+	}
+	right := left + width
+	bottom := top + height
+	if bottom > b.height || right > b.width {
+		return errors.New("IllegalArgumentException: The region must fit inside the matrix")
+	}
+	for y := top; y < bottom; y++ {
+		offset := y * b.rowSize
+		for x := left; x < right; x++ {
+			b.bits[offset+(x/32)] |= 1 << uint(x%32)
+		}
+	}
+	return nil
+}
+
+func (b *BitMatrix) GetRow(y int, row *BitArray) *BitArray {
+	if row == nil || row.GetSize() < b.width {
+		row = NewBitArray(b.width)
+	} else {
+		row.Clear()
+	}
+	offset := y * b.rowSize
+	for x := 0; x < b.rowSize; x++ {
+		row.SetBulk(x*32, b.bits[offset+x])
+	}
+	return row
+}
+
+func (b *BitMatrix) SetRow(y int, row *BitArray) {
+	offset := y * b.rowSize
+	copy(b.bits[offset:offset+b.rowSize], row.bits)
+}
+
+func (b *BitMatrix) Rotate180() {
+	width := b.GetWidth()
+	height := b.GetHeight()
+	topRow := NewBitArray(width)
+	bottomRow := NewBitArray(width)
+	for i := 0; i < (height+1)/2; i++ {
+		topRow = b.GetRow(i, topRow)
+		bottomRow = b.GetRow(height-1-i, bottomRow)
+		topRow.Reverse()
+		bottomRow.Reverse()
+		b.SetRow(i, bottomRow)
+		b.SetRow(height-1-i, topRow)
+	}
+}
+
+func (b *BitMatrix) GetEnclosingRectangle() []int {
+	left := b.width
+	top := b.height
+	right := -1
+	bottom := -1
+
+	for y := 0; y < b.height; y++ {
+		for x32 := 0; x32 < b.rowSize; x32++ {
+			theBits := b.bits[y*b.rowSize+x32]
+			if theBits != 0 {
+				if y < top {
+					top = y
+				}
+				if y > bottom {
+					bottom = y
+				}
+				if x32*32 < left {
+					bit := 0
+					for (theBits << uint(31-bit)) == 0 {
+						bit++
+					}
+					if (x32*32 + bit) < left {
+						left = x32*32 + bit
+					}
+				}
+				if x32*32+31 > right {
+					bit := 31
+					for (theBits >> uint(bit)) == 0 {
+						bit--
+					}
+					if (x32*32 + bit) > right {
+						right = x32*32 + bit
+					}
+				}
+			}
+		}
+	}
+
+	if right < left || bottom < top {
+		return nil
+	}
+
+	return []int{left, top, right - left + 1, bottom - top + 1}
+}
+
+func (b *BitMatrix) GetTopLeftOnBit() []int {
+	bitsOffset := 0
+	for bitsOffset < len(b.bits) && b.bits[bitsOffset] == 0 {
+		bitsOffset++
+	}
+	if bitsOffset == len(b.bits) {
+		return nil
+	}
+	y := bitsOffset / b.rowSize
+	x := (bitsOffset % b.rowSize) * 32
+
+	theBits := b.bits[bitsOffset]
+	bit := uint(0)
+	for (theBits << (31 - bit)) == 0 {
+		bit++
+	}
+	x += int(bit)
+	return []int{x, y}
+}
+
+func (b *BitMatrix) GetBottomRightOnBit() []int {
+	bitsOffset := len(b.bits) - 1
+	for bitsOffset >= 0 && b.bits[bitsOffset] == 0 {
+		bitsOffset--
+	}
+	if bitsOffset < 0 {
+		return nil
+	}
+
+	y := bitsOffset / b.rowSize
+	x := (bitsOffset % b.rowSize) * 32
+
+	theBits := b.bits[bitsOffset]
+	bit := uint(31)
+	for (theBits >> bit) == 0 {
+		bit--
+	}
+	x += int(bit)
+
+	return []int{x, y}
+}
+
+func (b *BitMatrix) GetWidth() int {
+	return b.width
+}
+
+func (b *BitMatrix) GetHeight() int {
+	return b.height
+}
+
+func (b *BitMatrix) GetRowSize() int {
+	return b.rowSize
+}
+
+//  public boolean equals(Object o)
+//  public int hashCode()
+
+func (b *BitMatrix) String() string {
+	return b.ToString("X ", "  ")
+}
+
+func (b *BitMatrix) ToString(setString, unsetString string) string {
+	return b.ToStringWithLineSeparator(setString, unsetString, "\n")
+}
+
+func (b *BitMatrix) ToStringWithLineSeparator(setString, unsetString, lineSeparator string) string {
+	setBytes := []byte(setString)
+	unsetBytes := []byte(unsetString)
+	lineSepBytes := []byte(lineSeparator)
+
+	lineSize := len(lineSeparator)
+	if len(setString) > len(unsetString) {
+		lineSize += b.width * len(setString)
+	} else {
+		lineSize += b.width * len(unsetString)
+	}
+	result := make([]byte, 0, b.height*lineSize)
+
+	for y := 0; y < b.height; y++ {
+		for x := 0; x < b.width; x++ {
+			var s []byte
+			if b.Get(x, y) {
+				s = setBytes
+			} else {
+				s = unsetBytes
+			}
+			result = append(result, s...)
+		}
+		result = append(result, lineSepBytes...)
+	}
+	return string(result)
+}
+
+//  public BitMatrix clone()
---
+++ common/bit_matrix_test.go
@@ -0,0 +1,382 @@
+package common
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewSquareBitMatrix(t *testing.T) {
+	b, e := NewSquareBitMatrix(3)
+
+	if e != nil {
+		t.Fatalf("NewSquareBitMatrix returns error: %v", e)
+	}
+	if b.width != 3 || b.height != 3 {
+		t.Fatalf("width/height = (%d,%d), expect (3,3)", b.width, b.height)
+	}
+}
+
+func TestNewBitMatrix(t *testing.T) {
+	if _, e := NewBitMatrix(0, 3); e == nil {
+		t.Fatalf("NewBitMatrix(0, 3) must be error")
+	}
+	if _, e := NewBitMatrix(3, -1); e == nil {
+		t.Fatalf("NewBitMatrix(3, -1) must be error")
+	}
+
+	b, e := NewBitMatrix(33, 3)
+	if e != nil {
+		t.Fatalf("NewBitMatrix returns error: %v", e)
+	}
+	if b.width != 33 || b.height != 3 {
+		t.Fatalf("width/height = (%d,%d), expect (33,3)", b.width, b.height)
+	}
+	if b.rowSize < b.width/32 {
+		t.Fatalf("b.rowSize too small, rowSize=%v (width=%v)", b.rowSize, b.width)
+	}
+	if len(b.bits) < b.rowSize*b.height {
+		t.Fatalf("b.bits too small, len(b.bits)=%v", len(b.bits))
+	}
+}
+
+func testBitMatrixGet(t *testing.T, b *BitMatrix, x, y int, expect bool) {
+	if r := b.Get(x, y); r != expect {
+		t.Fatalf("[%v,%v] = %v, expect %v", x, y, r, expect)
+	}
+}
+
+func TestParseImageToBitMatrix(t *testing.T) {
+	if _, e := ParseImageToBitMatrix([][]bool{}); e == nil {
+		t.Fatalf("Parse from empty image must be error")
+	}
+
+	image := [][]bool{
+		[]bool{true, true, false},
+		[]bool{false, true, false},
+	}
+	b, e := ParseImageToBitMatrix(image)
+
+	if e != nil {
+		t.Fatalf("ParseImageToBitMatrix returns error, %v", e)
+	}
+	if b.width != 3 || b.height != 2 {
+		t.Fatalf("BitMatrix size is (%v,%v), expect (3,2)", b.width, b.height)
+	}
+	testBitMatrixGet(t, b, 2, 0, false)
+	testBitMatrixGet(t, b, 1, 1, true)
+}
+
+func TestParseStringToBitMatrix(t *testing.T) {
+	if _, e := ParseStringToBitMatrix("", "", ""); e == nil {
+		t.Fatalf("Parse from empty string must be error")
+	}
+	if _, e := ParseStringToBitMatrix("\n\n", "X", "."); e == nil {
+		t.Fatalf("Parse from no width string must be error")
+	}
+	if _, e := ParseStringToBitMatrix("XXX\n..\n", "X", "."); e == nil {
+		t.Fatalf("Parse from different width string must be error")
+	}
+	if _, e := ParseStringToBitMatrix("XY\nZZ\n", "X", "."); e == nil {
+		t.Fatalf("Parse from string containing unknown chars must be error")
+	}
+	if _, e := ParseStringToBitMatrix("XX\n.", "X", "."); e == nil {
+		t.Fatalf("Parse from incomplete string must be error")
+	}
+
+	var b *BitMatrix
+	var e error
+	b, e = ParseStringToBitMatrix("XX...", "XX", "...")
+	if e != nil {
+		t.Fatalf("ParseStringToBitMatrix returns error: %v", e)
+	}
+	if b.width != 2 || b.height != 1 {
+		t.Fatalf("BitMatrix size = (%v,%v), expect (2,1)", b.width, b.height)
+	}
+
+	b, e = ParseStringToBitMatrix("XX.\n.X.\n", "X", ".")
+	if e != nil {
+		t.Fatalf("ParseStringToBitMatrix returns error: %v", e)
+	}
+	if b.width != 3 || b.height != 2 {
+		t.Fatalf("BitMatrix size = (%v,%v), expect (3,2)", b.width, b.height)
+	}
+	if b.Get(1, 1) != true {
+		t.Fatalf("[1,1] is not true")
+	}
+	if b.Get(2, 1) != false {
+		t.Fatalf("[0, 2] is not false")
+	}
+}
+
+func TestBitMatrix_GetSetFlip(t *testing.T) {
+	b, _ := NewBitMatrix(7, 7)
+
+	testBitMatrixGet(t, b, 1, 1, false)
+	b.Set(1, 1)
+	testBitMatrixGet(t, b, 1, 1, true)
+	testBitMatrixGet(t, b, 1, 0, false)
+
+	b.Unset(1, 1)
+	testBitMatrixGet(t, b, 1, 1, false)
+
+	b.Flip(6, 6)
+	testBitMatrixGet(t, b, 6, 6, true)
+	b.Flip(6, 6)
+	testBitMatrixGet(t, b, 6, 6, false)
+}
+
+func TestBitMatrix_Xor(t *testing.T) {
+	var b, m *BitMatrix
+
+	b, _ = NewBitMatrix(1, 1)
+	m, _ = NewBitMatrix(2, 1)
+	if e := b.Xor(m); e == nil {
+		t.Fatalf("dimension missmatch xor must be error")
+	}
+	m, _ = NewBitMatrix(1, 2)
+	if e := b.Xor(m); e == nil {
+		t.Fatalf("dimension missmatch xor must be error")
+	}
+	m = &BitMatrix{1, 1, 2, nil}
+	if e := b.Xor(m); e == nil {
+		t.Fatalf("rowSize missmatch xor must be error")
+	}
+
+	// XX.       X.X     .XX
+	// ..X  xor  .XX  =  .X.
+	b, _ = ParseStringToBitMatrix("XX.\n..X", "X", ".")
+	m, _ = ParseStringToBitMatrix("X.X\n.XX", "X", ".")
+
+	e := b.Xor(m)
+	if e != nil {
+		t.Fatalf("Xor returns error: %v", e)
+	}
+	testBitMatrixGet(t, b, 0, 0, false)
+	testBitMatrixGet(t, b, 1, 0, true)
+	testBitMatrixGet(t, b, 2, 0, true)
+	testBitMatrixGet(t, b, 0, 1, false)
+	testBitMatrixGet(t, b, 1, 1, true)
+	testBitMatrixGet(t, b, 2, 1, false)
+}
+
+func TestBitMatrix_Clear(t *testing.T) {
+	b, _ := ParseStringToBitMatrix("X.XX..\nXX..XX\n..XX.X", "X", ".")
+
+	b.Clear()
+	for i := 0; i < len(b.bits); i++ {
+		if b.bits[i] != 0 {
+			t.Fatalf("bits[%v] is not cleared", i)
+		}
+	}
+}
+
+func TestBitMatrix_SetRegion(t *testing.T) {
+	b, _ := NewBitMatrix(40, 40)
+
+	if e := b.SetRegion(-1, 10, 15, 20); e == nil {
+		t.Fatalf("SetRegion must be error")
+	}
+	if e := b.SetRegion(15, -1, 15, 20); e == nil {
+		t.Fatalf("SetRegion must be error")
+	}
+	if e := b.SetRegion(15, 10, 0, 20); e == nil {
+		t.Fatalf("SetRegion must be error")
+	}
+	if e := b.SetRegion(15, 10, 15, 0); e == nil {
+		t.Fatalf("SetRegion must be error")
+	}
+	if e := b.SetRegion(15, 10, 26, 20); e == nil {
+		t.Fatalf("SetRegion must be error")
+	}
+	if e := b.SetRegion(15, 10, 15, 31); e == nil {
+		t.Fatalf("SetRegion must be error")
+	}
+
+	e := b.SetRegion(15, 10, 15, 20)
+	if e != nil {
+		t.Fatalf("SetRegion returns error, %v", e)
+	}
+	testBitMatrixGet(t, b, 14, 10, false)
+	testBitMatrixGet(t, b, 15, 9, false)
+	testBitMatrixGet(t, b, 15, 10, true)
+	testBitMatrixGet(t, b, 14, 29, false)
+	testBitMatrixGet(t, b, 15, 30, false)
+	testBitMatrixGet(t, b, 15, 29, true)
+	testBitMatrixGet(t, b, 29, 9, false)
+	testBitMatrixGet(t, b, 30, 10, false)
+	testBitMatrixGet(t, b, 29, 10, true)
+	testBitMatrixGet(t, b, 29, 30, false)
+	testBitMatrixGet(t, b, 30, 29, false)
+	testBitMatrixGet(t, b, 29, 29, true)
+	testBitMatrixGet(t, b, 20, 29, true)
+
+	e = b.SetRegion(20, 20, 20, 20)
+	if e != nil {
+		t.Fatalf("SetRegion returns error, %v", e)
+	}
+	testBitMatrixGet(t, b, 20, 39, true)
+	testBitMatrixGet(t, b, 39, 20, true)
+	testBitMatrixGet(t, b, 39, 39, true)
+}
+
+func TestBitMatrix_GetRow(t *testing.T) {
+	b, _ := ParseStringToBitMatrix("X.X.\nX..X\n.XX.", "X", ".")
+
+	ba := b.GetRow(0, nil)
+
+	testBit(t, ba, 0, true)
+	testBit(t, ba, 1, false)
+	testBit(t, ba, 2, true)
+	testBit(t, ba, 3, false)
+
+	ba = b.GetRow(1, NewBitArray(2))
+	testBit(t, ba, 0, true)
+	testBit(t, ba, 1, false)
+	testBit(t, ba, 2, false)
+	testBit(t, ba, 3, true)
+
+	ba = b.GetRow(2, ba)
+	testBit(t, ba, 0, false)
+	testBit(t, ba, 1, true)
+	testBit(t, ba, 2, true)
+	testBit(t, ba, 3, false)
+}
+
+func TestBitMatrix_SetRow(t *testing.T) {
+	b, _ := ParseStringToBitMatrix("X.X.XX.\n..XX..X", "X", ".")
+	a := NewBitArray(b.width)
+	a.SetRange(1, 4) // .XXX...
+
+	b.SetRow(1, a)
+
+	testBitMatrixGet(t, b, 0, 0, true)
+	testBitMatrixGet(t, b, 1, 0, false)
+	testBitMatrixGet(t, b, 2, 0, true)
+	testBitMatrixGet(t, b, 3, 0, false)
+	testBitMatrixGet(t, b, 0, 1, false)
+	testBitMatrixGet(t, b, 1, 1, true)
+	testBitMatrixGet(t, b, 2, 1, true)
+	testBitMatrixGet(t, b, 3, 1, true)
+	testBitMatrixGet(t, b, 4, 1, false)
+	testBitMatrixGet(t, b, 5, 1, false)
+	testBitMatrixGet(t, b, 6, 1, false)
+}
+
+func TestBitMatrix_Rotate180(t *testing.T) {
+	b, _ := ParseStringToBitMatrix("...X.\n.XX..\n..XXX", "X", ".")
+	b.Rotate180()
+
+	testBitMatrixGet(t, b, 0, 0, true)
+	testBitMatrixGet(t, b, 1, 0, true)
+	testBitMatrixGet(t, b, 2, 0, true)
+	testBitMatrixGet(t, b, 3, 0, false)
+	testBitMatrixGet(t, b, 4, 0, false)
+
+	testBitMatrixGet(t, b, 0, 1, false)
+	testBitMatrixGet(t, b, 1, 1, false)
+	testBitMatrixGet(t, b, 2, 1, true)
+	testBitMatrixGet(t, b, 3, 1, true)
+	testBitMatrixGet(t, b, 4, 1, false)
+
+	testBitMatrixGet(t, b, 0, 2, false)
+	testBitMatrixGet(t, b, 1, 2, true)
+	testBitMatrixGet(t, b, 2, 2, false)
+	testBitMatrixGet(t, b, 3, 2, false)
+	testBitMatrixGet(t, b, 4, 2, false)
+}
+
+func TestGetEnclosingRectangle(t *testing.T) {
+
+}
+
+func TestBitMatrix_GetEnclosingRecangle(t *testing.T) {
+	b, _ := NewBitMatrix(35, 35)
+
+	n := b.GetEnclosingRectangle()
+	if n != nil {
+		t.Fatalf("enclosing rectangle must be nil")
+	}
+
+	b.Set(5, 8)
+	b.Set(3, 10)
+	b.Set(33, 20)
+	r := b.GetEnclosingRectangle()
+	e := []int{3, 8, 31, 13}
+	if !reflect.DeepEqual(r, e) {
+		t.Fatalf("rectangle is %v, expect %v", r, e)
+	}
+}
+
+func TestBitMatrix_GetTopLeftOnBit(t *testing.T) {
+	b, _ := NewBitMatrix(35, 35)
+
+	if b.GetTopLeftOnBit() != nil {
+		t.Fatalf("TopLeftOnBit must be nil")
+	}
+
+	b.Set(34, 34)
+	r := b.GetTopLeftOnBit()
+	e := []int{34, 34}
+	if !reflect.DeepEqual(r, e) {
+		t.Fatalf("TopLeft is %v, expect %v", r, e)
+	}
+
+	b.Set(3, 20)
+	b.Set(8, 13)
+	r = b.GetTopLeftOnBit()
+	e = []int{8, 13}
+	if !reflect.DeepEqual(r, e) {
+		t.Fatalf("TopLeft is %v, expect %v", r, e)
+	}
+
+	b.Set(0, 0)
+	r = b.GetTopLeftOnBit()
+	e = []int{0, 0}
+	if !reflect.DeepEqual(r, e) {
+		t.Fatalf("TopLeft is %v, expect %v", r, e)
+	}
+}
+
+func TestBitMatrix_GetBottomRightOnBit(t *testing.T) {
+	b, _ := NewBitMatrix(35, 35)
+
+	if b.GetBottomRightOnBit() != nil {
+		t.Fatalf("BottomRightOnBit must be nil")
+	}
+
+	b.Set(0, 0)
+	r := b.GetBottomRightOnBit()
+	e := []int{0, 0}
+	if !reflect.DeepEqual(r, e) {
+		t.Fatalf("BottomRight is %v, expect %v", r, e)
+	}
+	b.Set(3, 20)
+	b.Set(8, 13)
+	r = b.GetBottomRightOnBit()
+	e = []int{3, 20}
+	if !reflect.DeepEqual(r, e) {
+		t.Fatalf("BottomRight is %v, expect %v", r, e)
+	}
+
+	b.Set(34, 34)
+	r = b.GetBottomRightOnBit()
+	e = []int{34, 34}
+	if !reflect.DeepEqual(r, e) {
+		t.Fatalf("BottomRight is %v, expect %v", r, e)
+	}
+}
+
+func TestBitMatrix_String(t *testing.T) {
+	s := "X   X   \n  X X   \n  X   X \n"
+	b, _ := ParseStringToBitMatrix(s, "X ", "  ")
+
+	if r := b.String(); r != s {
+		t.Fatalf("String is\n%s\nexpect:\n%s", r, s)
+	}
+
+	s2 := "X .X .\n.X X .\n.X .X \n"
+	if r := b.ToString("X ", "."); r != s2 {
+		t.Fatalf("String is\n%s\nexpect:\n%s", r, s2)
+	}
+
+}
