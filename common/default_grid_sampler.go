7d7c52fe72bc19d4f4affe2996bd150e243acdc1
makki_d <d.makiuchi@gmail.com>
2018-05-21 00:21:06 +0900

    add GridSampler and DefaultGridSampler (without test)

---
+++ common/default_grid_sampler.go
@@ -0,0 +1,64 @@
+package common
+
+import (
+	"github.com/makiuchi-d/gozxing"
+)
+
+type DefaultGridSampler struct{}
+
+func NewDefaultGridSampler() GridSampler {
+	return DefaultGridSampler{}
+}
+
+func (s DefaultGridSampler) SampleGrid(image *BitMatrix, dimensionX, dimensionY int,
+	p1ToX, p1ToY, p2ToX, p2ToY, p3ToX, p3ToY, p4ToX, p4ToY float64,
+	p1FromX, p1FromY, p2FromX, p2FromY, p3FromX, p3FromY, p4FromX, p4FromY float64) (*BitMatrix, error) {
+
+	transform := PerspectiveTransform_QuadrilateralToQuadrilateral(
+		p1ToX, p1ToY, p2ToX, p2ToY, p3ToX, p3ToY, p4ToX, p4ToY,
+		p1FromX, p1FromY, p2FromX, p2FromY, p3FromX, p3FromY, p4FromX, p4FromY)
+
+	return s.SampleGridFromTransform(image, dimensionX, dimensionY, transform)
+}
+
+func (s DefaultGridSampler) SampleGridFromTransform(image *BitMatrix,
+	dimensionX, dimensionY int, transform *PerspectiveTransform) (*BitMatrix, error) {
+
+	if dimensionX <= 0 || dimensionY <= 0 {
+		return nil, gozxing.NotFoundException_GetNotFoundInstance()
+	}
+	bits, e := NewBitMatrix(dimensionX, dimensionY)
+	if e != nil {
+		return nil, e
+	}
+	points := make([]float64, 2*dimensionX)
+	for y := 0; y < dimensionY; y++ {
+		max := len(points)
+		iValue := float64(y) + 0.5
+		for x := 0; x < max; x += 2 {
+			points[x] = float64(x/2) + 0.5
+			points[x+1] = iValue
+		}
+		transform.TransformPoints(points)
+
+		e := GridSampler_checkAndNudgePoints(image, points)
+		if e != nil {
+			return nil, e
+		}
+
+		for x := 0; x < max; x += 2 {
+			px := int(points[x])
+			py := int(points[y])
+
+			if px >= image.GetWidth() || py >= image.GetHeight() {
+				// cause of ArrayIndexOutOfBoundsException in image.Get(px, py)
+				return nil, gozxing.NotFoundException_GetNotFoundInstance()
+			}
+
+			if image.Get(px, py) {
+				bits.Set(x/2, y)
+			}
+		}
+	}
+	return bits, nil
+}
---
+++ common/grid_sampler.go
@@ -0,0 +1,78 @@
+package common
+
+import (
+	"github.com/makiuchi-d/gozxing"
+)
+
+type GridSampler interface {
+	SampleGrid(image *BitMatrix, dimensionX, dimensionY int,
+		p1ToX, p1ToY, p2ToX, p2ToY, p3ToX, p3ToY, p4ToX, p4ToY float64,
+		p1FromX, p1FromY, p2FromX, p2FromY, p3FromX, p3FromY, p4FromX, p4FromY float64) (*BitMatrix, error)
+
+	SampleGridFromTransform(image *BitMatrix,
+		dimensionX, dimensionY int, transform *PerspectiveTransform) (*BitMatrix, error)
+}
+
+var gridSampler GridSampler = NewDefaultGridSampler()
+
+func GridSampler_SetGridSampler(newGridSampler GridSampler) {
+	gridSampler = newGridSampler
+}
+
+func GridSampler_GetInstance() GridSampler {
+	return gridSampler
+}
+
+func GridSampler_checkAndNudgePoints(image *BitMatrix, points []float64) error {
+	width := image.GetWidth()
+	height := image.GetHeight()
+
+	nudged := true
+	for offset := 0; offset < len(points) && nudged; offset += 2 {
+		x := int(points[offset])
+		y := int(points[offset+1])
+		if x < -1 || x > width || y < -1 || y > height {
+			return gozxing.NotFoundException_GetNotFoundInstance()
+		}
+		nudged = false
+		if x == -1 {
+			points[offset] = 0.0
+			nudged = true
+		} else if x == width {
+			points[offset] = float64(width - 1)
+			nudged = true
+		}
+		if y == -1 {
+			points[offset+1] = 0.0
+			nudged = true
+		} else if y == height {
+			points[offset+1] = float64(height)
+			nudged = true
+		}
+	}
+
+	nudged = true
+	for offset := len(points) - 2; offset >= 0 && nudged; offset -= 2 {
+		x := int(points[offset])
+		y := int(points[offset+1])
+		if x < -1 || x > width || y < -1 || y > height {
+			return gozxing.NotFoundException_GetNotFoundInstance()
+		}
+		nudged = false
+		if x == -1 {
+			points[offset] = 0.0
+			nudged = true
+		} else if x == width {
+			points[offset] = float64(width - 1)
+			nudged = true
+		}
+		if y == -1 {
+			points[offset+1] = 0.0
+			nudged = true
+		} else if y == height {
+			points[offset+1] = float64(height - 1)
+			nudged = true
+		}
+	}
+	return nil
+}
