e32a03a3b5ff90fe991691f02e100d3a6449ef76
makki_d <d.makiuchi@gmail.com>
2018-05-21 11:46:22 +0900

    implement qrcode.decoder.ErrorCorrectionLevel

---
+++ qrcode/decoder/error_correction_level.go
@@ -0,0 +1,42 @@
+package decoder
+
+import (
+	"errors"
+)
+
+type ErrorCorrectionLevel int
+
+const (
+	ErrorCorrectionLevel_L ErrorCorrectionLevel = 0x01 // ~7% correction
+	ErrorCorrectionLevel_M ErrorCorrectionLevel = 0x00 // ~15% correction
+	ErrorCorrectionLevel_Q ErrorCorrectionLevel = 0x03 // ~25% correction
+	ErrorCorrectionLevel_H ErrorCorrectionLevel = 0x02 // ~30% correction
+)
+
+func ErrorCorrectionLevel_ForBits(bits int) (ErrorCorrectionLevel, error) {
+	switch bits {
+	case 0:
+		return ErrorCorrectionLevel_M, nil
+	case 1:
+		return ErrorCorrectionLevel_L, nil
+	case 2:
+		return ErrorCorrectionLevel_H, nil
+	case 3:
+		return ErrorCorrectionLevel_Q, nil
+	}
+	return -1, errors.New("IllegalArgumentException")
+}
+
+func (e ErrorCorrectionLevel) String() string {
+	switch e {
+	case ErrorCorrectionLevel_M:
+		return "M"
+	case ErrorCorrectionLevel_L:
+		return "L"
+	case ErrorCorrectionLevel_H:
+		return "H"
+	case ErrorCorrectionLevel_Q:
+		return "Q"
+	}
+	return "error"
+}
---
+++ qrcode/decoder/error_correction_level_test.go
@@ -0,0 +1,46 @@
+package decoder
+
+import (
+	"testing"
+)
+
+func testErrorCorrectionLevel_ForBits(t *testing.T, bits int, expect ErrorCorrectionLevel){
+	r, e := ErrorCorrectionLevel_ForBits(bits)
+	if e != nil {
+		t.Fatalf("ForBits(0) returns error, %v", e)
+	}
+	if r != expect {
+		t.Fatalf("ForBits(%v) != %v, expect %v", bits, r, expect)
+	}
+}
+
+func TestErrorCorrectionLevel_ForBits(t *testing.T){
+
+	if _, e := ErrorCorrectionLevel_ForBits(-1); e == nil {
+		t.Fatalf("ForBits(-1) must be error")
+	}
+
+	testErrorCorrectionLevel_ForBits(t, 0, ErrorCorrectionLevel_M)
+	testErrorCorrectionLevel_ForBits(t, 1, ErrorCorrectionLevel_L)
+	testErrorCorrectionLevel_ForBits(t, 2, ErrorCorrectionLevel_H)
+	testErrorCorrectionLevel_ForBits(t, 3, ErrorCorrectionLevel_Q)
+}
+
+func TestErrorCorrectionLevel_String(t *testing.T){
+	if s := ErrorCorrectionLevel_L.String(); s != "L" {
+		t.Fatalf("ErrorCorrectionLevel_L string is %v, expect L", s)
+	}
+	if s := ErrorCorrectionLevel_M.String(); s != "M" {
+		t.Fatalf("ErrorCorrectionLevel_M string is %v, expect M", s)
+	}
+	if s := ErrorCorrectionLevel_Q.String(); s != "Q" {
+		t.Fatalf("ErrorCorrectionLevel_Q string is %v, expect Q", s)
+	}
+	if s := ErrorCorrectionLevel_H.String(); s != "H" {
+		t.Fatalf("ErrorCorrectionLevel_H string is %v, expect H", s)
+	}
+	if s := ErrorCorrectionLevel(-1).String(); s != "error" {
+		t.Fatalf("invalid ErrorCorrectionLevel string must be \"error\", %v", s)
+	}
+
+}
