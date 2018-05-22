e45a2905f4a96895268f6ea78028cfbc76fac095
makki_d <d.makiuchi@gmail.com>
2018-05-23 00:19:32 +0900

    implement qrcode.detector.FinderPattern

---
+++ qrcode/detector/finder_pattern.go
@@ -0,0 +1,49 @@
+package detector
+
+import (
+	"math"
+
+	"github.com/makiuchi-d/gozxing"
+)
+
+type FinderPattern struct {
+	gozxing.ResultPoint
+	estimatedModuleSize float64
+	count               int
+}
+
+func NewFinderPattern1(posX, posY, estimatedModuleSize float64) FinderPattern {
+	return NewFinderPattern(posX, posY, estimatedModuleSize, 1)
+}
+
+func NewFinderPattern(posX, posY, estimatedModuleSize float64, count int) FinderPattern {
+	return FinderPattern{
+		gozxing.NewResultPoint(posX, posY),
+		estimatedModuleSize,
+		count,
+	}
+}
+
+func (f FinderPattern) GetEstimatedModuleSize() float64 {
+	return f.estimatedModuleSize
+}
+
+func (f FinderPattern) GetCount() int {
+	return f.count
+}
+
+func (f FinderPattern) AboutEquals(moduleSize, i, j float64) bool {
+	if math.Abs(i-f.GetY()) <= moduleSize && math.Abs(j-f.GetX()) <= moduleSize {
+		moduleSizeDiff := math.Abs(moduleSize - f.estimatedModuleSize)
+		return moduleSizeDiff <= 1.0 || moduleSizeDiff <= f.estimatedModuleSize
+	}
+	return false
+}
+
+func (f FinderPattern) CombineEstimate(i, j, newModuleSize float64) FinderPattern {
+	combinedCount := float64(f.count + 1)
+	combinedX := (float64(f.count)*f.GetX() + j) / combinedCount
+	combinedY := (float64(f.count)*f.GetY() + i) / combinedCount
+	combinedModuleSize := (float64(f.count)*f.estimatedModuleSize + newModuleSize) / combinedCount
+	return NewFinderPattern(combinedX, combinedY, combinedModuleSize, int(combinedCount))
+}
---
+++ qrcode/detector/finder_pattern_test.go
@@ -0,0 +1,45 @@
+package detector
+
+import (
+	"testing"
+)
+
+func TestNewFinderPattern1(t *testing.T) {
+	f := NewFinderPattern1(10, 20, 5)
+	if r := f.GetX(); r != 10 {
+		t.Fatalf("GetX() = %v, expect 10", r)
+	}
+	if r := f.GetY(); r != 20 {
+		t.Fatalf("GetY() = %v, expect 20", r)
+	}
+	if r := f.GetEstimatedModuleSize(); r != 5 {
+		t.Fatalf("GetEstimatedModuleSize() = %v, expect 5", r)
+	}
+	if r := f.GetCount(); r != 1 {
+		t.Fatalf("GetCount() = %v, expect 1", r)
+	}
+}
+
+func testFinderPattern_AboutEquals(t *testing.T, f FinderPattern, moduleSize, i, j float64, expect bool) {
+	if r := f.AboutEquals(moduleSize, i, j); r != expect {
+		t.Fatalf("AboutEquals(%v,%v,%v) = %v, expect %v", moduleSize, i, j, r, expect)
+	}
+}
+
+func TestFinderPattern_AboutEquals(t *testing.T) {
+	f := NewFinderPattern(10, 20, 4.5, 2)
+	testFinderPattern_AboutEquals(t, f, 4, 21, 11, true)
+	testFinderPattern_AboutEquals(t, f, 3, 21, 11, true)
+	testFinderPattern_AboutEquals(t, f, 10, 21, 11, false)
+	testFinderPattern_AboutEquals(t, f, 4, 20, 40, false)
+	testFinderPattern_AboutEquals(t, f, 4, 40, 10, false)
+}
+
+func TestFinderPattern_CombineEstimate(t *testing.T) {
+	f := NewFinderPattern(10, 20, 4, 3)
+	f2 := f.CombineEstimate(5, 8, 2)
+	expect := NewFinderPattern(9.5, 16.25, 3.5, 4)
+	if f2 != expect {
+		t.Fatalf("CombinedEstimate = %v, expect %v", f2, expect)
+	}
+}
