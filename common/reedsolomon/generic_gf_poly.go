1290076effb729437ce2fb863baa110f4461bc81
makki_d <d.makiuchi@gmail.com>
2018-06-04 00:21:59 +0900

    implement common.reedsolomon.GenericGFPoly

--- common/reedsolomon/generic_gf_poly.go
+++ common/reedsolomon/generic_gf_poly.go
@@ -2,6 +2,7 @@ package reedsolomon
 
 import (
 	"errors"
+	"fmt"
 )
 
 type GenericGFPoly struct {
@@ -38,7 +39,7 @@ func (this *GenericGFPoly) GetCoefficients() []int {
 	return this.coefficients
 }
 
-func (this *GenericGFPoly) Getdegree() int {
+func (this *GenericGFPoly) GetDegree() int {
 	return len(this.coefficients) - 1
 }
 
@@ -48,4 +49,185 @@ func (this *GenericGFPoly) IsZero() bool {
 
 func (this *GenericGFPoly) GetCoefficient(degree int) int {
 	return this.coefficients[len(this.coefficients)-1-degree]
+}
+
+func (this *GenericGFPoly) EvaluateAt(a int) int {
+	if a == 0 {
+		// Just return the x^0 coefficient
+		return this.GetCoefficient(0)
+	}
+	if a == 1 {
+		// Just the sum of the coefficients
+		result := 0
+		for _, coefficient := range this.coefficients {
+			result = GenericGF_addOrSubtract(result, coefficient)
+		}
+		return result
+	}
+	result := this.coefficients[0]
+	size := len(this.coefficients)
+	for i := 1; i < size; i++ {
+		result = GenericGF_addOrSubtract(this.field.Multiply(a, result), this.coefficients[i])
+	}
+	return result
+}
+
+func (this *GenericGFPoly) AddOrSubtract(other *GenericGFPoly) (*GenericGFPoly, error) {
+	if this.field != other.field {
+		return nil, errors.New("IllegalArgumentException: GenericGFPolys do not have same GenericGF field")
+	}
+	if this.IsZero() {
+		return other, nil
+	}
+	if other.IsZero() {
+		return this, nil
+	}
+
+	smallerCoefficients := this.coefficients
+	largerCoefficients := other.coefficients
+	if len(smallerCoefficients) > len(largerCoefficients) {
+		smallerCoefficients, largerCoefficients = largerCoefficients, smallerCoefficients
+	}
+	sumDiff := make([]int, len(largerCoefficients))
+	lengthDiff := len(largerCoefficients) - len(smallerCoefficients)
+	// Copy high-order terms only found in higher-degree polynomial's coefficients
+	copy(sumDiff, largerCoefficients[:lengthDiff])
+	for i := lengthDiff; i < len(largerCoefficients); i++ {
+		sumDiff[i] = GenericGF_addOrSubtract(smallerCoefficients[i-lengthDiff], largerCoefficients[i])
+	}
+
+	return NewGenericGFPoly(this.field, sumDiff)
+}
+
+func (this *GenericGFPoly) Multiply(other *GenericGFPoly) (*GenericGFPoly, error) {
+	if this.field != other.field {
+		return nil, errors.New("IllegalArgumentException: GenericGFPolys do not have same GenericGF field")
+	}
+	if this.IsZero() || other.IsZero() {
+		return this.field.GetZero(), nil
+	}
+	aCoefficients := this.coefficients
+	aLength := len(aCoefficients)
+	bCoefficients := other.coefficients
+	bLength := len(bCoefficients)
+	product := make([]int, aLength+bLength-1)
+	for i := 0; i < aLength; i++ {
+		aCoeff := aCoefficients[i]
+		for j := 0; j < bLength; j++ {
+			product[i+j] = GenericGF_addOrSubtract(product[i+j],
+				this.field.Multiply(aCoeff, bCoefficients[j]))
+		}
+	}
+	return NewGenericGFPoly(this.field, product)
+}
+
+func (this *GenericGFPoly) MultiplyBy(scalar int) *GenericGFPoly {
+	if scalar == 0 {
+		return this.field.GetZero()
+	}
+	if scalar == 1 {
+		return this
+	}
+	size := len(this.coefficients)
+	product := make([]int, size)
+	for i := 0; i < size; i++ {
+		product[i] = this.field.Multiply(this.coefficients[i], scalar)
+	}
+	ret, _ := NewGenericGFPoly(this.field, product)
+	return ret
+}
+
+func (this *GenericGFPoly) MultiplyByMonomial(degree, coefficient int) (*GenericGFPoly, error) {
+	if degree < 0 {
+		return nil, errors.New("IllegalArgumentException")
+	}
+	if coefficient == 0 {
+		return this.field.GetZero(), nil
+	}
+	size := len(this.coefficients)
+	product := make([]int, size+degree)
+	for i := 0; i < size; i++ {
+		product[i] = this.field.Multiply(this.coefficients[i], coefficient)
+	}
+	return NewGenericGFPoly(this.field, product)
+}
+
+func (this *GenericGFPoly) Divide(other *GenericGFPoly) ([]*GenericGFPoly, error) {
+	if this.field != other.field {
+		return nil, errors.New("IllegalArgumentException: GenericGFPolys do not have same GenericGF field")
+	}
+	if other.IsZero() {
+		return nil, errors.New("IllegalArgumentException: Divide by 0")
+	}
+
+	quotient := this.field.GetZero()
+	remainder := this
+
+	denominatorLeadingTerm := other.GetCoefficient(other.GetDegree())
+	inverseDenominatorLeadingTerm, e := this.field.Inverse(denominatorLeadingTerm)
+	if e != nil {
+		return nil, e
+	}
+
+	for remainder.GetDegree() >= other.GetDegree() && !remainder.IsZero() {
+		degreeDifference := remainder.GetDegree() - other.GetDegree()
+		scale := this.field.Multiply(remainder.GetCoefficient(remainder.GetDegree()), inverseDenominatorLeadingTerm)
+
+		term, e := other.MultiplyByMonomial(degreeDifference, scale)
+		if e != nil {
+			return nil, e
+		}
+		iterationQuotient, e := this.field.BuildMonomial(degreeDifference, scale)
+		if e != nil {
+			return nil, e
+		}
+		quotient, e = quotient.AddOrSubtract(iterationQuotient)
+		if e != nil {
+			return nil, e
+		}
+		remainder, e = remainder.AddOrSubtract(term)
+		if e != nil {
+			return nil, e
+		}
+	}
+
+	return []*GenericGFPoly{quotient, remainder}, nil
+}
+
+func (this *GenericGFPoly) String() string {
+	if this.GetDegree() == 0 {
+		return fmt.Sprintf("%d", this.GetCoefficient(0))
+	}
+	result := make([]byte, 0, 8*this.GetDegree())
+	for degree := this.GetDegree(); degree >= 0; degree-- {
+		coefficient := this.GetCoefficient(degree)
+		if coefficient != 0 {
+			if coefficient < 0 {
+				result = append(result, []byte(" - ")...)
+				coefficient = -coefficient
+			} else {
+				if len(result) > 0 {
+					result = append(result, []byte(" + ")...)
+				}
+			}
+			if degree == 0 || coefficient != 1 {
+				alphaPower, _ := this.field.Log(coefficient)
+				if alphaPower == 0 {
+					result = append(result, byte('1'))
+				} else if alphaPower == 1 {
+					result = append(result, byte('a'))
+				} else {
+					result = append(result, []byte(fmt.Sprintf("a^%d", alphaPower))...)
+				}
+			}
+			if degree != 0 {
+				if degree == 1 {
+					result = append(result, byte('x'))
+				} else {
+					result = append(result, []byte(fmt.Sprintf("x^%d", degree))...)
+				}
+			}
+		}
+	}
+	return string(result)
 }
---
+++ common/reedsolomon/generic_gf_poly_test.go
@@ -0,0 +1,252 @@
+package reedsolomon
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewGenericGFPoly(t *testing.T) {
+	gf := GenericGF_QR_CODE_FIELD_256
+
+	if _, e := NewGenericGFPoly(gf, []int{}); e == nil {
+		t.Fatalf("NewGenericGFPoly must be error")
+	}
+
+	coefficients := []int{0, 0, 0}
+	p, e := NewGenericGFPoly(gf, coefficients)
+	if e != nil {
+		t.Fatalf("NewGenericGFPoly returns error, %v", e)
+	}
+	if len(p.coefficients) != 1 || p.coefficients[0] != 0 {
+		t.Fatalf("coefficients is %v, expect %v", p.coefficients, coefficients)
+	}
+	if !p.IsZero() {
+		t.Fatalf("IsZero() must be true")
+	}
+
+	coefficients = []int{0, 1, 2}
+	p, e = NewGenericGFPoly(gf, coefficients)
+	if e != nil {
+		t.Fatalf("NewGenericGFPoly returns error, %v", e)
+	}
+	if !reflect.DeepEqual(p.GetCoefficients(), coefficients[1:]) {
+		t.Fatalf("coefficients is %v, expect %v", p.GetCoefficients(), coefficients[1:])
+	}
+
+	coefficients = []int{1, 2, 3}
+	p, e = NewGenericGFPoly(gf, coefficients)
+	if e != nil {
+		t.Fatalf("NewGenericGFPoly returns error, %v", e)
+	}
+	if !reflect.DeepEqual(p.GetCoefficients(), coefficients) {
+		t.Fatalf("coefficients is %v, expect %v", p.GetCoefficients(), coefficients)
+	}
+
+	if r := p.GetDegree(); r != 2 {
+		t.Fatalf("GetDegree = %v, expect 2", r)
+	}
+	if r := p.GetCoefficient(0); r != 3 {
+		t.Fatalf("GetCoefficient(0) = %v, expect 3", r)
+	}
+	if r := p.GetCoefficient(1); r != 2 {
+		t.Fatalf("GetCoefficient(1) = %v, expect 2", r)
+	}
+	if r := p.GetCoefficient(2); r != 1 {
+		t.Fatalf("GetCoefficient(2) = %v, expect 1", r)
+	}
+}
+
+func testGenericGFPoly_EvaluateAt(t *testing.T, p *GenericGFPoly, a, expect int) {
+	if r := p.EvaluateAt(a); r != expect {
+		t.Fatalf("EvaluateAt(%v) = %v, expect %v", a, r, expect)
+	}
+}
+
+func TestGenericGFPoly_EvaluateAt(t *testing.T) {
+	gf := GenericGF_QR_CODE_FIELD_256
+	coefficients := []int{2, 3, 5}
+	p, _ := NewGenericGFPoly(gf, coefficients)
+
+	testGenericGFPoly_EvaluateAt(t, p, 0, 5)
+	testGenericGFPoly_EvaluateAt(t, p, 1, 4)
+	testGenericGFPoly_EvaluateAt(t, p, 2, 11)
+	testGenericGFPoly_EvaluateAt(t, p, 3, 10)
+	testGenericGFPoly_EvaluateAt(t, p, 120, 88)
+	testGenericGFPoly_EvaluateAt(t, p, 255, 192)
+}
+
+func testGenericGFPoly_AddOrSubtract(t *testing.T, p, other *GenericGFPoly, expect []int) {
+	r, e := p.AddOrSubtract(other)
+	if e != nil {
+		t.Fatalf("AddOrSubtract returns error, %v", e)
+	}
+	if r.field != p.field {
+		t.Fatalf("AddOrSubtract returns different GF, %v, expect %v", r.field, p.field)
+	}
+	if !reflect.DeepEqual(r.coefficients, expect) {
+		t.Fatalf("AddOrSubtract coefficients = '%v', expect '%v'", r.coefficients, expect)
+	}
+}
+
+func TestGenericGFPoly_AddOrSubtract(t *testing.T) {
+	gf := GenericGF_QR_CODE_FIELD_256
+
+	p, _ := NewGenericGFPoly(gf, []int{0})
+	other, _ := NewGenericGFPoly(GenericGF_AZTEC_PARAM, []int{1, 2, 3})
+	if _, e := p.AddOrSubtract(other); e == nil {
+		t.Fatalf("AddOrSubtract must be error")
+	}
+
+	other, _ = NewGenericGFPoly(gf, []int{1, 2, 3})
+	testGenericGFPoly_AddOrSubtract(t, p, other, other.coefficients)
+
+	p, _ = NewGenericGFPoly(gf, []int{1, 2, 3})
+	other, _ = NewGenericGFPoly(gf, []int{0})
+	testGenericGFPoly_AddOrSubtract(t, p, other, p.coefficients)
+
+	other, _ = NewGenericGFPoly(gf, []int{3, 5, 7})
+	testGenericGFPoly_AddOrSubtract(t, p, other, []int{2, 7, 4})
+
+	other, _ = NewGenericGFPoly(gf, []int{3, 5})
+	testGenericGFPoly_AddOrSubtract(t, p, other, []int{1, 1, 6})
+}
+
+func testGenericGFPoly_Multiply(t *testing.T, p, o *GenericGFPoly, expect []int) {
+	r, e := p.Multiply(o)
+	if e != nil {
+		t.Fatalf("Multiply(%v) returns error, %v", o, e)
+	}
+	if r.field != p.field {
+		t.Fatalf("Multiply(%v) returns different GF, %v, expect %v", o, r.field, p.field)
+	}
+	if !reflect.DeepEqual(r.coefficients, expect) {
+		t.Fatalf("Multiply(%v) coefficients = '%v', expect '%v'", o, r.coefficients, expect)
+	}
+}
+
+func TestGenericGFPoly_Multiply(t *testing.T) {
+	gf := GenericGF_QR_CODE_FIELD_256
+
+	p, _ := NewGenericGFPoly(gf, []int{0})
+	o, _ := NewGenericGFPoly(GenericGF_AZTEC_PARAM, []int{1, 2, 3})
+	if _, e := p.Multiply(o); e == nil {
+		t.Fatalf("Multiply(%v) must be error", o)
+	}
+
+	o, _ = NewGenericGFPoly(gf, []int{1, 2, 3})
+	testGenericGFPoly_Multiply(t, p, o, []int{0})
+
+	p, _ = NewGenericGFPoly(gf, []int{1, 2, 3})
+	o, _ = NewGenericGFPoly(gf, []int{0})
+	testGenericGFPoly_Multiply(t, p, o, []int{0})
+
+	p, _ = NewGenericGFPoly(gf, []int{1, 2, 3})
+	o, _ = NewGenericGFPoly(gf, []int{7, 11})
+	testGenericGFPoly_Multiply(t, p, o, []int{7, 5, 31, 29})
+}
+
+func testGenericGFPoly_MultiplyBy(t *testing.T, p *GenericGFPoly, s int, expect []int) {
+	r := p.MultiplyBy(s)
+	if r.field != p.field {
+		t.Fatalf("MultiplyBy returns different GF, %v, expect %v", r.field, p.field)
+	}
+	if !reflect.DeepEqual(r.coefficients, expect) {
+		t.Fatalf("MultiplyBy(%v) coefficients = '%v', expect '%v'", s, r.coefficients, expect)
+	}
+}
+
+func TestGenericGFPoly_MultiplyBy(t *testing.T) {
+	gf := GenericGF_QR_CODE_FIELD_256
+	p, _ := NewGenericGFPoly(gf, []int{2, 3, 5, 7})
+
+	testGenericGFPoly_MultiplyBy(t, p, 0, []int{0})
+	testGenericGFPoly_MultiplyBy(t, p, 1, []int{2, 3, 5, 7})
+	testGenericGFPoly_MultiplyBy(t, p, 3, []int{6, 5, 15, 9})
+}
+
+func testGenericGFPoly_MultiplyMonomial(t *testing.T, p *GenericGFPoly, d, c int, expect []int) {
+	r, e := p.MultiplyByMonomial(d, c)
+	if e != nil {
+		t.Fatalf("MultiplyByMonomial(%v,%v) returns error, %v", d, c, e)
+	}
+	if r.field != p.field {
+		t.Fatalf("MultiplyByMonomial(%v,%v) returns different GF, %v, expect %v", d, c, r.field, p.field)
+	}
+	if !reflect.DeepEqual(r.coefficients, expect) {
+		t.Fatalf("MultiplyByMonomial(%v,%v) coefficients = '%v', expect '%v'", d, c, r.coefficients, expect)
+	}
+}
+
+func TestGenericGFPoly_MultiplyByMonomial(t *testing.T) {
+	gf := GenericGF_QR_CODE_FIELD_256
+	p, _ := NewGenericGFPoly(gf, []int{3, 5, 7})
+
+	if _, e := p.MultiplyByMonomial(-1, 3); e == nil {
+		t.Fatalf("MultiplyByMonomial(-1, 3) must be error")
+	}
+	testGenericGFPoly_MultiplyMonomial(t, p, 1, 0, []int{0})
+	testGenericGFPoly_MultiplyMonomial(t, p, 0, 1, []int{3, 5, 7})
+	testGenericGFPoly_MultiplyMonomial(t, p, 0, 3, []int{5, 15, 9})
+	testGenericGFPoly_MultiplyMonomial(t, p, 1, 1, []int{3, 5, 7, 0})
+	testGenericGFPoly_MultiplyMonomial(t, p, 1, 3, []int{5, 15, 9, 0})
+	testGenericGFPoly_MultiplyMonomial(t, p, 3, 5, []int{15, 17, 27, 0, 0, 0})
+}
+
+func testGenericGFPoly_Divide(t *testing.T, p, o *GenericGFPoly, expquot, exprem []int) {
+	arr, e := p.Divide(o)
+	if e != nil {
+		t.Fatalf("Divide(%v) returns error, %v", o, e)
+	}
+	if len(arr) != 2 {
+		t.Fatalf("Divide(%v) must return 2 GenericGFPoly, count=%v", o, len(arr))
+	}
+	if arr[0].field != p.field {
+		t.Fatalf("Divide(%v) quotient has different GF, %v", o, arr[0].field)
+	}
+	if arr[1].field != p.field {
+		t.Fatalf("Divide(%v) remainder has different GF, %v", o, arr[1].field)
+	}
+	if !reflect.DeepEqual(arr[0].coefficients, expquot) {
+		t.Fatalf("Divide(%v) quotient coefficients = '%v', expect '%v'", o, arr[0].coefficients, expquot)
+	}
+	if !reflect.DeepEqual(arr[1].coefficients, exprem) {
+		t.Fatalf("Divide(%v) reminder coefficients = '%v', expect '%v'", o, arr[1].coefficients, exprem)
+	}
+}
+
+func TestGenericGFPoly_Divide(t *testing.T) {
+	gf := GenericGF_QR_CODE_FIELD_256
+	p, _ := NewGenericGFPoly(gf, []int{11, 7, 5, 0, 3})
+	o, _ := NewGenericGFPoly(GenericGF_AZTEC_PARAM, []int{1, 2, 3})
+
+	if _, e := p.Divide(o); e == nil {
+		t.Fatalf("Divide by different field must be error")
+	}
+
+	o, _ = NewGenericGFPoly(gf, []int{0})
+	if _, e := p.Divide(o); e == nil {
+		t.Fatalf("Divide by 0 must be error")
+	}
+
+	testGenericGFPoly_Divide(t, p, p, []int{1}, []int{0})
+
+	o, _ = NewGenericGFPoly(gf, []int{1, 0, 0, 0, 0, 0})
+	testGenericGFPoly_Divide(t, p, o, []int{0}, p.coefficients)
+
+	o, _ = NewGenericGFPoly(gf, []int{3, 2, 1})
+	testGenericGFPoly_Divide(t, p, o, []int{242, 161, 147}, []int{154, 144})
+}
+
+func TestGenericGFPoly_String(t *testing.T) {
+	gf := GenericGF_QR_CODE_FIELD_256
+
+	if s := gf.GetZero().String(); s != "0" {
+		t.Fatalf("string of zero must be \"0\", %s", s)
+	}
+
+	p, _ := NewGenericGFPoly(gf, []int{3, 0, -2, 1, 1})
+	expect := "a^25x^4 - ax^2 + x + 1"
+	if s := p.String(); s != expect {
+		t.Fatalf("string is %s, expect %s", s, expect)
+	}
+}
