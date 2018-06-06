7a40f305e2e11c6f9562409b2c40327b131e6eb9
makki_d <d.makiuchi@gmail.com>
2018-06-06 10:19:52 +0900

    implement qrcode.decoder.DataMask

---
+++ qrcode/decoder/data_mask.go
@@ -0,0 +1,99 @@
+package decoder
+
+import (
+	"github.com/makiuchi-d/gozxing/common"
+)
+
+var DataMaskValues = []DataMask{
+
+	// See ISO 18004:2006 6.8.1
+
+	/**
+	 * 000: mask bits for which (x + y) mod 2 == 0
+	 */
+	{ // DATA_MASK_000
+		func(i, j int) bool {
+			return ((i + j) & 0x01) == 0
+		},
+	},
+
+	/**
+	 * 001: mask bits for which x mod 2 == 0
+	 */
+	{ // DATA_MASK_001
+		func(i, j int) bool {
+			return (i & 0x01) == 0
+		},
+	},
+
+	/**
+	 * 010: mask bits for which y mod 3 == 0
+	 */
+	{ // DATA_MASK_010
+		func(i, j int) bool {
+			return j%3 == 0
+		},
+	},
+
+	/**
+	 * 011: mask bits for which (x + y) mod 3 == 0
+	 */
+	{ // DATA_MASK_011
+		func(i, j int) bool {
+			return (i+j)%3 == 0
+		},
+	},
+
+	/**
+	 * 100: mask bits for which (x/2 + y/3) mod 2 == 0
+	 */
+	{ // DATA_MASK_100
+		func(i, j int) bool {
+			return (((i / 2) + (j / 3)) & 0x01) == 0
+		},
+	},
+
+	/**
+	 * 101: mask bits for which xy mod 2 + xy mod 3 == 0
+	 * equivalently, such that xy mod 6 == 0
+	 */
+	{ // DATA_MASK_101
+		func(i, j int) bool {
+			return (i*j)%6 == 0
+		},
+	},
+
+	/**
+	 * 110: mask bits for which (xy mod 2 + xy mod 3) mod 2 == 0
+	 * equivalently, such that xy mod 6 < 3
+	 */
+	{ // DATA_MASK_110
+		func(i, j int) bool {
+			return ((i * j) % 6) < 3
+		},
+	},
+
+	/**
+	 * 111: mask bits for which ((x+y)mod 2 + xy mod 3) mod 2 == 0
+	 * equivalently, such that (x + y + xy mod 3) mod 2 == 0
+	 */
+	{ // DATA_MASK_111
+		func(i, j int) bool {
+			return ((i + j + ((i * j) % 3)) & 0x01) == 0
+		},
+	},
+}
+
+type DataMask struct {
+	isMasked func(i, j int) bool
+}
+
+func (this DataMask) UnmaskBitMatrix(bits *common.BitMatrix, dimension int) {
+	for i := 0; i < dimension; i++ {
+		for j := 0; j < dimension; j++ {
+			if this.isMasked(i, j) {
+				bits.Flip(j, i)
+			}
+		}
+	}
+}
---
+++ qrcode/decoder/data_mask_test.go
@@ -0,0 +1,75 @@
+package decoder
+
+import (
+	"testing"
+
+	"github.com/makiuchi-d/gozxing/common"
+)
+
+func testMask(t *testing.T, mask DataMask, dimension int, condition func(int, int) bool) {
+	bits, _ := common.NewSquareBitMatrix(dimension)
+	mask.UnmaskBitMatrix(bits, dimension)
+	for i := 0; i < dimension; i++ {
+		for j := 0; j < dimension; j++ {
+			if condition(i, j) != bits.Get(j, i) {
+				t.Fatalf("(%v, %v)", i, j)
+			}
+		}
+	}
+}
+
+func testMaskAcrossDimensions(t *testing.T, reference int, condition func(int, int) bool) {
+	mask := DataMaskValues[reference]
+	for version := 1; version <= 40; version++ {
+		dimension := 17 + 4*version
+		testMask(t, mask, dimension, condition)
+	}
+}
+
+func TestMask0(t *testing.T) {
+	testMaskAcrossDimensions(t, 0, func(i, j int) bool {
+		return (i+j)%2 == 0
+	})
+}
+
+func TestMask1(t *testing.T) {
+	testMaskAcrossDimensions(t, 1, func(i, j int) bool {
+		return i%2 == 0
+	})
+}
+
+func TestMask2(t *testing.T) {
+	testMaskAcrossDimensions(t, 2, func(i, j int) bool {
+		return j%3 == 0
+	})
+}
+
+func TestMask3(t *testing.T) {
+	testMaskAcrossDimensions(t, 3, func(i, j int) bool {
+		return (i+j)%3 == 0
+	})
+}
+
+func TestMask4(t *testing.T) {
+	testMaskAcrossDimensions(t, 4, func(i, j int) bool {
+		return (i/2+j/3)%2 == 0
+	})
+}
+
+func TestMask5(t *testing.T) {
+	testMaskAcrossDimensions(t, 5, func(i, j int) bool {
+		return (i*j)%2+(i*j)%3 == 0
+	})
+}
+
+func TestMask6(t *testing.T) {
+	testMaskAcrossDimensions(t, 6, func(i, j int) bool {
+		return ((i*j)%2+(i*j)%3)%2 == 0
+	})
+}
+
+func TestMask7(t *testing.T) {
+	testMaskAcrossDimensions(t, 7, func(i, j int) bool {
+		return ((i+j)%2+(i*j)%3)%2 == 0
+	})
+}
