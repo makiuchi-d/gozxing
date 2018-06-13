ef9bdd7c2c6cfc19f8c6d361714cda7f2098e28c
makki_d <d.makiuchi@gmail.com>
2018-06-13 09:51:13 +0900

    implement qrcode.decoder.Mode

---
+++ qrcode/decoder/mode.go
@@ -0,0 +1,73 @@
+package decoder
+
+import (
+	"errors"
+)
+
+type Mode struct {
+	characterCountBitsForVersions []int
+	bits                          int
+}
+
+var (
+	Mode_TERMINATOR           = NewMode([]int{0, 0, 0}, 0x00) // Not really a mode...
+	Mode_NUMERIC              = NewMode([]int{10, 12, 14}, 0x01)
+	Mode_ALPHANUMERIC         = NewMode([]int{9, 11, 13}, 0x02)
+	Mode_STRUCTURED_APPEND    = NewMode([]int{0, 0, 0}, 0x03) // Not supported
+	Mode_BYTE                 = NewMode([]int{8, 16, 16}, 0x04)
+	Mode_ECI                  = NewMode([]int{0, 0, 0}, 0x07) // character counts don't apply
+	Mode_KANJI                = NewMode([]int{8, 10, 12}, 0x08)
+	Mode_FNC1_FIRST_POSITION  = NewMode([]int{0, 0, 0}, 0x05)
+	Mode_FNC1_SECOND_POSITION = NewMode([]int{0, 0, 0}, 0x09)
+	/** See GBT 18284-2000; "Hanzi" is a transliteration of this mode name. */
+	Mode_HANZI = NewMode([]int{8, 10, 12}, 0x0D)
+)
+
+func NewMode(characterCountBitsForVersions []int, bits int) *Mode {
+	return &Mode{characterCountBitsForVersions, bits}
+}
+
+func ModeForBits(bits int) (*Mode, error) {
+	switch bits {
+	case 0x0:
+		return Mode_TERMINATOR, nil
+	case 0x1:
+		return Mode_NUMERIC, nil
+	case 0x2:
+		return Mode_ALPHANUMERIC, nil
+	case 0x3:
+		return Mode_STRUCTURED_APPEND, nil
+	case 0x4:
+		return Mode_BYTE, nil
+	case 0x5:
+		return Mode_FNC1_FIRST_POSITION, nil
+	case 0x7:
+		return Mode_ECI, nil
+	case 0x8:
+		return Mode_KANJI, nil
+	case 0x9:
+		return Mode_FNC1_SECOND_POSITION, nil
+	case 0xD:
+		// 0xD is defined in GBT 18284-2000, may not be supported in foreign country
+		return Mode_HANZI, nil
+	default:
+		return nil, errors.New("IllegalArgumentException")
+	}
+}
+
+func (this *Mode) GetCharacterCountBits(version *Version) int {
+	number := version.GetVersionNumber()
+	var offset int
+	if number <= 9 {
+		offset = 0
+	} else if number <= 26 {
+		offset = 1
+	} else {
+		offset = 2
+	}
+	return this.characterCountBitsForVersions[offset]
+}
+
+func (this *Mode) GetBits() int {
+	return this.bits
+}
---
+++ qrcode/decoder/mode_test.go
@@ -0,0 +1,80 @@
+package decoder
+
+import(
+	"testing"
+)
+
+func testModeForBits(t *testing.T, bits int, expect *Mode) {
+	mode, e := ModeForBits(bits)
+	if e != nil {
+		t.Fatalf("ModeForBits(%v) returns error, %v", bits, e)
+	}
+	if mode != expect {
+		t.Fatalf("ModeForBits(%v) = %v, expect %v", bits, mode, expect)
+	}
+}
+
+func TestModeForBits(t *testing.T){
+	testModeForBits(t, 0, Mode_TERMINATOR)
+	testModeForBits(t, 1, Mode_NUMERIC)
+	testModeForBits(t, 2, Mode_ALPHANUMERIC)
+	testModeForBits(t, 3, Mode_STRUCTURED_APPEND)
+	testModeForBits(t, 4, Mode_BYTE)
+	testModeForBits(t, 5, Mode_FNC1_FIRST_POSITION)
+	testModeForBits(t, 7, Mode_ECI)
+	testModeForBits(t, 8, Mode_KANJI)
+	testModeForBits(t, 9, Mode_FNC1_SECOND_POSITION)
+	testModeForBits(t, 0xD, Mode_HANZI)
+
+	if _, e := ModeForBits(6); e == nil {
+		t.Fatalf("ModeForBits(6) must be error")
+	}
+}
+
+func TestModeNumeric(t *testing.T){
+	mode := Mode_NUMERIC
+	if r:=mode.GetBits(); r != 1 {
+		t.Fatalf("Bits = %v, expect 1", r)
+	}
+
+	ver,_ := Version_GetVersionForNumber(9)
+	if r := mode.GetCharacterCountBits(ver); r != 10 {
+		t.Fatalf("CharacterCountBits(ver=9) = %v, expect 10", r)
+	}
+	ver,_ = Version_GetVersionForNumber(10)
+	if r := mode.GetCharacterCountBits(ver); r != 12 {
+		t.Fatalf("CharacterCountBits(ver=10) = %v, expect 12", r)
+	}
+	ver,_ = Version_GetVersionForNumber(26)
+	if r := mode.GetCharacterCountBits(ver); r != 12 {
+		t.Fatalf("CharacterCountBits(ver=26) = %v, expect 12", r)
+	}
+	ver,_ = Version_GetVersionForNumber(27)
+	if r := mode.GetCharacterCountBits(ver); r != 14 {
+		t.Fatalf("CharacterCountBits(ver=23) = %v, expect 14", r)
+	}
+}
+
+func TestModeAlphaNumeric(t *testing.T){
+	mode := Mode_ALPHANUMERIC
+	if r:=mode.GetBits(); r != 2 {
+		t.Fatalf("Bits = %v, expect 2", r)
+	}
+
+	ver,_ := Version_GetVersionForNumber(9)
+	if r := mode.GetCharacterCountBits(ver); r != 9 {
+		t.Fatalf("CharacterCountBits(ver=9) = %v, expect 9", r)
+	}
+	ver,_ = Version_GetVersionForNumber(10)
+	if r := mode.GetCharacterCountBits(ver); r != 11 {
+		t.Fatalf("CharacterCountBits(ver=10) = %v, expect 11", r)
+	}
+	ver,_ = Version_GetVersionForNumber(26)
+	if r := mode.GetCharacterCountBits(ver); r != 11 {
+		t.Fatalf("CharacterCountBits(ver=26) = %v, expect 11", r)
+	}
+	ver,_ = Version_GetVersionForNumber(27)
+	if r := mode.GetCharacterCountBits(ver); r != 13 {
+		t.Fatalf("CharacterCountBits(ver=23) = %v, expect 13", r)
+	}
+}
