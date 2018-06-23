43d03d3d8530442992180dad8bbc58c012017d7e
makki_d <d.makiuchi@gmail.com>
2018-06-23 10:45:26 +0900

    implement BarcodeFormat

---
+++ barcode_format.go
@@ -0,0 +1,54 @@
+package gozxing
+
+const (
+	/** Aztec 2D barcode format. */
+	BarcodeFormat_AZTEC = iota
+
+	/** CODABAR 1D format. */
+	BarcodeFormat_CODABAR
+
+	/** Code 39 1D format. */
+	BarcodeFormat_CODE_39
+
+	/** Code 93 1D format. */
+	BarcodeFormat_CODE_93
+
+	/** Code 128 1D format. */
+	BarcodeFormat_CODE_128
+
+	/** Data Matrix 2D barcode format. */
+	BarcodeFormat_DATA_MATRIX
+
+	/** EAN-8 1D format. */
+	BarcodeFormat_EAN_8
+
+	/** EAN-13 1D format. */
+	BarcodeFormat_EAN_13
+
+	/** ITF (Interleaved Two of Five) 1D format. */
+	BarcodeFormat_ITF
+
+	/** MaxiCode 2D barcode format. */
+	BarcodeFormat_MAXICODE
+
+	/** PDF417 format. */
+	BarcodeFormat_PDF_417
+
+	/** QR Code 2D barcode format. */
+	BarcodeFormat_QR_CODE
+
+	/** RSS 14 */
+	BarcodeFormat_RSS_14
+
+	/** RSS EXPANDED */
+	BarcodeFormat_RSS_EXPANDED
+
+	/** UPC-A 1D format. */
+	BarcodeFormat_UPC_A
+
+	/** UPC-E 1D format. */
+	BarcodeFormat_UPC_E
+
+	/** UPC/EAN extension format. Not a stand-alone format. */
+	BarcodeFormat_UPC_EAN_EXTENSION
+)
