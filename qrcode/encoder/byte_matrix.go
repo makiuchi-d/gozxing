805f47bba6835ac9078419c83bfd6c79acf4fe77
makki_d <d.makiuchi@gmail.com>
2018-07-07 08:30:20 +0900

    implement qrcode.encoder.ByteMatrix

---
+++ qrcode/encoder/byte_matrix.go
@@ -0,0 +1,69 @@
+package encoder
+
+type ByteMatrix struct {
+	bytes  [][]byte
+	width  int
+	height int
+}
+
+func NewByteMatrix(width, height int) *ByteMatrix {
+	bytes := make([][]byte, height)
+	for i := 0; i < height; i++ {
+		bytes[i] = make([]byte, width)
+	}
+	return &ByteMatrix{bytes, width, height}
+}
+
+func (this *ByteMatrix) GetHeight() int {
+	return this.height
+}
+
+func (this *ByteMatrix) GetWidth() int {
+	return this.width
+}
+
+func (this *ByteMatrix) Get(x, y int) byte {
+	return this.bytes[y][x]
+}
+
+func (this *ByteMatrix) GetArray() [][]byte {
+	return this.bytes
+}
+
+func (this *ByteMatrix) Set(x, y int, value byte) {
+	this.bytes[y][x] = value
+}
+
+func (this *ByteMatrix) SetBool(x, y int, value bool) {
+	if value {
+		this.bytes[y][x] = 1
+	} else {
+		this.bytes[y][x] = 0
+	}
+}
+
+func (this *ByteMatrix) Clear(value byte) {
+	for y := range this.bytes {
+		for x := range this.bytes[y] {
+			this.bytes[y][x] = value
+		}
+	}
+}
+
+func (this *ByteMatrix) String() string {
+	result := make([]byte, 0, 2*(this.width+1)*this.height)
+	for _, row := range this.bytes {
+		for _, b := range row {
+			switch b {
+			case 0:
+				result = append(result, " 0"...)
+			case 1:
+				result = append(result, " 1"...)
+			default:
+				result = append(result, "  "...)
+			}
+		}
+		result = append(result, '\n')
+	}
+	return string(result)
+}
---
+++ qrcode/encoder/byte_matrix_test.go
@@ -0,0 +1,77 @@
+package encoder
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewByteMatrix(t *testing.T) {
+	bm := NewByteMatrix(3, 5)
+
+	if r := bm.GetHeight(); r != 5 {
+		t.Fatalf("GetHeight = %v, expect 5", r)
+	}
+	if r := bm.GetWidth(); r != 3 {
+		t.Fatalf("GetWidth = %v, expect 3", r)
+	}
+	arr := bm.GetArray()
+	expect := [][]byte{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
+	if !reflect.DeepEqual(arr, expect) {
+		t.Fatalf("GetArr = %v, expect %v", arr, expect)
+	}
+}
+
+func TestByteMatrix_SetGet(t *testing.T) {
+	bm := NewByteMatrix(3, 5)
+
+	bm.Set(1, 1, 1)
+	bm.Set(2, 2, 0)
+	bm.SetBool(0, 3, true)
+	bm.SetBool(1, 4, false)
+	if r := bm.Get(0, 0); r != 0 {
+		t.Fatalf("Get(0,0) = %v, expect 0", r)
+	}
+	if r := bm.Get(1, 1); r != 1 {
+		t.Fatalf("Get(1,1) = %v, expect 1", r)
+	}
+	if r := bm.Get(2, 2); r != 0 {
+		t.Fatalf("Get(2,2) = %v, expect 0", r)
+	}
+	if r := bm.Get(0, 3); r != 1 {
+		t.Fatalf("Get(0,3) = %v, expect 1", r)
+	}
+	if r := bm.Get(1, 4); r != 0 {
+		t.Fatalf("Get(1,4) = %v, expect 0", r)
+	}
+
+	arr := bm.GetArray()
+	expect := [][]byte{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}, {1, 0, 0}, {0, 0, 0}}
+	if !reflect.DeepEqual(arr, expect) {
+		t.Fatalf("GetArr = %v, expect %v", arr, expect)
+	}
+}
+
+func TestByteMatrix_Clear(t *testing.T) {
+	bm := NewByteMatrix(3, 2)
+
+	bm.Clear(1)
+	arr := bm.GetArray()
+	expect := [][]byte{{1, 1, 1}, {1, 1, 1}}
+	if !reflect.DeepEqual(arr, expect) {
+		t.Fatalf("GetArr = %v, expect %v", arr, expect)
+	}
+}
+
+func TestByteMatrix_String(t *testing.T) {
+	bm := NewByteMatrix(3, 3)
+	bm.Set(1, 0, 1)
+	bm.Set(1, 1, 2)
+	bm.Set(2, 1, 1)
+	bm.Set(0, 2, 1)
+	bm.Set(1, 2, 1)
+	bm.Set(2, 2, 1)
+	expect := " 0 1 0\n 0   1\n 1 1 1\n"
+	if s := bm.String(); s != expect {
+		t.Fatalf("String:\n%v\nexpect:\n%v", s, expect)
+	}
+}
