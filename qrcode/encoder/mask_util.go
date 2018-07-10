b8de33cdb9c008cc37c708cea2f5926f8e754229
makki_d <d.makiuchi@gmail.com>
2018-07-10 11:28:07 +0900

    implement qrcode.encoder.MaskUtil

---
+++ qrcode/encoder/mask_util.go
@@ -0,0 +1,207 @@
+package encoder
+
+import (
+	"fmt"
+)
+
+const (
+	// Penalty weights from section 6.8.2.1
+	maskUtilN1 = 3
+	maskUtilN2 = 3
+	maskUtilN3 = 40
+	maskUtilN4 = 10
+)
+
+// MaskUtil_applyMaskPenaltyRule1 Apply mask penalty rule 1 and return the penalty.
+// Find repetitive cells with the same color and give penalty to them. Example: 00000 or 11111.
+func MaskUtil_applyMaskPenaltyRule1(matrix *ByteMatrix) int {
+	return applyMaskPenaltyRule1Internal(matrix, true) + applyMaskPenaltyRule1Internal(matrix, false)
+}
+
+
+// MaskUtil_applyMaskPenaltyRule2 Apply mask penalty rule 2 and return the penalty.
+// Find 2x2 blocks with the same color and give penalty to them.
+// This is actually equivalent to the spec's rule, which is to find MxN blocks and give a penalty
+// proportional to (M-1)x(N-1), because this is the number of 2x2 blocks inside such a block.
+func MaskUtil_applyMaskPenaltyRule2(matrix *ByteMatrix) int {
+	penalty := 0
+	array := matrix.GetArray()
+	width := matrix.GetWidth()
+	height := matrix.GetHeight()
+	for y := 0; y < height-1; y++ {
+		arrayY := array[y]
+		for x := 0; x < width-1; x++ {
+			value := arrayY[x]
+			if value == arrayY[x+1] && value == array[y+1][x] && value == array[y+1][x+1] {
+				penalty++
+			}
+		}
+	}
+	return maskUtilN2 * penalty
+}
+
+// MaskUtil_applyMaskPenaltyRule3 Apply mask penalty rule 3 and return the penalty.
+// Find consecutive runs of 1:1:3:1:1:4 starting with black, or 4:1:1:3:1:1 starting with white,
+// and give penalty to them.  If we find patterns like 000010111010000, we give penalty once.
+func MaskUtil_applyMaskPenaltyRule3(matrix *ByteMatrix) int {
+	numPenalties := 0
+	array := matrix.GetArray()
+	width := matrix.GetWidth()
+	height := matrix.GetHeight()
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			arrayY := array[y] // We can at least optimize this access
+			if x+6 < width &&
+				arrayY[x] == 1 &&
+				arrayY[x+1] == 0 &&
+				arrayY[x+2] == 1 &&
+				arrayY[x+3] == 1 &&
+				arrayY[x+4] == 1 &&
+				arrayY[x+5] == 0 &&
+				arrayY[x+6] == 1 &&
+				(isWhiteHorizontal(arrayY, x-4, x) || isWhiteHorizontal(arrayY, x+7, x+11)) {
+				numPenalties++
+			}
+			if y+6 < height &&
+				array[y][x] == 1 &&
+				array[y+1][x] == 0 &&
+				array[y+2][x] == 1 &&
+				array[y+3][x] == 1 &&
+				array[y+4][x] == 1 &&
+				array[y+5][x] == 0 &&
+				array[y+6][x] == 1 &&
+				(isWhiteVertical(array, x, y-4, y) || isWhiteVertical(array, x, y+7, y+11)) {
+				numPenalties++
+			}
+		}
+	}
+	return numPenalties * maskUtilN3
+}
+
+func isWhiteHorizontal(rowArray []byte, from, to int) bool {
+	if from < 0 {
+		from = 0
+	}
+	if to > len(rowArray) {
+		to = len(rowArray)
+	}
+	for i := from; i < to; i++ {
+		if rowArray[i] == 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func isWhiteVertical(array [][]byte, col, from, to int) bool {
+	if from < 0 {
+		from = 0
+	}
+	if to > len(array) {
+		to = len(array)
+	}
+	for i := from; i < to; i++ {
+		if array[i][col] == 1 {
+			return false
+		}
+	}
+	return true
+}
+
+// MaskUtil_applyMaskPenaltyRule4 Apply mask penalty rule 4 and return the penalty.
+// Calculate the ratio of dark cells and give penalty if the ratio is far from 50%.
+// It gives 10 penalty for 5% distance.
+func MaskUtil_applyMaskPenaltyRule4(matrix *ByteMatrix) int {
+	numDarkCells := 0
+	array := matrix.GetArray()
+	width := matrix.GetWidth()
+	height := matrix.GetHeight()
+	for y := 0; y < height; y++ {
+		arrayY := array[y]
+		for x := 0; x < width; x++ {
+			if arrayY[x] == 1 {
+				numDarkCells++
+			}
+		}
+	}
+	numTotalCells := matrix.GetHeight() * matrix.GetWidth()
+	distance := numDarkCells*2 - numTotalCells
+	if distance < 0 {
+		distance = -distance
+	}
+	fivePercentVariances := distance * 10 / numTotalCells
+	return fivePercentVariances * maskUtilN4
+}
+
+// MaskUtil_getDataMaskBit Return the mask bit for "getMaskPattern" at "x" and "y".
+// See 8.8 of JISX0510:2004 for mask pattern conditions.
+func MaskUtil_getDataMaskBit(maskPattern, x, y int) (bool, error) {
+	var intermediate int
+	switch maskPattern {
+	case 0:
+		intermediate = (y + x) & 0x1
+		break
+	case 1:
+		intermediate = y & 0x1
+		break
+	case 2:
+		intermediate = x % 3
+		break
+	case 3:
+		intermediate = (y + x) % 3
+		break
+	case 4:
+		intermediate = ((y / 2) + (x / 3)) & 0x1
+		break
+	case 5:
+		temp := y * x
+		intermediate = (temp & 0x1) + (temp % 3)
+		break
+	case 6:
+		temp := y * x
+		intermediate = ((temp & 0x1) + (temp % 3)) & 0x1
+		break
+	case 7:
+		temp := y * x
+		intermediate = ((temp % 3) + ((y + x) & 0x1)) & 0x1
+		break
+	default:
+		return false, fmt.Errorf("IllegalArgumentException: Invalid mask pattern: %d", maskPattern)
+	}
+	return (intermediate == 0), nil
+}
+
+func applyMaskPenaltyRule1Internal(matrix *ByteMatrix, isHorizontal bool) int {
+	penalty := 0
+	iLimit := matrix.GetWidth()
+	jLimit := matrix.GetHeight()
+	if isHorizontal {
+		iLimit, jLimit = jLimit, iLimit
+	}
+	array := matrix.GetArray()
+	for i := 0; i < iLimit; i++ {
+		numSameBitCells := 0
+		prevBit := -1
+		for j := 0; j < jLimit; j++ {
+			var bit int
+			if isHorizontal {
+				bit = int(array[i][j])
+			} else {
+				bit = int(array[j][i])
+			}
+			if bit == prevBit {
+				numSameBitCells++
+			} else {
+				if numSameBitCells >= 5 {
+					penalty += maskUtilN1 + (numSameBitCells - 5)
+				}
+				numSameBitCells = 1 // Include the cell itself.
+				prevBit = bit
+			}
+		}
+		if numSameBitCells >= 5 {
+			penalty += maskUtilN1 + (numSameBitCells - 5)
+		}
+	}
+	return penalty
+}
---
+++ qrcode/encoder/mask_util_test.go
@@ -0,0 +1,213 @@
+package encoder
+
+import (
+	"testing"
+)
+
+func makeCheckerMatrix(size int) *ByteMatrix {
+	matrix := NewByteMatrix(size, size)
+	for j := 0; j < size; j++ {
+		for i := j % 2; i < size; i += 2 {
+			matrix.SetBool(i, j, true)
+		}
+	}
+	return matrix
+}
+
+func TestMaskUtil_applyMaskPenaltyRule1(t *testing.T) {
+	matrix := makeCheckerMatrix(15)
+
+	for i := 0; i < 5; i++ {
+		matrix.SetBool(i+3, 3, true)
+		matrix.SetBool(i+2, 8, true)
+		matrix.SetBool(3, i+3, true)
+		matrix.SetBool(8, i+1, true)
+
+		matrix.SetBool(i+8, 5, false)
+		matrix.SetBool(i+1, 6, false)
+		matrix.SetBool(10, i+8, false)
+		matrix.SetBool(12, i+6, false)
+		matrix.SetBool(13, i+10, false)
+	}
+
+	p1 := applyMaskPenaltyRule1Internal(matrix, false)
+	if p1 != 23 {
+		t.Fatalf("PenaltyRule1(vertical) = %v, expect 23", p1)
+	}
+
+	p2 := applyMaskPenaltyRule1Internal(matrix, true)
+	if p2 != 14 {
+		t.Fatalf("PenaltyRule1(vertical) = %v, expect 23", p2)
+	}
+
+	if p := MaskUtil_applyMaskPenaltyRule1(matrix); p != (23 + 14) {
+		t.Fatalf("PenaltyRule1 = %v, expect %v", p, 23+14)
+	}
+}
+
+func TestMaskUtil_applyMaskPenaltyRule2(t *testing.T) {
+	matrix := makeCheckerMatrix(15)
+
+	matrix.SetBool(3, 3, false)
+	matrix.SetBool(4, 4, false)
+	matrix.SetBool(10, 4, false)
+	matrix.SetBool(11, 5, false)
+
+	matrix.SetBool(8, 9, true)
+	matrix.SetBool(9, 10, true)
+	matrix.SetBool(4, 7, true)
+	matrix.SetBool(5, 8, true)
+	matrix.SetBool(4, 9, true)
+
+	if p := MaskUtil_applyMaskPenaltyRule2(matrix); p != 5*3 {
+		t.Fatalf("PenaltyRule2 = %v, expect %v", p, 5*3)
+	}
+}
+
+func TestMaskUtil_applyMaskPenaltyRule3(t *testing.T) {
+	matrix := NewByteMatrix(15, 15)
+
+	matrix.SetBool(4, 0, true)
+	matrix.SetBool(6, 0, true)
+	matrix.SetBool(7, 0, true)
+	matrix.SetBool(8, 0, true)
+	matrix.SetBool(10, 0, true)
+
+	matrix.SetBool(8, 14, true)
+	matrix.SetBool(10, 14, true)
+	matrix.SetBool(11, 14, true)
+	matrix.SetBool(12, 14, true)
+	matrix.SetBool(14, 14, true)
+
+	matrix.SetBool(4, 2, true)
+	matrix.SetBool(4, 3, true)
+	matrix.SetBool(4, 4, true)
+	matrix.SetBool(4, 6, true)
+
+	matrix.SetBool(14, 8, true)
+	matrix.SetBool(14, 10, true)
+	matrix.SetBool(14, 11, true)
+	matrix.SetBool(14, 12, true)
+	matrix.SetBool(14, 14, true)
+
+	if p := MaskUtil_applyMaskPenaltyRule3(matrix); p != 4*40 {
+		t.Fatalf("PenaltyRule3 = %v, expect %v", p, 4*40)
+	}
+}
+
+func TestMaskUtil_isWhiteHorizontal(t *testing.T) {
+	arr := []byte{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0}
+
+	if r := isWhiteHorizontal(arr, -1, 4); r != true {
+		t.Fatalf("isWhiteHorizontal(-1,4) must be %v", !r)
+	}
+
+	if r := isWhiteHorizontal(arr, 0, 8); r != false {
+		t.Fatalf("isWhiteHorizontal(0,8) must be %v", !r)
+	}
+
+	if r := isWhiteHorizontal(arr, 8, 14); r != false {
+		t.Fatalf("isWhiteHorizontal(8,14) must be %v", !r)
+	}
+
+	if r := isWhiteHorizontal(arr, 15, 20); r != true {
+		t.Fatalf("isWhiteHorizontal(15,20) must be %v", !r)
+	}
+}
+
+func TestMaskUtil_isWhiteVertical(t *testing.T) {
+	arr := [][]byte{
+		{0, 0, 1},
+		{0, 0, 1},
+		{0, 0, 0},
+		{0, 1, 0},
+		{0, 0, 0},
+	}
+
+	if r := isWhiteVertical(arr, 0, -1, 6); r != true {
+		t.Fatalf("isWhiteVertical(0, -1, 6) must be %v", !r)
+	}
+
+	if r := isWhiteVertical(arr, 1, 0, 3); r != true {
+		t.Fatalf("isWhiteVertical(0, 0, 3) must be %v", !r)
+	}
+
+	if r := isWhiteVertical(arr, 1, 0, 4); r != false {
+		t.Fatalf("isWhiteVertical(0, 0, 3) must be %v", !r)
+	}
+
+	if r := isWhiteVertical(arr, 2, 2, 5); r != true {
+		t.Fatalf("isWhiteVertical(0, 0, 3) must be %v", !r)
+	}
+}
+
+func TestMaskUtil_applyMaskPenaltyRule4(t *testing.T) {
+	matrix := NewByteMatrix(3, 3)
+
+	if p := MaskUtil_applyMaskPenaltyRule4(matrix); p != 100 {
+		t.Fatalf("PenaltyRule4 = %v, expect 100", p)
+	}
+
+	matrix.SetBool(0, 0, true)
+	matrix.SetBool(0, 1, true)
+	matrix.SetBool(0, 2, true)
+	if p := MaskUtil_applyMaskPenaltyRule4(matrix); p != 30 {
+		t.Fatalf("PenaltyRule4 = %v, expect 30", p)
+	}
+
+	matrix.SetBool(1, 0, true)
+	matrix.SetBool(1, 1, true)
+	if p := MaskUtil_applyMaskPenaltyRule4(matrix); p != 10 {
+		t.Fatalf("PenaltyRule4 = %v, expect 10", p)
+	}
+}
+
+func testMaskPattern(t *testing.T, maskPattern int, isMasked func(int, int) bool) {
+	for x := 0; x < 30; x++ {
+		for y := 0; y < 30; y++ {
+			bit, e := MaskUtil_getDataMaskBit(maskPattern, x, y)
+			if e != nil {
+				t.Fatalf("getDataMaskBit(%v,%v,%v) returns error, %v", maskPattern, x, y, e)
+			}
+			if expect := isMasked(x, y); bit != expect {
+				t.Fatalf("getDataMaskBit(%v,%v,%v) = %v, expect %v", maskPattern, x, y, bit, expect)
+			}
+		}
+	}
+}
+
+func TestMaskUtil_getDataMaskBit(t *testing.T) {
+	testMaskPattern(t, 0, func(j, i int) bool {
+		return (i+j)%2 == 0
+	})
+	testMaskPattern(t, 1, func(j, i int) bool {
+		return i%2 == 0
+	})
+	testMaskPattern(t, 2, func(j, i int) bool {
+		return j%3 == 0
+	})
+	testMaskPattern(t, 3, func(j, i int) bool {
+		return (i+j)%3 == 0
+	})
+	testMaskPattern(t, 4, func(j, i int) bool {
+		return (i/2+j/3)%2 == 0
+	})
+	testMaskPattern(t, 5, func(j, i int) bool {
+		return (i*j)%2+(i*j)%3 == 0
+	})
+
+	testMaskPattern(t, 6, func(j, i int) bool {
+		return ((i*j)%2+(i*j)%3)%2 == 0
+	})
+	testMaskPattern(t, 7, func(j, i int) bool {
+		return ((i+j)%2+(i*j)%3)%2 == 0
+	})
+
+	if _, e := MaskUtil_getDataMaskBit(-1, 0, 0); e == nil {
+		t.Fatalf("getDataMaskBit(-1,0,0) must be error")
+	}
+	if _, e := MaskUtil_getDataMaskBit(8, 0, 0); e == nil {
+		t.Fatalf("getDataMaskBit(8,0,0) must be error")
+	}
+
+}
