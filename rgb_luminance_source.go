1efa728e69e6b9a77c3bd05ab9eecce8bb067ae4
makki_d <d.makiuchi@gmail.com>
2018-07-21 09:59:15 +0900

    implement RGBLuminanceSource

---
+++ rgb_luminance_source.go
@@ -0,0 +1,114 @@
+package gozxing
+
+import (
+	"errors"
+	"fmt"
+)
+
+type RGBLuminanceSource struct {
+	LuminanceSourceBase
+	luminances []byte
+	dataWidth  int
+	dataHeight int
+	left       int
+	top        int
+}
+
+func NewRGBLuminanceSource(width, height int, pixels []int) *RGBLuminanceSource {
+	dataWidth := width
+	dataHeight := height
+	left := 0
+	top := 0
+
+	// In order to measure pure decoding speed, we convert the entire image to a greyscale array
+	// up front, which is the same as the Y channel of the YUVLuminanceSource in the real app.
+	//
+	// Total number of pixels suffices, can ignore shape
+	size := width * height
+	luminances := make([]byte, size)
+	for offset := 0; offset < size; offset++ {
+		pixel := pixels[offset]
+		r := (pixel >> 16) & 0xff  // red
+		g2 := (pixel >> 7) & 0x1fe // 2 * green
+		b := pixel & 0xff          // blue
+		// Calculate green-favouring average cheaply
+		luminances[offset] = byte((r + g2 + b) / 4)
+	}
+
+	return &RGBLuminanceSource{
+		LuminanceSourceBase{width, height},
+		luminances,
+		dataWidth,
+		dataHeight,
+		left,
+		top,
+	}
+}
+
+func (this *RGBLuminanceSource) GetRow(y int, row []byte) ([]byte, error) {
+	if y < 0 || y >= this.GetHeight() {
+		return row, fmt.Errorf("IllegalArgumentException: Requested row is outside the image: %d", y)
+	}
+	width := this.GetWidth()
+	if row == nil || len(row) < width {
+		row = make([]byte, width)
+	}
+	offset := (y+this.top)*this.dataWidth + this.left
+	copy(row, this.luminances[offset:offset+width])
+	return row, nil
+}
+
+func (this *RGBLuminanceSource) GetMatrix() []byte {
+	width := this.GetWidth()
+	height := this.GetHeight()
+
+	// If the caller asks for the entire underlying image, save the copy and give them the
+	// original data. The docs specifically warn that result.length must be ignored.
+	if width == this.dataWidth && height == this.dataHeight {
+		return this.luminances
+	}
+
+	area := width * height
+	matrix := make([]byte, area)
+	inputOffset := this.top*this.dataWidth + this.left
+
+	// If the width matches the full width of the underlying data, perform a single copy.
+	if width == this.dataWidth {
+		copy(matrix, this.luminances[inputOffset:inputOffset+area])
+		return matrix
+	}
+
+	// Otherwise copy one cropped row at a time.
+	for y := 0; y < height; y++ {
+		outputOffset := y * width
+		copy(matrix[outputOffset:outputOffset+width], this.luminances[inputOffset:inputOffset+width])
+		inputOffset += this.dataWidth
+	}
+	return matrix
+}
+
+func (this *RGBLuminanceSource) IsCropSupported() bool {
+	return true
+}
+
+func (this *RGBLuminanceSource) Crop(left, top, width, height int) (LuminanceSource, error) {
+	if left+width > this.dataWidth || top+height > this.dataHeight {
+		return nil, errors.New("IllegalArgumentException: Crop rectangle does not fit within image data")
+	}
+	return &RGBLuminanceSource{
+		LuminanceSourceBase: LuminanceSourceBase{width, height},
+		luminances:          this.luminances,
+		dataWidth:           this.dataWidth,
+		dataHeight:          this.dataHeight,
+		left:                this.left + left,
+		top:                 this.top + top,
+	}, nil
+}
+
+func (this *RGBLuminanceSource) Invert() LuminanceSource {
+	return LuminanceSourceInvert(this)
+}
+
+func (this *RGBLuminanceSource) String() string {
+	return LuminanceSourceString(this)
+}
---
+++ rgb_luminance_source_test.go
@@ -0,0 +1,156 @@
+package gozxing
+
+import (
+	"reflect"
+	"testing"
+)
+
+func makeRGBLSource(width, height int) *RGBLuminanceSource {
+	pixels := make([]int, width*height)
+	for h := 0; h < height; h++ {
+		for w := 0; w < width; w++ {
+			pixels[h*width+w] = wh2rgb(w, h, width, height)
+		}
+	}
+	return NewRGBLuminanceSource(width, height, pixels)
+}
+
+func wh2rgb(w, h, width, height int) int {
+	r := (255 * w * 2 / (width - 1)) & 0xff
+	g := (255 * (w + h) / (width + height - 2)) & 0xff
+	b := (255 * h * 2 / (height - 1)) & 0xff
+	return (r << 16) + (g << 8) + b
+}
+
+func rgb2lumina(rgb int) byte {
+	r := (rgb >> 16) & 0xff
+	g := (rgb >> 8) & 0xff
+	b := rgb & 0xff
+	return byte((r + 2*g + b) / 4)
+}
+
+func TestRGBLuminanceSource_GetRow(t *testing.T) {
+	src := makeRGBLSource(10, 10)
+
+	if _, e := src.GetRow(-1, nil); e == nil {
+		t.Fatalf("GetRow(-1) must be error")
+	}
+	if _, e := src.GetRow(10, nil); e == nil {
+		t.Fatalf("GetRow(-1) must be error")
+	}
+
+	row, e := src.GetRow(0, nil)
+	if e != nil {
+		t.Fatalf("GetRow(0) returns error, %v", e)
+	}
+	// r = (255 * w * 2 / 9) & 0xff => 0, 56,113,170,226, 27, 84,140,197,254
+	// g = (255 * w / 18) & 0xff    => 0, 14, 28, 42, 56, 70, 85, 99,113,127
+	// b = 0
+	// (r + 2*g + b) 4
+	expect := []byte{0, 21, 42, 63, 84, 41, 63, 84, 105, 127}
+	if !reflect.DeepEqual(row, expect) {
+		t.Fatalf("GetRow(0) = %v, expect %v", row, expect)
+	}
+
+	row, e = src.GetRow(9, row)
+	if e != nil {
+		t.Fatalf("GetRow(0) returns error, %v", e)
+	}
+	// r = (255 * w * 2 / 9) & 0xff  =>   0, 56,113,170,226, 27, 84,140,197,254
+	// g = (255 * (w+9) / 18) & 0xff => 127,141,155,170,184,198,212,226,240,255
+	// b = 254
+	expect = []byte{127, 148, 169, 191, 212, 169, 190, 211, 232, 254}
+	if !reflect.DeepEqual(row, expect) {
+		t.Fatalf("GetRow(0) = %v, expect %v", row, expect)
+	}
+}
+
+func TestRGBLuminanceSource_GetMatrix(t *testing.T) {
+	src := makeRGBLSource(10, 10)
+	matrix := src.GetMatrix()
+	if len(matrix) != len(src.luminances) {
+		t.Fatalf("GetMatrix len=%d, expect %d", len(matrix), len(src.luminances))
+	}
+	for i := range matrix {
+		w := i % src.dataWidth
+		h := i / src.dataWidth
+		if exp := rgb2lumina(wh2rgb(w, h, 10, 10)); matrix[i] != exp {
+			t.Fatalf("GetMatrix matrix[%v] = %v, expect %v", i, matrix[i], exp)
+		}
+	}
+
+	if !src.IsCropSupported() {
+		t.Fatalf("IsCropSupported must be true")
+	}
+
+	if _, e := src.Crop(1, 0, 10, 10); e == nil {
+		t.Fatalf("Crop must be error")
+	}
+
+	if _, e := src.Crop(0, 0, 10, 11); e == nil {
+		t.Fatalf("Crop must be error")
+	}
+
+	crop, e := src.Crop(0, 2, 10, 6)
+	if e != nil {
+		t.Fatalf("Crop returns error, %v", e)
+	}
+	cmatrix := crop.GetMatrix()
+	if !reflect.DeepEqual(cmatrix, matrix[20:len(matrix)-20]) {
+		t.Fatalf("%v", crop)
+	}
+
+	crop, e = crop.Crop(2, 0, 6, 6)
+	if e != nil {
+		t.Fatalf("Crop returns error, %v", e)
+	}
+	cmatrix = crop.GetMatrix()
+	expect := []byte{
+		84, 105, 127, 84, 105, 126,
+		105, 127, 148, 105, 127, 148,
+		127, 148, 169, 126, 148, 169,
+		84, 105, 126, 84, 105, 126,
+		105, 127, 148, 105, 127, 148,
+		126, 148, 169, 126, 148, 169,
+	}
+	if !reflect.DeepEqual(cmatrix, expect) {
+		t.Fatalf("Cropped matrix = %v, expect %v", cmatrix, expect)
+	}
+}
+
+func TestRGBLuminanceSource_Invert(t *testing.T) {
+	src := makeRGBLSource(10, 10)
+	inv := src.Invert()
+
+	srcm := src.GetMatrix()
+	invm := inv.GetMatrix()
+
+	for y := 0; y < 10; y++ {
+		for x := 0; x < 10; x++ {
+			i := y*10 + x
+			exp := byte(255 - int(srcm[i]))
+			if invm[i] != exp {
+				t.Fatalf("Inverted matrix[%v] = %v, expect %v", i, invm[i], exp)
+			}
+		}
+	}
+}
+
+func TestRGBLuminanceSource_String(t *testing.T) {
+	src := makeRGBLSource(10, 10)
+	str := src.String()
+	expect := "" +
+		"####+##+++\n" +
+		"###++#+++.\n" +
+		"##++++++..\n" +
+		"#+++.++...\n" +
+		"+++..+... \n" +
+		"##++++++..\n" +
+		"#+++.++...\n" +
+		"+++..+... \n" +
+		"++......  \n" +
+		"+... ..   \n"
+	if str != expect {
+		t.Fatalf("String:\n%vexpect:\n%v", str, expect)
+	}
+}
