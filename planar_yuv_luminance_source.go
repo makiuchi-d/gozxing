a571a1cb12756f68c056d5ff53cdf98d15773740
makki_d <d.makiuchi@gmail.com>
2018-07-25 10:31:30 +0900

    implement PlanarYUVLuminanceSource

---
+++ planar_yuv_luminance_source.go
@@ -0,0 +1,143 @@
+package gozxing
+
+import (
+	"errors"
+	"fmt"
+)
+
+const thumbnailScaleFactor = 2
+
+type PlanarYUVLuminanceSource struct {
+	LuminanceSourceBase
+	yuvData    []byte
+	dataWidth  int
+	dataHeight int
+	left       int
+	top        int
+}
+
+func NewPlanarYUVLuminanceSource(yuvData []byte,
+	dataWidth, dataHeight, left, top, width, height int,
+	reverseHorizontal bool) (LuminanceSource, error) {
+
+	if left+width > dataWidth || top+height > dataHeight {
+		return nil, errors.New("IllegalArgumentException: Crop rectangle does not fit within image data")
+	}
+
+	yuvsrc := &PlanarYUVLuminanceSource{
+		LuminanceSourceBase{width, height},
+		yuvData,
+		dataWidth,
+		dataHeight,
+		left,
+		top,
+	}
+	if reverseHorizontal {
+		yuvsrc.reverseHorizontal(width, height)
+	}
+	return yuvsrc, nil
+}
+
+func (this *PlanarYUVLuminanceSource) Invert() LuminanceSource {
+	return LuminanceSourceInvert(this)
+}
+
+func (this *PlanarYUVLuminanceSource) String() string {
+	return LuminanceSourceString(this)
+}
+
+func (this *PlanarYUVLuminanceSource) GetRow(y int, row []byte) ([]byte, error) {
+	if y < 0 || y >= this.GetHeight() {
+		return nil, fmt.Errorf("IllegalArgumentException: Requested row is outside the image: %v", y)
+	}
+	width := this.GetWidth()
+	if row == nil || len(row) < width {
+		row = make([]byte, width)
+	}
+	offset := (y+this.top)*this.dataWidth + this.left
+	copy(row, this.yuvData[offset:offset+width])
+	return row, nil
+}
+
+func (this *PlanarYUVLuminanceSource) GetMatrix() []byte {
+	width := this.GetWidth()
+	height := this.GetHeight()
+
+	// If the caller asks for the entire underlying image, save the copy and give them the
+	// original data. The docs specifically warn that result.length must be ignored.
+	if width == this.dataWidth && height == this.dataHeight {
+		return this.yuvData
+	}
+
+	area := width * height
+	matrix := make([]byte, area)
+	inputOffset := this.top*this.dataWidth + this.left
+
+	// If the width matches the full width of the underlying data, perform a single copy.
+	if width == this.dataWidth {
+		copy(matrix, this.yuvData[inputOffset:inputOffset+area])
+		return matrix
+	}
+
+	// Otherwise copy one cropped row at a time.
+	for y := 0; y < height; y++ {
+		outputOffset := y * width
+		copy(matrix[outputOffset:], this.yuvData[inputOffset:inputOffset+width])
+		inputOffset += this.dataWidth
+	}
+	return matrix
+}
+
+func (this *PlanarYUVLuminanceSource) IsCropSupported() bool {
+	return true
+}
+
+func (this *PlanarYUVLuminanceSource) Crop(left, top, width, height int) (LuminanceSource, error) {
+	return NewPlanarYUVLuminanceSource(
+		this.yuvData,
+		this.dataWidth,
+		this.dataHeight,
+		this.left+left,
+		this.top+top,
+		width,
+		height,
+		false)
+}
+
+func (this *PlanarYUVLuminanceSource) RenderThumbnail() []int {
+	width := this.GetThumbnailWidth()
+	height := this.GetThumbnailHeight()
+	pixels := make([]int, width*height)
+	yuv := this.yuvData
+	inputOffset := this.top*this.dataWidth + this.left
+
+	for y := 0; y < height; y++ {
+		outputOffset := y * width
+		for x := 0; x < width; x++ {
+			grey := int(yuv[inputOffset+x*thumbnailScaleFactor]) & 0xff
+			pixels[outputOffset+x] = 0xFF000000 | (grey * 0x00010101)
+		}
+		inputOffset += this.dataWidth * thumbnailScaleFactor
+	}
+	return pixels
+}
+
+// GetThumbnailWidth return width of image from {@link #renderThumbnail()}
+func (this *PlanarYUVLuminanceSource) GetThumbnailWidth() int {
+	return this.GetWidth() / thumbnailScaleFactor
+}
+
+// GetThumbnailHeight return height of image from {@link #renderThumbnail()}
+func (this *PlanarYUVLuminanceSource) GetThumbnailHeight() int {
+	return this.GetHeight() / thumbnailScaleFactor
+}
+
+func (this *PlanarYUVLuminanceSource) reverseHorizontal(width, height int) {
+	yuvData := this.yuvData
+	for y, rowStart := 0, this.top*this.dataWidth+this.left; y < height; y, rowStart = y+1, rowStart+this.dataWidth {
+		middle := rowStart + width/2
+		for x1, x2 := rowStart, rowStart+width-1; x1 < middle; x1, x2 = x1+1, x2-1 {
+			yuvData[x1], yuvData[x2] = yuvData[x2], yuvData[x1]
+		}
+	}
+}
---
+++ planar_yuv_luminance_source_test.go
@@ -0,0 +1,165 @@
+package gozxing
+
+import (
+	"reflect"
+	"testing"
+)
+
+func makeYUVSource(w, h int) *PlanarYUVLuminanceSource {
+	pixels := make([]byte, w*h*2)
+	for j := 0; j < h; j++ {
+		for i := 0; i < w; i++ {
+			pixels[j*w+i] = byte((i + j) * 255 / (w + h - 2))
+		}
+	}
+
+	src, _ := NewPlanarYUVLuminanceSource(pixels, w, h, 0, 0, w, h, false)
+	return src.(*PlanarYUVLuminanceSource)
+}
+
+func TestNewPlanarYUVLuminanceSource(t *testing.T) {
+	_, e := NewPlanarYUVLuminanceSource(nil, 10, 10, 5, 5, 10, 10, false)
+	if e == nil {
+		t.Fatalf("NewPlanarYUVLuminanceSource must be error")
+	}
+
+	src, e := NewPlanarYUVLuminanceSource(
+		[]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, 5, 2, 0, 0, 5, 2, true)
+	if e != nil {
+		t.Fatalf("NewPlanarYUVLuminanceSource returns error, %v", e)
+	}
+	yuvsrc, ok := src.(*PlanarYUVLuminanceSource)
+	if !ok {
+		t.Fatalf("src is not *PlanarYUVLuminanceSource, %T", src)
+	}
+	expect := []byte{5, 4, 3, 2, 1, 0, 9, 8, 7, 6}
+	if !reflect.DeepEqual(yuvsrc.yuvData, expect) {
+		t.Fatalf("yuvData = %v, expect %v", yuvsrc.yuvData, expect)
+	}
+}
+
+func TestPlanarYUVLuminanceSource_GetRow(t *testing.T) {
+	src := makeYUVSource(10, 10)
+
+	if _, e := src.GetRow(-1, nil); e == nil {
+		t.Fatalf("GetRow must be error")
+	}
+	if _, e := src.GetRow(10, nil); e == nil {
+		t.Fatalf("GetRow must be error")
+	}
+
+	row, e := src.GetRow(5, nil)
+	if e != nil {
+		t.Fatalf("GetRow returns error, %v", e)
+	}
+	expect := make([]byte, 10)
+	for i := 0; i < 10; i++ {
+		expect[i] = byte((5 + i) * 255 / 18)
+	}
+	if !reflect.DeepEqual(row, expect) {
+		t.Fatalf("GetRow = %v, expect %v", row, expect)
+	}
+}
+
+func TestPlanarYUVLuminanceSource_IsCropSupported(t *testing.T) {
+	src := makeYUVSource(10, 10)
+	if !src.IsCropSupported() {
+		t.Fatalf("IsCropSupported must be true")
+	}
+}
+
+func TestPlanarYUVLuminanceSource_GetCroppedMatrix(t *testing.T) {
+	src := makeYUVSource(10, 10)
+
+	matrix := src.GetMatrix()
+	if !reflect.DeepEqual(matrix, src.yuvData) {
+		t.Fatalf("GetMatrix:\n%v\nexpect:\n%v", matrix, src.yuvData)
+	}
+
+	_, e := src.Crop(8, 8, 8, 8)
+	if e == nil {
+		t.Fatalf("Crop(8,8,8,8) must be error")
+	}
+
+	crop, e := src.Crop(0, 3, 10, 5)
+	if e != nil {
+		t.Fatalf("Crop returns error, %v", e)
+	}
+	matrix = crop.GetMatrix()
+	expect := make([]byte, 10*5)
+	for j := 0; j < 5; j++ {
+		for i := 0; i < 10; i++ {
+			expect[j*10+i] = byte((i + j + 3) * 255 / 18)
+		}
+	}
+	if !reflect.DeepEqual(matrix, expect) {
+		t.Fatalf("GetMatrix:\n%v\nexpect:\n%v", matrix, expect)
+	}
+
+	crop2, e := crop.Crop(3, 0, 5, 5)
+	if e != nil {
+		t.Fatalf("Crop returns error, %v", e)
+	}
+	matrix = crop2.GetMatrix()
+	expect = make([]byte, 5*5)
+	for j := 0; j < 5; j++ {
+		for i := 0; i < 5; i++ {
+			expect[j*5+i] = byte((i + j + 6) * 255 / 18)
+		}
+	}
+	if !reflect.DeepEqual(matrix, expect) {
+		t.Fatalf("GetMatrix:\n%v\nexpect:\n%v", matrix, expect)
+	}
+}
+
+func TestPlanarYUVLuminanceSource_ReaderThumbnail(t *testing.T) {
+	src := makeYUVSource(10, 10)
+
+	thumb := src.RenderThumbnail()
+	expect := make([]int, 5*5)
+	for j := 0; j < 5; j++ {
+		for i := 0; i < 5; i++ {
+			y := (i + j) * 2 * 255 / 18
+			expect[j*5+i] = 0xff000000 | y*0x00010101
+		}
+	}
+	if !reflect.DeepEqual(thumb, expect) {
+		t.Fatalf("\n%v\n%v", thumb, expect)
+	}
+}
+
+func TestPlanarYUVLuminanceSource_Invert(t *testing.T) {
+	src := makeYUVSource(10, 10)
+	inv := src.Invert()
+
+	smat := src.GetMatrix()
+	imat := inv.GetMatrix()
+
+	if len(smat) < 100 || len(imat) < 100 {
+		t.Fatalf("matrix length is not enough, smat=%v, imat=%v", len(smat), len(imat))
+	}
+	for i := 0; i < 100; i++ {
+		if imat[i] != 255-smat[i] {
+			t.Fatalf("inverted matrix[%v] = %v, expect %v", i, imat[i], 255-smat[i])
+		}
+	}
+}
+
+func TestPlanarYUVLuminanceSource_String(t *testing.T) {
+	src := makeYUVSource(10, 10)
+	str := src.String()
+	expect := "" +
+		"#####+++++\n" +
+		"####+++++.\n" +
+		"###+++++..\n" +
+		"##+++++...\n" +
+		"#+++++....\n" +
+		"+++++.... \n" +
+		"++++....  \n" +
+		"+++....   \n" +
+		"++....    \n" +
+		"+....     \n"
+	if str != expect {
+		t.Fatalf("String:\n%v\nexpect:\n%v", str, expect)
+	}
+}
