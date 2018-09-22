d59498a5b31aaaf4b9917a802e43941a170f4ddc
makki_d <d.makiuchi@gmail.com>
2018-09-22 23:08:00 +0900

    implement datamatrix.decoder.DataBlock

---
+++ datamatrix/decoder/data_block.go
@@ -0,0 +1,108 @@
+package decoder
+
+import (
+	"errors"
+)
+
+// DataBlock Encapsulates a block of data within a Data Matrix Code.
+// Data Matrix Codes may split their data into multiple blocks,
+// each of which is a unit of data and error-correction codewords.
+// Each is represented by an instance of this class.
+type DataBlock struct {
+	numDataCodewords int
+	codewords        []byte
+}
+
+// DataBlocks_getDataBlocks When Data Matrix Codes use multiple data blocks,
+// they actually interleave the bytes of each of them.
+// That is, the first byte of data block 1 to n is written, then the second bytes, and so on. This
+// method will separate the data into original blocks.
+//
+// @param rawCodewords bytes as read directly from the Data Matrix Code
+// @param version version of the Data Matrix Code
+// @return DataBlocks containing original bytes, "de-interleaved" from representation in the Data Matrix Code
+//
+func DataBlocks_getDataBlocks(rawCodewords []byte, version *Version) ([]DataBlock, error) {
+	// Figure out the number and size of data blocks used by this version
+	ecBlocks := version.getECBlocks()
+
+	// First count the total number of data blocks
+	totalBlocks := 0
+	ecBlockArray := ecBlocks.getECBlocks()
+	for _, ecBlock := range ecBlockArray {
+		totalBlocks += ecBlock.getCount()
+	}
+
+	// Now establish DataBlocks of the appropriate size and number of data codewords
+	result := make([]DataBlock, totalBlocks)
+	numResultBlocks := 0
+	for _, ecBlock := range ecBlockArray {
+		for i := 0; i < ecBlock.getCount(); i++ {
+			numDataCodewords := ecBlock.getDataCodewords()
+			numBlockCodewords := ecBlocks.getECCodewords() + numDataCodewords
+			result[numResultBlocks].numDataCodewords = numDataCodewords
+			result[numResultBlocks].codewords = make([]byte, numBlockCodewords)
+			numResultBlocks++
+		}
+	}
+
+	// All blocks have the same amount of data, except that the last n
+	// (where n may be 0) have 1 less byte. Figure out where these start.
+	// TODO(bbrown): There is only one case where there is a difference for Data Matrix for size 144
+	longerBlocksTotalCodewords := len(result[0].codewords)
+	// shorterBlocksTotalCodewords := longerBlocksTotalCodewords - 1
+
+	longerBlocksNumDataCodewords := longerBlocksTotalCodewords - ecBlocks.getECCodewords()
+	shorterBlocksNumDataCodewords := longerBlocksNumDataCodewords - 1
+	// The last elements of result may be 1 element shorter for 144 matrix
+	// first fill out as many elements as all of them have minus 1
+	rawCodewordsOffset := 0
+	for i := 0; i < shorterBlocksNumDataCodewords; i++ {
+		for j := 0; j < numResultBlocks; j++ {
+			result[j].codewords[i] = rawCodewords[rawCodewordsOffset]
+			rawCodewordsOffset++
+		}
+	}
+
+	// Fill out the last data block in the longer ones
+	specialVersion := version.getVersionNumber() == 24
+	numLongerBlocks := numResultBlocks
+	if specialVersion {
+		numLongerBlocks = 8
+	}
+	for j := 0; j < numLongerBlocks; j++ {
+		result[j].codewords[longerBlocksNumDataCodewords-1] = rawCodewords[rawCodewordsOffset]
+		rawCodewordsOffset++
+	}
+
+	// Now add in error correction blocks
+	max := len(result[0].codewords)
+	for i := longerBlocksNumDataCodewords; i < max; i++ {
+		for j := 0; j < numResultBlocks; j++ {
+			jOffset := j
+			iOffset := i
+			if specialVersion {
+				jOffset = (j + 8) % numResultBlocks
+				if jOffset > 7 {
+					iOffset = i - 1
+				}
+			}
+			result[jOffset].codewords[iOffset] = rawCodewords[rawCodewordsOffset]
+			rawCodewordsOffset++
+		}
+	}
+
+	if rawCodewordsOffset != len(rawCodewords) {
+		return nil, errors.New("IllegalArgumentException")
+	}
+
+	return result, nil
+}
+
+func (d *DataBlock) getNumDataCodewords() int {
+	return d.numDataCodewords
+}
+
+func (d *DataBlock) getCodewords() []byte {
+	return d.codewords
+}
---
+++ datamatrix/decoder/data_block_test.go
@@ -0,0 +1,81 @@
+package decoder
+
+import (
+	"testing"
+)
+
+func TestGetDataBlocks(t *testing.T) {
+	version, _ := Version_getVersionForDimensions(10, 10)
+	rawCW := make([]byte, version.getTotalCodewords()+10)
+	_, e := DataBlocks_getDataBlocks(rawCW, version)
+	if e == nil {
+		t.Fatalf("getDataBlocks must be error")
+	}
+
+	// version 1
+	version, _ = Version_getVersionForDimensions(10, 10)
+	rawCW = make([]byte, version.getTotalCodewords())
+	dbs, e := DataBlocks_getDataBlocks(rawCW, version)
+	if e != nil {
+		t.Fatalf("getDataBlocks(ver1) returns error, %v", e)
+	}
+	if r := len(dbs); r != 1 {
+		t.Fatalf("getDataBlocks(ver1) len = %v, expect 1", r)
+	}
+	for _, d := range dbs {
+		if r := d.getNumDataCodewords(); r != 3 {
+			t.Fatalf("getDataBlocks(ver1) numDataCodewords = %v, expect 3", r)
+		}
+		if r := d.getCodewords(); len(r) != 8 {
+			t.Fatalf("getDataBlocks(ver1) len(codewords) = %v, expect 8", len(r))
+		}
+	}
+
+	// version 15
+	version, _ = Version_getVersionForDimensions(52, 52)
+	rawCW = make([]byte, version.getTotalCodewords())
+	dbs, e = DataBlocks_getDataBlocks(rawCW, version)
+	if e != nil {
+		t.Fatalf("getDataBlocks(ver1) returns error, %v", e)
+	}
+	if r := len(dbs); r != 2 {
+		t.Fatalf("getDataBlocks(ver1) len = %v, expect 2", r)
+	}
+	for _, d := range dbs {
+		if r := d.getNumDataCodewords(); r != 102 {
+			t.Fatalf("getDataBlocks(ver1) numDataCodewords = %v, expect 102", r)
+		}
+		if r := d.getCodewords(); len(r) != 144 {
+			t.Fatalf("getDataBlocks(ver1) len(codewords) = %v, expect 144", len(r))
+		}
+	}
+
+	// version 24 (special version)
+	version, _ = Version_getVersionForDimensions(144, 144)
+	rawCW = make([]byte, version.getTotalCodewords())
+	dbs, e = DataBlocks_getDataBlocks(rawCW, version)
+	if e != nil {
+		t.Fatalf("getDataBlocks(ver1) returns error, %v", e)
+	}
+	if r := len(dbs); r != 8+2 {
+		t.Fatalf("getDataBlocks(ver1) len = %v, expect 10", r)
+	}
+	for i := 0; i < 8; i++ {
+		d := dbs[i]
+		if r := d.getNumDataCodewords(); r != 156 {
+			t.Fatalf("getDataBlocks(ver1) numDataCodewords = %v, expect 3", r)
+		}
+		if r := d.getCodewords(); len(r) != 62+156 {
+			t.Fatalf("getDataBlocks(ver1) len(codewords) = %v, expect 62+156", len(r))
+		}
+	}
+	for i := 8; i < 10; i++ {
+		d := dbs[i]
+		if r := d.getNumDataCodewords(); r != 155 {
+			t.Fatalf("getDataBlocks(ver1) numDataCodewords = %v, expect 155", r)
+		}
+		if r := d.getCodewords(); len(r) != 62+155 {
+			t.Fatalf("getDataBlocks(ver1) len(codewords) = %v, expect 62+155", len(r))
+		}
+	}
+}
