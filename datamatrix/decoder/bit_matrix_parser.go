46b65a91a668527ab9afd7e14d752e978736efb6
makki_d <d.makiuchi@gmail.com>
2018-09-28 08:20:04 +0900

    implement datamatrix.decoder.BitMatrixParser

---
+++ datamatrix/decoder/bit_matrix_parser.go
@@ -0,0 +1,431 @@
+package decoder
+
+import (
+	"errors"
+
+	"github.com/makiuchi-d/gozxing"
+)
+
+type BitMatrixParser struct {
+	mappingBitMatrix  *gozxing.BitMatrix
+	readMappingMatrix *gozxing.BitMatrix
+	version           *Version
+}
+
+// NewBitMatrixParser construct parser
+// @param bitMatrix {@link BitMatrix} to parse
+// @throws FormatException if dimension is < 8 or > 144 or not 0 mod 2
+func NewBitMatrixParser(bitMatrix *gozxing.BitMatrix) (*BitMatrixParser, error) {
+	dimension := bitMatrix.GetHeight()
+	if dimension < 8 || dimension > 144 || (dimension&0x01) != 0 {
+		return nil, gozxing.GetFormatExceptionInstance()
+	}
+
+	version, e := readVersion(bitMatrix)
+	if e != nil {
+		return nil, e
+	}
+	mappingBitMatrix, _ := extractDataRegion(version, bitMatrix)
+	readMappingMatrix, _ := gozxing.NewBitMatrix(mappingBitMatrix.GetWidth(), mappingBitMatrix.GetHeight())
+	return &BitMatrixParser{
+		mappingBitMatrix:  mappingBitMatrix,
+		readMappingMatrix: readMappingMatrix,
+		version:           version,
+	}, nil
+}
+
+func (p *BitMatrixParser) GetVersion() *Version {
+	return p.version
+}
+
+// readVersion Creates the version object based on the dimension of the original bit matrix from
+// the datamatrix code.
+//
+// See ISO 16022:2006 Table 7 - ECC 200 symbol attributes
+//
+// @param bitMatrix Original {@link BitMatrix} including alignment patterns
+// @return {@link Version} encapsulating the Data Matrix Code's "version"
+// @throws FormatException if the dimensions of the mapping matrix are not valid
+// Data Matrix dimensions.
+func readVersion(bitMatrix *gozxing.BitMatrix) (*Version, error) {
+	numRows := bitMatrix.GetHeight()
+	numColumns := bitMatrix.GetWidth()
+	return getVersionForDimensions(numRows, numColumns)
+}
+
+// readCodewords Reads the bits in the BitMatrix representing the mapping matrix (No alignment patterns)
+// in the correct order in order to reconstitute the codewords bytes contained within the
+// Data Matrix Code.
+//
+// @return bytes encoded within the Data Matrix Code
+// @throws FormatException if the exact number of bytes expected is not read
+func (p *BitMatrixParser) readCodewords() ([]byte, error) {
+
+	result := make([]byte, p.version.getTotalCodewords())
+	resultOffset := 0
+
+	row := 4
+	column := 0
+
+	numRows := p.mappingBitMatrix.GetHeight()
+	numColumns := p.mappingBitMatrix.GetWidth()
+
+	corner1Read := false
+	corner2Read := false
+	corner3Read := false
+	corner4Read := false
+
+	// Read all of the codewords
+	for {
+		// Check the four corner cases
+		if (row == numRows) && (column == 0) && !corner1Read {
+			result[resultOffset] = p.readCorner1(numRows, numColumns)
+			resultOffset++
+			row -= 2
+			column += 2
+			corner1Read = true
+		} else if (row == numRows-2) && (column == 0) && ((numColumns & 0x03) != 0) && !corner2Read {
+			result[resultOffset] = p.readCorner2(numRows, numColumns)
+			resultOffset++
+			row -= 2
+			column += 2
+			corner2Read = true
+		} else if (row == numRows+4) && (column == 2) && ((numColumns & 0x07) == 0) && !corner3Read {
+			result[resultOffset] = p.readCorner3(numRows, numColumns)
+			resultOffset++
+			row -= 2
+			column += 2
+			corner3Read = true
+		} else if (row == numRows-2) && (column == 0) && ((numColumns & 0x07) == 4) && !corner4Read {
+			result[resultOffset] = p.readCorner4(numRows, numColumns)
+			resultOffset++
+			row -= 2
+			column += 2
+			corner4Read = true
+		} else {
+			// Sweep upward diagonally to the right
+			for {
+				if (row < numRows) && (column >= 0) && !p.readMappingMatrix.Get(column, row) {
+					result[resultOffset] = p.readUtah(row, column, numRows, numColumns)
+					resultOffset++
+				}
+				row -= 2
+				column += 2
+				if !((row >= 0) && (column < numColumns)) {
+					break
+				}
+			}
+			row += 1
+			column += 3
+
+			// Sweep downward diagonally to the left
+			for {
+				if (row >= 0) && (column < numColumns) && !p.readMappingMatrix.Get(column, row) {
+					result[resultOffset] = p.readUtah(row, column, numRows, numColumns)
+					resultOffset++
+				}
+				row += 2
+				column -= 2
+				if !((row < numRows) && (column >= 0)) {
+					break
+				}
+			}
+			row += 3
+			column += 1
+		}
+
+		if !((row < numRows) || (column < numColumns)) {
+			break
+		}
+	}
+
+	if resultOffset != p.version.getTotalCodewords() {
+		return nil, gozxing.GetFormatExceptionInstance()
+	}
+	return result, nil
+}
+
+// readModule Reads a bit of the mapping matrix accounting for boundary wrapping.
+//
+// @param row Row to read in the mapping matrix
+// @param column Column to read in the mapping matrix
+// @param numRows Number of rows in the mapping matrix
+// @param numColumns Number of columns in the mapping matrix
+// @return value of the given bit in the mapping matrix
+func (p *BitMatrixParser) readModule(row, column, numRows, numColumns int) bool {
+	// Adjust the row and column indices based on boundary wrapping
+	if row < 0 {
+		row += numRows
+		column += 4 - ((numRows + 4) & 0x07)
+	}
+	if column < 0 {
+		column += numColumns
+		row += 4 - ((numColumns + 4) & 0x07)
+	}
+	p.readMappingMatrix.Set(column, row)
+	return p.mappingBitMatrix.Get(column, row)
+}
+
+// readUtah Reads the 8 bits of the standard Utah-shaped pattern.
+//
+// See ISO 16022:2006, 5.8.1 Figure 6
+//
+// @param row Current row in the mapping matrix, anchored at the 8th bit (LSB) of the pattern
+// @param column Current column in the mapping matrix, anchored at the 8th bit (LSB) of the pattern
+// @param numRows Number of rows in the mapping matrix
+// @param numColumns Number of columns in the mapping matrix
+// @return byte from the utah shape
+//
+func (p *BitMatrixParser) readUtah(row, column, numRows, numColumns int) byte {
+	currentByte := byte(0)
+	if p.readModule(row-2, column-2, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(row-2, column-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(row-1, column-2, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(row-1, column-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(row-1, column, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(row, column-2, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(row, column-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(row, column, numRows, numColumns) {
+		currentByte |= 1
+	}
+	return currentByte
+}
+
+// readCorner1Reads the 8 bits of the special corner condition 1.
+//
+// See ISO 16022:2006, Figure F.3
+//
+// @param numRows Number of rows in the mapping matrix
+// @param numColumns Number of columns in the mapping matrix
+// @return byte from the Corner condition 1
+//
+func (p *BitMatrixParser) readCorner1(numRows, numColumns int) byte {
+	currentByte := byte(0)
+	if p.readModule(numRows-1, 0, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(numRows-1, 1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(numRows-1, 2, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-2, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(1, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(2, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(3, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	return currentByte
+}
+
+// readCorner2 Reads the 8 bits of the special corner condition 2.
+//
+// See ISO 16022:2006, Figure F.4
+//
+// @param numRows Number of rows in the mapping matrix
+// @param numColumns Number of columns in the mapping matrix
+// @return byte from the Corner condition 2
+func (p *BitMatrixParser) readCorner2(numRows, numColumns int) byte {
+	currentByte := byte(0)
+	if p.readModule(numRows-3, 0, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(numRows-2, 0, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(numRows-1, 0, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-4, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-3, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-2, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(1, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	return currentByte
+}
+
+// readCorner3 Reads the 8 bits of the special corner condition 3.
+//
+// See ISO 16022:2006, Figure F.5
+//
+// @param numRows Number of rows in the mapping matrix
+// @param numColumns Number of columns in the mapping matrix
+// @return byte from the Corner condition 3
+//
+func (p *BitMatrixParser) readCorner3(numRows, numColumns int) byte {
+	currentByte := byte(0)
+	if p.readModule(numRows-1, 0, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(numRows-1, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-3, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-2, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(1, numColumns-3, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(1, numColumns-2, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(1, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	return currentByte
+}
+
+// readCorner4 <p>Reads the 8 bits of the special corner condition 4.</p>
+//
+// See ISO 16022:2006, Figure F.6
+//
+// @param numRows Number of rows in the mapping matrix
+// @param numColumns Number of columns in the mapping matrix
+// @return byte from the Corner condition 4
+//
+func (p *BitMatrixParser) readCorner4(numRows, numColumns int) byte {
+	currentByte := byte(0)
+	if p.readModule(numRows-3, 0, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(numRows-2, 0, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(numRows-1, 0, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-2, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(0, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(1, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(2, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	currentByte <<= 1
+	if p.readModule(3, numColumns-1, numRows, numColumns) {
+		currentByte |= 1
+	}
+	return currentByte
+}
+
+// extractDataRegion Extracts the data region from a {@link BitMatrix} that contains alignment patterns.
+//
+// @param bitMatrix Original {@link BitMatrix} with alignment patterns
+// @return BitMatrix that has the alignment patterns removed
+//
+func extractDataRegion(version *Version, bitMatrix *gozxing.BitMatrix) (*gozxing.BitMatrix, error) {
+	symbolSizeRows := version.getSymbolSizeRows()
+	symbolSizeColumns := version.getSymbolSizeColumns()
+
+	if bitMatrix.GetHeight() != symbolSizeRows {
+		return nil, errors.New("IllegalArgumentException: Dimension of bitMatrix must match the version size")
+	}
+
+	dataRegionSizeRows := version.getDataRegionSizeRows()
+	dataRegionSizeColumns := version.getDataRegionSizeColumns()
+
+	numDataRegionsRow := symbolSizeRows / dataRegionSizeRows
+	numDataRegionsColumn := symbolSizeColumns / dataRegionSizeColumns
+
+	sizeDataRegionRow := numDataRegionsRow * dataRegionSizeRows
+	sizeDataRegionColumn := numDataRegionsColumn * dataRegionSizeColumns
+
+	bitMatrixWithoutAlignment, _ := gozxing.NewBitMatrix(sizeDataRegionColumn, sizeDataRegionRow)
+	for dataRegionRow := 0; dataRegionRow < numDataRegionsRow; dataRegionRow++ {
+		dataRegionRowOffset := dataRegionRow * dataRegionSizeRows
+		for dataRegionColumn := 0; dataRegionColumn < numDataRegionsColumn; dataRegionColumn++ {
+			dataRegionColumnOffset := dataRegionColumn * dataRegionSizeColumns
+			for i := 0; i < dataRegionSizeRows; i++ {
+				readRowOffset := dataRegionRow*(dataRegionSizeRows+2) + 1 + i
+				writeRowOffset := dataRegionRowOffset + i
+				for j := 0; j < dataRegionSizeColumns; j++ {
+					readColumnOffset := dataRegionColumn*(dataRegionSizeColumns+2) + 1 + j
+					if bitMatrix.Get(readColumnOffset, readRowOffset) {
+						writeColumnOffset := dataRegionColumnOffset + j
+						bitMatrixWithoutAlignment.Set(writeColumnOffset, writeRowOffset)
+					}
+				}
+			}
+		}
+	}
+	return bitMatrixWithoutAlignment, nil
+}
---
+++ datamatrix/decoder/bit_matrix_parser_test.go
@@ -0,0 +1,335 @@
+package decoder
+
+import (
+	"testing"
+
+	"github.com/makiuchi-d/gozxing"
+	"reflect"
+)
+
+const (
+	dm4str = "" +
+		"##  ##  ##  ##  ##  ##  ##  ##  \n" +
+		"##  ####  ####  ##  ####      ##\n" +
+		"####      ####  ####            \n" +
+		"######  ####  ##    ####      ##\n" +
+		"####                ##########  \n" +
+		"######      ##    ##        ####\n" +
+		"####    ####          ##        \n" +
+		"######  ####  ##    ##        ##\n" +
+		"##    ##  ##############        \n" +
+		"##  ####  ##    ######  ####  ##\n" +
+		"######  ##    ##      ######    \n" +
+		"##  ####  ######        ####  ##\n" +
+		"##    ##  ######  ##  ######    \n" +
+		"########    ##  ##        ##  ##\n" +
+		"############  ##  ######    ##  \n" +
+		"################################\n"
+	dm5str = "" +
+		"##  ##  ##  ##  ##  ##  ##  ##  ##  \n" +
+		"##########      ####  ####        ##\n" +
+		"##    ##  ##########    ####    ##  \n" +
+		"######  ######        ####  ########\n" +
+		"##    ##  ########  ##  ##  ####    \n" +
+		"##  ######  ##  ##        ##########\n" +
+		"######  ##    ##    ##  ####        \n" +
+		"######        ##  ####            ##\n" +
+		"####  ##  ####  ##    ##  ##        \n" +
+		"####  ##              ##    ########\n" +
+		"######    ##        ####            \n" +
+		"##  ####  ######  ##########      ##\n" +
+		"####  ######    ##      ####        \n" +
+		"##  ##########        ##############\n" +
+		"##############  ##          ##  ##  \n" +
+		"########      ##    ######  ##    ##\n" +
+		"##  ##  ##        ##  ########      \n" +
+		"####################################\n"
+	dm7str = "" +
+		"##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  \n" +
+		"##        ##    ####          ##  ##    ####\n" +
+		"######                  ##########  ######  \n" +
+		"######    ####        ####  ####  ##    ####\n" +
+		"##  ##  ####  ########  ##  ############    \n" +
+		"########  ##  ##  ##              ##########\n" +
+		"######  ####        ##  ####  ##      ##    \n" +
+		"##  ##    ####    ######  ##  ##  ##########\n" +
+		"##        ####  ##      ##        ##  ##    \n" +
+		"##  ############  ########    ##  ##  ######\n" +
+		"########  ##  ##  ######################    \n" +
+		"##    ##    ##  ######    ####            ##\n" +
+		"##  ####    ####  ##  ######      ####  ##  \n" +
+		"##    ########      ######        ####  ####\n" +
+		"##########  ##    ##  ##                    \n" +
+		"####          ##      ##      ##############\n" +
+		"####  ##############  ####  ##  ##  ####    \n" +
+		"####    ######  ##  ##  ##              ####\n" +
+		"####      ##  ######    ####  ####    ##    \n" +
+		"####    ##################    ######  ######\n" +
+		"##      ##  ######  ##      ##        ####  \n" +
+		"############################################\n"
+	dm25str = "" +
+		"##  ##  ##  ##  ##  ##  ##  ##  ##  \n" +
+		"##  ####    ##    ##    ##  ####  ##\n" +
+		"####      ####  ##    ##  ####      \n" +
+		"##  ######    ##    ####  ####  ####\n" +
+		"##    ######  ##################    \n" +
+		"##  ####    ##    ########      ####\n" +
+		"####  ######      ##########  ####  \n" +
+		"####################################\n"
+	dm26str = "" +
+		"##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  \n" +
+		"##  ####    ##        ####  ######          ##  ##  ##  ##    ##\n" +
+		"####      ####  ##############  ##  ##########                  \n" +
+		"##  ######    ##            ########      ##        ######  ####\n" +
+		"##    ######              ##    ####  ####    ##############    \n" +
+		"######      ##    ##    ############      ####        ######  ##\n" +
+		"############      ##    ##      ####  ####  ##    ########  ##  \n" +
+		"################################################################\n"
+)
+
+func TestReadVersion(t *testing.T) {
+	dm, _ := gozxing.NewBitMatrix(14, 14)
+	v, e := readVersion(dm)
+	if e != nil {
+		t.Fatalf("readVersion returns error, %v", e)
+	}
+	if n := v.getVersionNumber(); n != 3 {
+		t.Fatalf("readVersion verNum = %v, expect 3", n)
+	}
+}
+
+func TestExtractDataRegion(t *testing.T) {
+	// version mismatch
+	dm, _ := gozxing.NewBitMatrix(10, 10)
+	v, _ := getVersionForDimensions(14, 14)
+	_, e := extractDataRegion(v, dm)
+	if e == nil {
+		t.Fatalf("extractDataRegion must be error")
+	}
+
+	dm, _ = gozxing.ParseStringToBitMatrix(dm5str, "##", "  ")
+	v, _ = readVersion(dm)
+	b, e := extractDataRegion(v, dm)
+	if e != nil {
+		t.Fatalf("extractDataRegion returns error, %v", e)
+	}
+	expect, _ := gozxing.ParseStringToBitMatrix(""+
+		"########      ####  ####        \n"+
+		"    ##  ##########    ####    ##\n"+
+		"####  ######        ####  ######\n"+
+		"    ##  ########  ##  ##  ####  \n"+
+		"  ######  ##  ##        ########\n"+
+		"####  ##    ##    ##  ####      \n"+
+		"####        ##  ####            \n"+
+		"##  ##  ####  ##    ##  ##      \n"+
+		"##  ##              ##    ######\n"+
+		"####    ##        ####          \n"+
+		"  ####  ######  ##########      \n"+
+		"##  ######    ##      ####      \n"+
+		"  ##########        ############\n"+
+		"############  ##          ##  ##\n"+
+		"######      ##    ######  ##    \n"+
+		"  ##  ##        ##  ########    \n", "##", "  ")
+	if w, h, ew, eh := b.GetWidth(), b.GetHeight(), expect.GetWidth(), expect.GetHeight(); w != ew || h != eh {
+		t.Fatalf("extractDataRegion size=%vx%v, expect %vx%v", w, h, ew, eh)
+	}
+	for j := 0; j < b.GetHeight(); j++ {
+		for i := 0; i < b.GetWidth(); i++ {
+			if bb, eb := b.Get(i, j), expect.Get(i, j); bb != eb {
+				t.Fatalf("extractDataRegion [%v,%v] = %v, expect %v", i, j, bb, eb)
+			}
+		}
+	}
+
+	dm, _ = gozxing.ParseStringToBitMatrix(dm26str, "##", "  ")
+	v, _ = readVersion(dm)
+	b, e = extractDataRegion(v, dm)
+	if e != nil {
+		t.Fatalf("extractDataRegion returns error, %v", e)
+	}
+	expect, _ = gozxing.ParseStringToBitMatrix(""+
+		"  ####    ##        ####  ##          ##  ##  ##  ##    \n"+
+		"##      ####  ##############  ##########                \n"+
+		"  ######    ##            ####      ##        ######  ##\n"+
+		"    ######              ##  ##  ####    ##############  \n"+
+		"####      ##    ##    ########      ####        ######  \n"+
+		"##########      ##    ##    ##  ####  ##    ########  ##\n", "##", "  ")
+
+	if w, h, ew, eh := b.GetWidth(), b.GetHeight(), expect.GetWidth(), expect.GetHeight(); w != ew || h != eh {
+		t.Fatalf("extractDataRegion size=%vx%v, expect %vx%v", w, h, ew, eh)
+	}
+	for j := 0; j < b.GetHeight(); j++ {
+		for i := 0; i < b.GetWidth(); i++ {
+			if bb, eb := b.Get(i, j), expect.Get(i, j); bb != eb {
+				t.Fatalf("extractDataRegion [%v,%v] = %v, expect %v", i, j, bb, eb)
+			}
+		}
+	}
+}
+
+func TestReadCorner1(t *testing.T) {
+	// datamatrix ver 3
+	dm, _ := gozxing.NewBitMatrix(14, 14)
+	parser, _ := NewBitMatrixParser(dm)
+	if n := parser.GetVersion().getVersionNumber(); n != 3 {
+		t.Fatalf("DataMatrix version = %v, expect 3", n)
+	}
+	parser.mappingBitMatrix.Set(10, 0)
+	parser.mappingBitMatrix.Set(11, 0)
+	parser.mappingBitMatrix.Set(11, 1)
+	parser.mappingBitMatrix.Set(11, 2)
+	parser.mappingBitMatrix.Set(11, 3)
+	parser.mappingBitMatrix.Set(0, 11)
+	parser.mappingBitMatrix.Set(1, 11)
+	parser.mappingBitMatrix.Set(2, 11)
+
+	if b := parser.readCorner1(12, 12); b != 255 {
+		t.Fatalf("readCorner1 = %v, expect 255", b)
+	}
+}
+
+func TestReadCorner2(t *testing.T) {
+	// datamatrix ver 4
+	dm, _ := gozxing.NewBitMatrix(16, 16)
+	parser, _ := NewBitMatrixParser(dm)
+	if n := parser.GetVersion().getVersionNumber(); n != 4 {
+		t.Fatalf("DataMatrix version = %v, expect 4", n)
+	}
+	parser.mappingBitMatrix.Set(10, 0)
+	parser.mappingBitMatrix.Set(11, 0)
+	parser.mappingBitMatrix.Set(12, 0)
+	parser.mappingBitMatrix.Set(13, 0)
+	parser.mappingBitMatrix.Set(13, 1)
+	parser.mappingBitMatrix.Set(0, 11)
+	parser.mappingBitMatrix.Set(0, 12)
+	parser.mappingBitMatrix.Set(0, 13)
+
+	if b := parser.readCorner2(14, 14); b != 255 {
+		t.Fatalf("readCorner2 = %v, expect 255", b)
+	}
+}
+
+func TestReadCorner3(t *testing.T) {
+	// datamatrix ver 25
+	dm, _ := gozxing.NewBitMatrix(18, 8)
+	parser, _ := NewBitMatrixParser(dm)
+	if n := parser.GetVersion().getVersionNumber(); n != 25 {
+		t.Fatalf("DataMatrix version = %v, expect 25", n)
+	}
+	parser.mappingBitMatrix.Set(13, 0)
+	parser.mappingBitMatrix.Set(14, 0)
+	parser.mappingBitMatrix.Set(15, 0)
+	parser.mappingBitMatrix.Set(13, 1)
+	parser.mappingBitMatrix.Set(14, 1)
+	parser.mappingBitMatrix.Set(15, 1)
+	parser.mappingBitMatrix.Set(0, 5)
+	parser.mappingBitMatrix.Set(15, 5)
+
+	if b := parser.readCorner3(6, 16); b != 255 {
+		t.Fatalf("readCorner3 = %v, expect 255", b)
+	}
+}
+
+func TestReadCorner4(t *testing.T) {
+	// datamatrix ver 26
+	dm, _ := gozxing.NewBitMatrix(32, 8)
+	parser, _ := NewBitMatrixParser(dm)
+	if n := parser.GetVersion().getVersionNumber(); n != 26 {
+		t.Fatalf("DataMatrix version = %v, expect 26", n)
+	}
+	parser.mappingBitMatrix.Set(26, 0)
+	parser.mappingBitMatrix.Set(27, 0)
+	parser.mappingBitMatrix.Set(27, 1)
+	parser.mappingBitMatrix.Set(27, 2)
+	parser.mappingBitMatrix.Set(27, 3)
+	parser.mappingBitMatrix.Set(0, 3)
+	parser.mappingBitMatrix.Set(0, 4)
+	parser.mappingBitMatrix.Set(0, 5)
+
+	if b := parser.readCorner4(6, 28); b != 255 {
+		t.Fatalf("readCorner4 = %v, expect 255", b)
+	}
+}
+
+func TestReadCodewords(t *testing.T) {
+	// with corner1
+	dm, _ := gozxing.ParseStringToBitMatrix(dm7str, "##", "  ")
+	expect := []byte{
+		231, 54, 241, 136, 30, 181, 76, 226, 121, 15, 166, 61, 129, 192, 87, 237,
+		133, 28, 178, 73, 223, 118, 14, 164, 59, 209, 104, 254, 150, 45, 185, 156,
+		250, 26, 23, 222, 122, 70, 235, 125, 103, 178, 1, 9, 184, 168, 61, 131, 225, 13,
+	}
+	parser, _ := NewBitMatrixParser(dm)
+	b, e := parser.readCodewords()
+	if e != nil {
+		t.Fatalf("readCodewords returns error, %v", e)
+	}
+	if !reflect.DeepEqual(b, expect) {
+		t.Fatalf("readCodewords =\n%v,\nexpect\n%v", b, expect)
+	}
+
+	// with corner2
+	dm, _ = gozxing.ParseStringToBitMatrix(dm4str, "##", "  ")
+	expect = []byte{
+		73, 102, 109, 109, 112, 33, 88, 112, 115, 109, 101, 129, 99, 7, 194, 15,
+		202, 155, 119, 170, 200, 35, 246, 70,
+	}
+	parser, _ = NewBitMatrixParser(dm)
+	b, e = parser.readCodewords()
+	if e != nil {
+		t.Fatalf("readCodewords returns error, %v", e)
+	}
+	if !reflect.DeepEqual(b, expect) {
+		t.Fatalf("readCodewords =\n%v,\nexpect\n%v", b, expect)
+	}
+
+	// with corner3
+	dm, _ = gozxing.ParseStringToBitMatrix(dm25str, "##", "  ")
+	expect = []byte{
+		98, 99, 100, 101, 102, 115, 244, 185, 139, 42, 95, 255,
+	}
+	parser, _ = NewBitMatrixParser(dm)
+	b, e = parser.readCodewords()
+	if e != nil {
+		t.Fatalf("readCodewords returns error, %v", e)
+	}
+	if !reflect.DeepEqual(b, expect) {
+		t.Fatalf("readCodewords =\n%v,\nexpect\n%v", b, expect)
+	}
+
+	// with corner4
+	dm, _ = gozxing.ParseStringToBitMatrix(dm26str, "##", "  ")
+	expect = []byte{
+		98, 99, 100, 101, 102, 103, 129, 56, 206, 101, 202, 9, 172, 57, 10, 232,
+		131, 157, 121, 70, 245,
+	}
+	parser, _ = NewBitMatrixParser(dm)
+	b, e = parser.readCodewords()
+	if e != nil {
+		t.Fatalf("readCodewords returns error, %v", e)
+	}
+	if !reflect.DeepEqual(b, expect) {
+		t.Fatalf("readCodewords =\n%v,\nexpect\n%v", b, expect)
+	}
+
+	// invalid mapping bitmatrix
+	parser.mappingBitMatrix, _ = gozxing.NewBitMatrix(6, 6)
+	_, e = parser.readCodewords()
+	if _, ok := e.(gozxing.FormatException); !ok {
+		t.Fatalf("readCodewords must be FormatException, %T", e)
+	}
+}
+
+func TestNewBitMatrixParser(t *testing.T) {
+	bm, _ := gozxing.NewBitMatrix(5, 5)
+	_, e := NewBitMatrixParser(bm)
+	if _, ok := e.(gozxing.FormatException); !ok {
+		t.Fatalf("NewBitMatrixParser must be FormatException, %T", e)
+	}
+
+	bm, _ = gozxing.NewBitMatrix(1, 8)
+	_, e = NewBitMatrixParser(bm)
+	if e == nil {
+		t.Fatalf("NewBitMatrixParser must be error")
+	}
+}
