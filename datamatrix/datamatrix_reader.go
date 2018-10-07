156546bc1e78e0f79ae1b0ccd79f10be8622faca
makki_d <d.makiuchi@gmail.com>
2018-10-08 00:31:19 +0900

    implement datamatrix.DataMatrixReader

---
+++ datamatrix/datamatrix_reader.go
@@ -0,0 +1,154 @@
+package datamatrix
+
+import (
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/common"
+	"github.com/makiuchi-d/gozxing/datamatrix/decoder"
+	"github.com/makiuchi-d/gozxing/datamatrix/detector"
+)
+
+var (
+	noPoints = []gozxing.ResultPoint{}
+)
+
+type DataMatrixReader struct {
+	decoder *decoder.Decoder
+}
+
+func NewDataMatrixReader() *DataMatrixReader {
+	return &DataMatrixReader{
+		decoder: decoder.NewDecoder(),
+	}
+}
+
+func (r *DataMatrixReader) DecodeWithoutHints(image *gozxing.BinaryBitmap) (*gozxing.Result, error) {
+	return r.Decode(image, nil)
+}
+
+// Decode Locates and decodes a Data Matrix code in an image.
+//
+// @return a String representing the content encoded by the Data Matrix code
+// @throws NotFoundException if a Data Matrix code cannot be found
+// @throws FormatException if a Data Matrix code cannot be decoded
+// @throws ChecksumException if error correction fails
+//
+func (r *DataMatrixReader) Decode(image *gozxing.BinaryBitmap, hints map[gozxing.DecodeHintType]interface{}) (*gozxing.Result, error) {
+	var decoderResult *common.DecoderResult
+	var points []gozxing.ResultPoint
+	if _, ok := hints[gozxing.DecodeHintType_PURE_BARCODE]; ok {
+		blackm, e := image.GetBlackMatrix()
+		if e != nil {
+			return nil, e
+		}
+		bits, e := extractPureBits(blackm)
+		if e != nil {
+			return nil, e
+		}
+		decoderResult, e = r.decoder.Decode(bits)
+		if e != nil {
+			return nil, e
+		}
+		points = noPoints
+	} else {
+		blackm, e := image.GetBlackMatrix()
+		if e != nil {
+			return nil, e
+		}
+		detector, e := detector.NewDetector(blackm)
+		if e != nil {
+			return nil, e
+		}
+		detectorResult, e := detector.Detect()
+		if e != nil {
+			return nil, e
+		}
+		decoderResult, e = r.decoder.Decode(detectorResult.GetBits())
+		if e != nil {
+			return nil, e
+		}
+		points = detectorResult.GetPoints()
+	}
+	result := gozxing.NewResult(decoderResult.GetText(), decoderResult.GetRawBytes(), points,
+		gozxing.BarcodeFormat_DATA_MATRIX)
+	byteSegments := decoderResult.GetByteSegments()
+	if byteSegments != nil {
+		result.PutMetadata(gozxing.ResultMetadataType_BYTE_SEGMENTS, byteSegments)
+	}
+	ecLevel := decoderResult.GetECLevel()
+	if ecLevel != "" {
+		result.PutMetadata(gozxing.ResultMetadataType_ERROR_CORRECTION_LEVEL, ecLevel)
+	}
+	return result, nil
+}
+
+func (r *DataMatrixReader) Reset() {
+	// do nothing
+}
+
+// extractPureBits This method detects a code in a "pure" image -- that is, pure monochrome image
+// which contains only an unrotated, unskewed, image of a code, with some white border
+// around it. This is a specialized method that works exceptionally fast in this special case.
+//
+// @see com.google.zxing.qrcode.QRCodeReader#extractPureBits(BitMatrix)
+//
+func extractPureBits(image *gozxing.BitMatrix) (*gozxing.BitMatrix, error) {
+
+	leftTopBlack := image.GetTopLeftOnBit()
+	rightBottomBlack := image.GetBottomRightOnBit()
+	if leftTopBlack == nil || rightBottomBlack == nil {
+		return nil, gozxing.GetNotFoundExceptionInstance()
+	}
+
+	moduleSize, e := moduleSize(leftTopBlack, image)
+	if e != nil {
+		return nil, e
+	}
+
+	top := leftTopBlack[1]
+	bottom := rightBottomBlack[1]
+	left := leftTopBlack[0]
+	right := rightBottomBlack[0]
+
+	matrixWidth := (right - left + 1) / moduleSize
+	matrixHeight := (bottom - top + 1) / moduleSize
+	if matrixWidth <= 0 || matrixHeight <= 0 {
+		return nil, gozxing.GetNotFoundExceptionInstance()
+	}
+
+	// Push in the "border" by half the module width so that we start
+	// sampling in the middle of the module. Just in case the image is a
+	// little off, this will help recover.
+	nudge := moduleSize / 2
+	top += nudge
+	left += nudge
+
+	// Now just read off the bits
+	bits, _ := gozxing.NewBitMatrix(matrixWidth, matrixHeight)
+	for y := 0; y < matrixHeight; y++ {
+		iOffset := top + y*moduleSize
+		for x := 0; x < matrixWidth; x++ {
+			if image.Get(left+x*moduleSize, iOffset) {
+				bits.Set(x, y)
+			}
+		}
+	}
+	return bits, nil
+}
+
+func moduleSize(leftTopBlack []int, image *gozxing.BitMatrix) (int, error) {
+	width := image.GetWidth()
+	x := leftTopBlack[0]
+	y := leftTopBlack[1]
+	for x < width && image.Get(x, y) {
+		x++
+	}
+	if x == width {
+		return 0, gozxing.GetNotFoundExceptionInstance()
+	}
+
+	moduleSize := x - leftTopBlack[0]
+	if moduleSize == 0 {
+		return 0, gozxing.GetNotFoundExceptionInstance()
+	}
+	return moduleSize, nil
+}
---
+++ datamatrix/datamatrix_reader_test.go
@@ -0,0 +1,297 @@
+package datamatrix
+
+import (
+	"testing"
+
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/testutil"
+)
+
+const (
+	dm4str = "" +
+		"                                        \n" +
+		"                                        \n" +
+		"    ##  ##  ##  ##  ##  ##  ##  ##      \n" +
+		"    ##  ####  ####  ##  ####      ##    \n" +
+		"    ####      ####  ####                \n" +
+		"    ######  ####  ##    ####      ##    \n" +
+		"    ####                ##########      \n" +
+		"    ######      ##    ##        ####    \n" +
+		"    ####    ####          ##            \n" +
+		"    ######  ####  ##    ##        ##    \n" +
+		"    ##    ##  ##############            \n" +
+		"    ##  ####  ##    ######  ####  ##    \n" +
+		"    ######  ##    ##      ######        \n" +
+		"    ##  ####  ######        ####  ##    \n" +
+		"    ##    ##  ######  ##  ######        \n" +
+		"    ########    ##  ##        ##  ##    \n" +
+		"    ############  ##  ######    ##      \n" +
+		"    ################################    \n" +
+		"                                        \n" +
+		"                                        \n"
+
+	dmstr = "" +
+		"                                                \n" +
+		"                                                \n" +
+		"      ##  ##  ##  ##  ##  ##  ##  ##  ##        \n" +
+		"      ##########      ####  ####        ##      \n" +
+		"      ##    ##  ##########    ####    ##        \n" +
+		"      ######  ######        ####  ########      \n" +
+		"      ##    ##  ########  ##  ##  ####          \n" +
+		"      ##  ######  ##  ##        ##########      \n" +
+		"      ######  ##    ##    ##  ####              \n" +
+		"      ######        ##  ####            ##      \n" +
+		"      ####  ##  ####  ##    ##  ##              \n" +
+		"      ####  ##              ##    ########      \n" +
+		"      ######    ##        ####                  \n" +
+		"      ##  ####  ######  ##########      ##      \n" +
+		"      ####  ######    ##      ####              \n" +
+		"      ##  ##########        ##############      \n" +
+		"      ##############  ##          ##  ##        \n" +
+		"      ########      ##    ######  ##    ##      \n" +
+		"      ##  ##  ##        ##  ########            \n" +
+		"      ####################################      \n" +
+		"                                                \n" +
+		"                                                \n"
+)
+
+func TestModuleSize(t *testing.T) {
+	image, _ := gozxing.NewBitMatrix(10, 10)
+	leftTopBlack := []int{3, 3}
+
+	_, e := moduleSize(leftTopBlack, image)
+	if _, ok := e.(gozxing.NotFoundException); !ok {
+		t.Fatalf("moduleSize must be NotFoundException, %T", e)
+	}
+
+	image.SetRegion(3, 3, 7, 7)
+	_, e = moduleSize(leftTopBlack, image)
+	if _, ok := e.(gozxing.NotFoundException); !ok {
+		t.Fatalf("moduleSize must be NotFoundException, %T", e)
+	}
+
+	image.Clear()
+	image.SetRegion(3, 3, 3, 3)
+	m, e := moduleSize(leftTopBlack, image)
+	if e != nil {
+		t.Fatalf("moduleSize returns error, %v", e)
+	}
+	if m != 3 {
+		t.Fatalf("moduleSize = %v, expect 3", m)
+	}
+}
+
+func TestExtractPureBits(t *testing.T) {
+	image, _ := gozxing.NewBitMatrix(10, 8)
+
+	_, e := extractPureBits(image)
+	if _, ok := e.(gozxing.NotFoundException); !ok {
+		t.Fatalf("extractPureBits must be NotFoundException, %T", e)
+	}
+
+	image.SetRegion(3, 3, 7, 5)
+	_, e = extractPureBits(image)
+	if e == nil {
+		t.Fatalf("extractPureBits must be error")
+	}
+
+	image.Clear()
+	image.SetRegion(0, 0, 9, 8)
+	_, e = extractPureBits(image)
+	if _, ok := e.(gozxing.NotFoundException); !ok {
+		t.Fatalf("extractPureBits must be NotFoundException, %T", e)
+	}
+
+	image, _ = gozxing.ParseStringToBitMatrix(dmstr, "##", "  ")
+	image = testutil.ExpandBitMatrix(image, 2)
+	expect, _ := gozxing.ParseStringToBitMatrix(""+
+		"##  ##  ##  ##  ##  ##  ##  ##  ##  \n"+
+		"##########      ####  ####        ##\n"+
+		"##    ##  ##########    ####    ##  \n"+
+		"######  ######        ####  ########\n"+
+		"##    ##  ########  ##  ##  ####    \n"+
+		"##  ######  ##  ##        ##########\n"+
+		"######  ##    ##    ##  ####        \n"+
+		"######        ##  ####            ##\n"+
+		"####  ##  ####  ##    ##  ##        \n"+
+		"####  ##              ##    ########\n"+
+		"######    ##        ####            \n"+
+		"##  ####  ######  ##########      ##\n"+
+		"####  ######    ##      ####        \n"+
+		"##  ##########        ##############\n"+
+		"##############  ##          ##  ##  \n"+
+		"########      ##    ######  ##    ##\n"+
+		"##  ##  ##        ##  ########      \n"+
+		"####################################\n", "##", "  ")
+
+	b, e := extractPureBits(image)
+	if e != nil {
+		t.Fatalf("extractPureBits returns error, %v", e)
+	}
+	for j := 0; j < expect.GetHeight(); j++ {
+		for i := 0; i < expect.GetWidth(); i++ {
+			if b.Get(i, j) != expect.Get(i, j) {
+				t.Fatalf("bits(%v,%v) = %v, expect %v", i, j, b.Get(i, j), expect.Get(i, j))
+			}
+		}
+	}
+}
+
+func TestDataMatrixReader_Reset(t *testing.T) {
+	d := NewDataMatrixReader()
+	d.Reset() // do nothing
+}
+
+func TestDataMatrixReader_DecodePureBarcode(t *testing.T) {
+	reader := NewDataMatrixReader()
+	hints := map[gozxing.DecodeHintType]interface{}{
+		gozxing.DecodeHintType_PURE_BARCODE: true,
+	}
+
+	img, _ := gozxing.NewBitMatrix(10, 10)
+	bmp := testutil.NewBinaryBitmapFromBitMatrix(img)
+	_, e := reader.Decode(bmp, hints)
+	if e == nil {
+		t.Fatalf("Decode must be error")
+	}
+
+	img.SetRegion(0, 0, 10, 10)
+	bmp = testutil.NewBinaryBitmapFromBitMatrix(img)
+	_, e = reader.Decode(bmp, hints)
+	if e == nil {
+		t.Fatalf("Decode must be error")
+	}
+
+	img, _ = gozxing.ParseStringToBitMatrix(dm4str, "##", "  ")
+	img.SetRegion(5, 5, 10, 10)
+	bmp = testutil.NewBinaryBitmapFromBitMatrix(testutil.ExpandBitMatrix(img, 2))
+	_, e = reader.Decode(bmp, hints)
+	if e == nil {
+		t.Fatalf("Decode must be error")
+	}
+
+	img, _ = gozxing.ParseStringToBitMatrix(dm4str, "##", "  ")
+	bmp = testutil.NewBinaryBitmapFromBitMatrix(testutil.ExpandBitMatrix(img, 2))
+	result, e := reader.Decode(bmp, hints)
+	if e != nil {
+		t.Fatalf("Decode returns error, %v", e)
+	}
+	expect := "Hello World"
+	if r := result.GetText(); r != expect {
+		t.Fatalf("Decode result=\"%v\", expect \"%v\"", r, expect)
+	}
+}
+
+func TestDataMatrixReader_DecodeWithoutHints(t *testing.T) {
+	reader := NewDataMatrixReader()
+
+	img, _ := gozxing.NewBitMatrix(10, 10)
+	bmp := testutil.NewBinaryBitmapFromBitMatrix(img)
+	_, e := reader.DecodeWithoutHints(bmp)
+	if e == nil {
+		t.Fatalf("Decode must be error")
+	}
+
+	img.SetRegion(0, 0, 10, 10)
+	bmp = testutil.NewBinaryBitmapFromBitMatrix(img)
+	_, e = reader.DecodeWithoutHints(bmp)
+	if e == nil {
+		t.Fatalf("Decode must be error")
+	}
+
+	img, _ = gozxing.NewBitMatrix(40, 40)
+	bmp = testutil.NewBinaryBitmapFromBitMatrix(img)
+	_, e = reader.DecodeWithoutHints(bmp)
+	if e == nil {
+		t.Fatalf("Decode must be error")
+	}
+
+	img, _ = gozxing.ParseStringToBitMatrix(dm4str, "##", "  ")
+	img.SetRegion(5, 5, 10, 10)
+	bmp = testutil.NewBinaryBitmapFromBitMatrix(testutil.ExpandBitMatrix(img, 2))
+	_, e = reader.DecodeWithoutHints(bmp)
+	if e == nil {
+		t.Fatalf("Decode must be error")
+	}
+
+	img, _ = gozxing.ParseStringToBitMatrix(dmstr, "##", "  ")
+	bmp = testutil.NewBinaryBitmapFromBitMatrix(testutil.ExpandBitMatrix(img, 4))
+	result, e := reader.DecodeWithoutHints(bmp)
+	if e != nil {
+		t.Fatalf("Decode returns error, %v\n", e)
+	}
+	expect := "Testing C40"
+	if r := result.GetText(); r != expect {
+		t.Fatalf("Decode result=\"%v\", expect \"%v\"", r, expect)
+	}
+}
+
+func testDataMatrixReader_Decode(t *testing.T, file, expect string, pure bool) {
+	reader := NewDataMatrixReader()
+	bmp := testutil.NewBinaryBitmapFromFile(file)
+	var help map[gozxing.DecodeHintType]interface{}
+	if pure {
+		help = make(map[gozxing.DecodeHintType]interface{})
+		help[gozxing.DecodeHintType_PURE_BARCODE] = true
+	}
+	result, e := reader.Decode(bmp, help)
+	if e != nil {
+		t.Fatalf("Decode(%v) returns error, %v", file, e)
+	}
+	if txt := result.GetText(); txt != expect {
+		t.Fatalf("Decode(%v) = \"%v\", expect \"%v\"", file, txt, expect)
+	}
+}
+
+func TestDataMatrixReader_Decode(t *testing.T) {
+
+	// testdata from zxing core/src/test/resources/blackbox/datamatrix-1/
+	testDataMatrixReader_Decode(t, "testdata/0123456789.png", "0123456789", false)
+	testDataMatrixReader_Decode(t, "testdata/C40.png", "Testing C40", true)
+	testDataMatrixReader_Decode(t, "testdata/EDIFACT.png", "EDIFACTEDIFACT", true)
+	testDataMatrixReader_Decode(t, "testdata/GUID.png", "10f27ce-acb7-4e4e-a7ae-a0b98da6ed4a", false)
+	testDataMatrixReader_Decode(t, "testdata/HelloWorld_Text_L_Kaywa.png", "Hello World", true)
+	testDataMatrixReader_Decode(t, "testdata/HelloWorld_Text_L_Kaywa_1_error_byte.png", "Hello World", true)
+	testDataMatrixReader_Decode(t, "testdata/HelloWorld_Text_L_Kaywa_2_error_byte.png", "Hello World", true)
+	testDataMatrixReader_Decode(t, "testdata/HelloWorld_Text_L_Kaywa_3_error_byte.png", "Hello World", true)
+	testDataMatrixReader_Decode(t, "testdata/HelloWorld_Text_L_Kaywa_4_error_byte.png", "Hello World", true)
+	testDataMatrixReader_Decode(t, "testdata/X12.png", "X12X12X12X12", true)
+	testDataMatrixReader_Decode(t, "testdata/abcd-18x8.png", "abcde", true)
+	testDataMatrixReader_Decode(t, "testdata/abcd-26x12.png", "abcdefghijklm", true)
+	testDataMatrixReader_Decode(t, "testdata/abcd-32x8.png", "abcdef", true)
+	testDataMatrixReader_Decode(t, "testdata/abcd-36x12.png", "abcdefghijklmnopq", true)
+	testDataMatrixReader_Decode(t, "testdata/abcd-36x16.png", "abcdefghijklmnopqrstuvwxyz", true)
+	testDataMatrixReader_Decode(t, "testdata/abcd-48x16.png", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW", true)
+	testDataMatrixReader_Decode(t, "testdata/abcd-52x52-IDAutomation.png",
+		"abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd", true)
+	testDataMatrixReader_Decode(t, "testdata/abcd-52x52.png",
+		"abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd", true)
+	testDataMatrixReader_Decode(t, "testdata/abcdefg-64x64.png", ""+
+		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(),./\\"+
+		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(),./\\"+
+		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(),./\\", true)
+	testDataMatrixReader_Decode(t, "testdata/abcdefg.png", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(),./\\", true)
+	testDataMatrixReader_Decode(t, "testdata/zxing_URL_L_Kayway.png", "http://code.google.com/p/zxing/", true)
+
+	// testdata from zxing core/src/test/resources/blackbox/datamatrix-1/
+	testDataMatrixReader_Decode(t, "testdata/01.png", "http://google.com/m", false)
+	testDataMatrixReader_Decode(t, "testdata/02.png", "http://google.com/m", false)
+	testDataMatrixReader_Decode(t, "testdata/04.png", "http://google.com/m", false)
+	testDataMatrixReader_Decode(t, "testdata/05.png", "http://google.com/m", false)
+	testDataMatrixReader_Decode(t, "testdata/06.png", "http://google.com/m", false)
+	testDataMatrixReader_Decode(t, "testdata/07.png", "http://google.com/m", false)
+	testDataMatrixReader_Decode(t, "testdata/08.png", "http://google.com/m", false)
+	testDataMatrixReader_Decode(t, "testdata/09.png", "This is a test of our DataMatrix support using a longer piece of text, and therefore a more dense barcode.", false)
+	testDataMatrixReader_Decode(t, "testdata/10.png", "This is a test of our DataMatrix support using a longer piece of text, and therefore a more dense barcode.", false)
+	testDataMatrixReader_Decode(t, "testdata/12.png", "This is a test of our DataMatrix support using a longer piece of text, and therefore a more dense barcode.", false)
+	testDataMatrixReader_Decode(t, "testdata/13.png", "This is a test of our DataMatrix support using a longer piece of text, and therefore a more dense barcode.", false)
+	testDataMatrixReader_Decode(t, "testdata/14.png", "This is a test of our DataMatrix support using a longer piece of text, and therefore a more dense barcode.", false)
+	testDataMatrixReader_Decode(t, "testdata/15.png", "This is a test of our DataMatrix support using a longer piece of text, and therefore a more dense barcode.", false)
+
+	// original zxing cannot read these too.
+	//testDataMatrixReader_Decode(t, "testdata/03.png", "http://google.com/m", false)
+	//testDataMatrixReader_Decode(t, "testdata/11.png","This is a test of our DataMatrix support using a longer piece of text, and therefore a more dense barcode.", false)
+	//testDataMatrixReader_Decode(t, "testdata/16.png","This is a test of our DataMatrix support using a longer piece of text, and therefore a more dense barcode.", false)
+	//testDataMatrixReader_Decode(t, "testdata/17.png","This is a test of our DataMatrix support using a longer piece of text, and therefore a more dense barcode.", false)
+	//testDataMatrixReader_Decode(t, "testdata/18.png","This is a test of our DataMatrix support using a longer piece of text, and therefore a more dense barcode.", false)
+}
