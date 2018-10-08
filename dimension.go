7b9eb9dc1d99e9fa9764c89ec954f060b393ec35
makki_d <d.makiuchi@gmail.com>
2018-10-09 00:09:25 +0900

    implement Dimension

---
+++ dimension.go
@@ -0,0 +1,38 @@
+package gozxing
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Dimension struct {
+	width  int
+	height int
+}
+
+func NewDimension(width, height int) (*Dimension, error) {
+	if width < 0 || height < 0 {
+		return nil, errors.New("IllegalArgumentException")
+	}
+	return &Dimension{width, height}, nil
+}
+
+func (this *Dimension) GetWidth() int {
+	return this.width
+}
+
+func (this *Dimension) GetHeight() int {
+	return this.height
+}
+
+func (this *Dimension) Equals(other *Dimension) bool {
+	return this.width == other.width && this.height == other.height
+}
+
+func (this *Dimension) HashCode() int {
+	return this.width*32713 + this.height
+}
+
+func (this *Dimension) String() string {
+	return fmt.Sprintf("%dx%d", this.width, this.height)
+}
---
+++ dimension_test.go
@@ -0,0 +1,63 @@
+package gozxing
+
+import (
+	"testing"
+)
+
+func TestDimension(t *testing.T) {
+	_, e := NewDimension(-1, 1)
+	if e == nil {
+		t.Fatalf("NewDimension(0,1) must be error")
+	}
+	_, e = NewDimension(1, -1)
+	if e == nil {
+		t.Fatalf("NewDimension(1,0) must be error")
+	}
+	d, e := NewDimension(10, 20)
+	if e != nil {
+		t.Fatalf("NewDimension(10, 20) returns error: %v", e)
+	}
+	if w := d.GetWidth(); w != 10 {
+		t.Fatalf("GetWidth = %v, expect 10", w)
+	}
+	if h := d.GetHeight(); h != 20 {
+		t.Fatalf("GetHeight = %v, expect 20", h)
+	}
+}
+
+func TestDimension_Equals(t *testing.T) {
+	d1, _ := NewDimension(10, 20)
+	d2, _ := NewDimension(11, 20)
+	if d1.Equals(d2) {
+		t.Fatalf("%v not equals %v", d1, d2)
+	}
+
+	d2, _ = NewDimension(10, 19)
+	if d1.Equals(d2) {
+		t.Fatalf("%v not equals %v", d1, d2)
+	}
+
+	d2, _ = NewDimension(10,20)
+	if !d1.Equals(d2) {
+		t.Fatalf("%v equals %v", d1, d2)
+	}
+}
+
+func TestDimension_HashCode(t *testing.T) {
+	d, _ := NewDimension(10, 20)
+	if h := d.HashCode(); h != 327150 {
+		t.Fatalf("HashCode = %v, expect 327150", h)
+	}
+
+	d, _ = NewDimension(20, 15)
+	if h := d.HashCode(); h != 654275 {
+		t.Fatalf("HashCode = %v, expect 654275", h)
+	}
+}
+
+func TestDimension_String(t *testing.T) {
+	d, _ := NewDimension(10, 20)
+	if s := d.String(); s != "10x20" {
+		t.Fatalf("String = \"%v\", expect \"10x20\"", s)
+	}
+}
