2437abdb56e50ad0093c79e564b3f4479eb9540b
makki_d <d.makiuchi@gmail.com>
2018-10-20 00:29:09 +0900

    implement datamatrix.encoder.X12Encoder

---
+++ datamatrix/encoder/x12_encoder.go
@@ -0,0 +1,79 @@
+package encoder
+
+type X12Encoder struct{}
+
+func NewX12Encoder() Encoder {
+	return X12Encoder{}
+}
+
+func (this X12Encoder) getEncodingMode() int {
+	return HighLevelEncoder_X12_ENCODATION
+}
+
+func (this X12Encoder) encode(context *EncoderContext) error {
+	//step C
+	buffer := make([]byte, 0)
+	for context.HasMoreCharacters() {
+		c := context.GetCurrentChar()
+		context.pos++
+
+		var e error
+		buffer, e = x12EncodeChar(c, buffer)
+		if e != nil {
+			return e
+		}
+
+		count := len(buffer)
+		if (count % 3) == 0 {
+			buffer = c40WriteNextTriplet(context, buffer)
+
+			newMode := HighLevelEncoder_lookAheadTest(context.GetMessage(), context.pos, this.getEncodingMode())
+			if newMode != this.getEncodingMode() {
+				// Return to ASCII encodation, which will actually handle latch to new mode
+				context.SignalEncoderChange(HighLevelEncoder_ASCII_ENCODATION)
+				break
+			}
+		}
+	}
+	return x12HandleEOD(context, buffer)
+}
+
+func x12EncodeChar(c byte, sb []byte) ([]byte, error) {
+	switch c {
+	case '\r':
+		sb = append(sb, 0)
+	case '*':
+		sb = append(sb, 1)
+	case '>':
+		sb = append(sb, 2)
+	case ' ':
+		sb = append(sb, 3)
+	default:
+		if c >= '0' && c <= '9' {
+			sb = append(sb, c-48+4)
+		} else if c >= 'A' && c <= 'Z' {
+			sb = append(sb, c-65+14)
+		} else {
+			return sb, illegalCharacter(c)
+		}
+	}
+	return sb, nil
+}
+
+func x12HandleEOD(context *EncoderContext, buffer []byte) error {
+	e := context.UpdateSymbolInfo()
+	if e != nil {
+		return e
+	}
+	available := context.GetSymbolInfo().GetDataCapacity() - context.GetCodewordCount()
+	count := len(buffer)
+	context.pos -= count
+	if context.GetRemainingCharacters() > 1 || available > 1 ||
+		context.GetRemainingCharacters() != available {
+		context.WriteCodeword(HighLevelEncoder_X12_UNLATCH)
+	}
+	if context.GetNewEncoding() < 0 {
+		context.SignalEncoderChange(HighLevelEncoder_ASCII_ENCODATION)
+	}
+	return nil
+}
---
+++ datamatrix/encoder/x12_encoder_test.go
@@ -0,0 +1,91 @@
+package encoder
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestX12Encoder_getEncodingMode(t *testing.T) {
+	enc := NewX12Encoder()
+	if r := enc.getEncodingMode(); r != HighLevelEncoder_X12_ENCODATION {
+		t.Fatalf("getEncodingMode != %v, expect %v", r, HighLevelEncoder_X12_ENCODATION)
+	}
+}
+
+func testX12EncodeChar(t *testing.T, c, expect byte) {
+	sb, e := x12EncodeChar(c, []byte{})
+	if e != nil {
+		t.Fatalf("x12EncodeChar(%v) returns error: %v", c, e)
+	}
+	if !reflect.DeepEqual(sb, []byte{expect}) {
+		t.Fatalf("x12EncodeChar(%v) sb = %v, expect [%v]", c, sb, expect)
+	}
+}
+
+func TestX12EncodeChar(t *testing.T) {
+	_, e := x12EncodeChar('^', []byte{})
+	if e == nil {
+		t.Fatalf("x12EncodeChar must be error")
+	}
+
+	testX12EncodeChar(t, '\r', 0)
+	testX12EncodeChar(t, '*', 1)
+	testX12EncodeChar(t, '>', 2)
+	testX12EncodeChar(t, ' ', 3)
+	testX12EncodeChar(t, '0', 4)
+	testX12EncodeChar(t, '9', 13)
+	testX12EncodeChar(t, 'A', 14)
+	testX12EncodeChar(t, 'Z', 39)
+}
+
+func TestX12HandleEOD(t *testing.T) {
+	ctx, _ := NewEncoderContext("")
+	ctx.codewords = make([]byte, 1559)
+	e := x12HandleEOD(ctx, []byte{})
+	if e == nil {
+		t.Fatalf("x12HandleEOD must be error")
+	}
+
+	ctx, _ = NewEncoderContext("AAA0")
+	ctx.symbolInfo = symbols[10]
+	ctx.pos = 4
+	ctx.codewords = []byte{89, 191}
+	e = x12HandleEOD(ctx, []byte{1})
+	if e != nil {
+		t.Fatalf("x12HandleEOD returns error: %v", e)
+	}
+	if r := ctx.GetCodewords(); !reflect.DeepEqual(r, []byte{89, 191, 254}) {
+		t.Fatalf("context codewords = %v, expect [89 191 254]", r)
+	}
+	if ctx.pos != 3 {
+		t.Fatalf("context pos = %v, expect 3", ctx.pos)
+	}
+}
+
+func TestX12Encoder_encode(t *testing.T) {
+	enc := NewX12Encoder()
+
+	ctx, _ := NewEncoderContext("^")
+	ctx.SignalEncoderChange(HighLevelEncoder_X12_ENCODATION)
+	e := enc.encode(ctx)
+	if e == nil {
+		t.Fatalf("encode must be error")
+	}
+
+	ctx, _ = NewEncoderContext("AAA000000")
+	ctx.SignalEncoderChange(HighLevelEncoder_X12_ENCODATION)
+	e = enc.encode(ctx)
+	if e != nil {
+		t.Fatalf("encode returns error: %v", e)
+	}
+	expect := []byte{89, 191, 254}
+	if r := ctx.GetCodewords(); !reflect.DeepEqual(r, expect) {
+		t.Fatalf("context codewords = %v, expect %v", r, expect)
+	}
+	if ctx.pos != 3 {
+		t.Fatalf("context pos = %v, expect 3", ctx.pos)
+	}
+	if r := ctx.GetNewEncoding(); r != HighLevelEncoder_ASCII_ENCODATION {
+		t.Fatalf("context nextEncoding = %v, expect %v", r, HighLevelEncoder_ASCII_ENCODATION)
+	}
+}
