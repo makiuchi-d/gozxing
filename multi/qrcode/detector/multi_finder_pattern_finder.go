70d2aa8353eca295e663055123ac21cc0c910fc8
makki_d <d.makiuchi@gmail.com>
2018-12-29 20:34:47 +0900

    implement multi.qrcode.detector.MultiFinderPatternFinder

---
+++ multi/qrcode/detector/multi_finder_pattern_finder.go
@@ -0,0 +1,264 @@
+package detector
+
+import (
+	"math"
+	"sort"
+
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/qrcode/detector"
+)
+
+// This class attempts to find finder patterns in a QR Code.
+// Finder patterns are the square markers at three corners of a QR Code.
+//
+// This class is thread-safe but not reentrant. Each thread must allocate its own object.
+//
+// In contrast to {@link FinderPatternFinder}, this class will return an array of all possible
+// QR code locations in the image.
+//
+// Use the TRY_HARDER hint to ask for a more thorough detection.
+//
+type MultiFinderPatternFinder struct {
+	*detector.FinderPatternFinder
+}
+
+// private static final FinderPatternInfo[] EMPTY_RESULT_ARRAY = new FinderPatternInfo[0];
+// private static final FinderPattern[][] EMPTY_FP_2D_ARRAY = new FinderPattern[0][];
+
+const (
+	// TODO MIN_MODULE_COUNT and MAX_MODULE_COUNT would be great hints to ask the user for
+	// since it limits the number of regions to decode
+
+	// max. legal count of modules per QR code edge (177)
+	MAX_MODULE_COUNT_PER_EDGE = 180
+	// min. legal count per modules per QR code edge (11)
+	MIN_MODULE_COUNT_PER_EDGE = 9
+
+	// More or less arbitrary cutoff point for determining if two finder patterns might belong
+	// to the same code if they differ less than DIFF_MODSIZE_CUTOFF_PERCENT percent in their
+	// estimated modules sizes.
+	DIFF_MODSIZE_CUTOFF_PERCENT = 0.05
+
+	// More or less arbitrary cutoff point for determining if two finder patterns might belong
+	// to the same code if they differ less than DIFF_MODSIZE_CUTOFF pixels/module in their
+	// estimated modules sizes.
+	DIFF_MODSIZE_CUTOFF = 0.5
+)
+
+// ModuleSizeComparator A comparator that orders FinderPatterns by their estimated module size.
+func ModuleSizeComparator(possibleCenters []*detector.FinderPattern) func(int, int) bool {
+	return func(i, j int) bool {
+		center1 := possibleCenters[i]
+		center2 := possibleCenters[j]
+		value := center2.GetEstimatedModuleSize() - center1.GetEstimatedModuleSize()
+		return value < 0
+	}
+}
+
+// NewMultiFinderPatternFinder Creates a finder that will search the image for three finder patterns.
+//
+// @param image image to search
+//
+func NewMultiFinderPatternFinder(image *gozxing.BitMatrix, resultPointCallback gozxing.ResultPointCallback) *MultiFinderPatternFinder {
+	return &MultiFinderPatternFinder{
+		detector.NewFinderPatternFinder(image, resultPointCallback),
+	}
+}
+
+// selectMultipleBestPatterns select the best patterns.
+// @return the 3 best {@link FinderPattern}s from our list of candidates. The "best" are
+//         those that have been detected at least {@link #CENTER_QUORUM} times, and whose module
+//         size differs from the average among those patterns the least
+// @throws NotFoundException if 3 such finder patterns do not exist
+func (this *MultiFinderPatternFinder) selectMultipleBestPatterns() ([][]*detector.FinderPattern, error) {
+	possibleCenters := this.GetPossibleCenters()
+	size := len(possibleCenters)
+
+	if size < 3 {
+		// Couldn't find enough finder patterns
+		return nil, gozxing.GetNotFoundExceptionInstance()
+	}
+
+	// Begin HE modifications to safely detect multiple codes of equal size
+	if size == 3 {
+		return [][]*detector.FinderPattern{
+			{
+				possibleCenters[0],
+				possibleCenters[1],
+				possibleCenters[2],
+			},
+		}, nil
+	}
+
+	// Sort by estimated module size to speed up the upcoming checks
+	sort.Slice(possibleCenters, ModuleSizeComparator(possibleCenters))
+
+	// Now lets start: build a list of tuples of three finder locations that
+	//  - feature similar module sizes
+	//  - are placed in a distance so the estimated module count is within the QR specification
+	//  - have similar distance between upper left/right and left top/bottom finder patterns
+	//  - form a triangle with 90° angle (checked by comparing top right/bottom left distance
+	//    with pythagoras)
+	//
+	// Note: we allow each point to be used for more than one code region: this might seem
+	// counterintuitive at first, but the performance penalty is not that big. At this point,
+	// we cannot make a good quality decision whether the three finders actually represent
+	// a QR code, or are just by chance layouted so it looks like there might be a QR code there.
+	// So, if the layout seems right, lets have the decoder try to decode.
+
+	results := make([][]*detector.FinderPattern, 0) // holder for the results
+
+	for i1 := 0; i1 < (size - 2); i1++ {
+		p1 := possibleCenters[i1]
+		if p1 == nil {
+			continue
+		}
+
+		for i2 := i1 + 1; i2 < (size - 1); i2++ {
+			p2 := possibleCenters[i2]
+			if p2 == nil {
+				continue
+			}
+
+			// Compare the expected module sizes; if they are really off, skip
+			vModSize12 := (p1.GetEstimatedModuleSize() - p2.GetEstimatedModuleSize()) /
+				math.Min(p1.GetEstimatedModuleSize(), p2.GetEstimatedModuleSize())
+			vModSize12A := math.Abs(p1.GetEstimatedModuleSize() - p2.GetEstimatedModuleSize())
+			if vModSize12A > DIFF_MODSIZE_CUTOFF && vModSize12 >= DIFF_MODSIZE_CUTOFF_PERCENT {
+				// break, since elements are ordered by the module size deviation there cannot be
+				// any more interesting elements for the given p1.
+				break
+			}
+
+			for i3 := i2 + 1; i3 < size; i3++ {
+				p3 := possibleCenters[i3]
+				if p3 == nil {
+					continue
+				}
+
+				// Compare the expected module sizes; if they are really off, skip
+				vModSize23 := (p2.GetEstimatedModuleSize() - p3.GetEstimatedModuleSize()) /
+					math.Min(p2.GetEstimatedModuleSize(), p3.GetEstimatedModuleSize())
+				vModSize23A := math.Abs(p2.GetEstimatedModuleSize() - p3.GetEstimatedModuleSize())
+				if vModSize23A > DIFF_MODSIZE_CUTOFF && vModSize23 >= DIFF_MODSIZE_CUTOFF_PERCENT {
+					// break, since elements are ordered by the module size deviation there cannot be
+					// any more interesting elements for the given p1.
+					break
+				}
+
+				bl, tl, tr := gozxing.ResultPoint_OrderBestPatterns(p1, p2, p3)
+				test := []*detector.FinderPattern{
+					bl.(*detector.FinderPattern), tl.(*detector.FinderPattern), tr.(*detector.FinderPattern),
+				}
+
+				// Calculate the distances: a = topleft-bottomleft, b=topleft-topright, c = diagonal
+				info := detector.NewFinderPatternInfo(test[0], test[1], test[2])
+				dA := gozxing.ResultPoint_Distance(info.GetTopLeft(), info.GetBottomLeft())
+				dC := gozxing.ResultPoint_Distance(info.GetTopRight(), info.GetBottomLeft())
+				dB := gozxing.ResultPoint_Distance(info.GetTopLeft(), info.GetTopRight())
+
+				// Check the sizes
+				estimatedModuleCount := (dA + dB) / (p1.GetEstimatedModuleSize() * 2.0)
+				if estimatedModuleCount > MAX_MODULE_COUNT_PER_EDGE ||
+					estimatedModuleCount < MIN_MODULE_COUNT_PER_EDGE {
+					continue
+				}
+
+				// Calculate the difference of the edge lengths in percent
+				vABBC := math.Abs((dA - dB) / math.Min(dA, dB))
+				if vABBC >= 0.1 {
+					continue
+				}
+
+				// Calculate the diagonal length by assuming a 90° angle at topleft
+				dCpy := math.Sqrt(dA*dA + dB*dB)
+				// Compare to the real distance in %
+				vPyC := math.Abs((dC - dCpy) / math.Min(dC, dCpy))
+
+				if vPyC >= 0.1 {
+					continue
+				}
+
+				// All tests passed!
+				results = append(results, test)
+			}
+		}
+	}
+
+	if len(results) > 0 {
+		return results, nil
+	}
+
+	// Nothing found!
+	return nil, gozxing.GetNotFoundExceptionInstance()
+}
+
+func (this *MultiFinderPatternFinder) FindMulti(hints map[gozxing.DecodeHintType]interface{}) ([]*detector.FinderPatternInfo, error) {
+	_, tryHarder := hints[gozxing.DecodeHintType_TRY_HARDER]
+	image := this.GetImage()
+	maxI := image.GetHeight()
+	maxJ := image.GetWidth()
+	// We are looking for black/white/black/white/black modules in
+	// 1:1:3:1:1 ratio; this tracks the number of such modules seen so far
+
+	// Let's assume that the maximum version QR Code we support takes up 1/4 the height of the
+	// image, and then account for the center being 3 modules in size. This gives the smallest
+	// number of pixels the center could be, so skip this often. When trying harder, look for all
+	// QR versions regardless of how dense they are.
+	iSkip := (3 * maxI) / (4 * detector.FinderPatternFinder_MAX_MODULES)
+	if iSkip < detector.FinderPatternFinder_MIN_SKIP || tryHarder {
+		iSkip = detector.FinderPatternFinder_MIN_SKIP
+	}
+
+	stateCount := make([]int, 5)
+	for i := iSkip - 1; i < maxI; i += iSkip {
+		// Get a row of black/white values
+		detector.FinderPatternFinder_ClearCounts(stateCount)
+		currentState := 0
+		for j := 0; j < maxJ; j++ {
+			if image.Get(j, i) {
+				// Black pixel
+				if (currentState & 1) == 1 { // Counting white pixels
+					currentState++
+				}
+				stateCount[currentState]++
+			} else { // White pixel
+				if (currentState & 1) == 0 { // Counting black pixels
+					if currentState == 4 { // A winner?
+						if detector.FinderPatternFinder_foundPatternCross(stateCount) &&
+							this.HandlePossibleCenter(stateCount, i, j) { // Yes
+							// Clear state to start looking again
+							currentState = 0
+							detector.FinderPatternFinder_ClearCounts(stateCount)
+						} else { // No, shift counts back by two
+							detector.FinderPatternFinder_ShiftCounts2(stateCount)
+							currentState = 3
+						}
+					} else {
+						currentState++
+						stateCount[currentState]++
+					}
+				} else { // Counting white pixels
+					stateCount[currentState]++
+				}
+			}
+		} // for j=...
+
+		if detector.FinderPatternFinder_foundPatternCross(stateCount) {
+			this.HandlePossibleCenter(stateCount, i, maxJ)
+		}
+	} // for i=iSkip-1 ...
+	patternInfo, e := this.selectMultipleBestPatterns()
+	if e != nil {
+		return nil, e
+	}
+	result := make([]*detector.FinderPatternInfo, 0)
+	for _, pattern := range patternInfo {
+		bl, tl, tr := gozxing.ResultPoint_OrderBestPatterns(pattern[0], pattern[1], pattern[2])
+		result = append(result,
+			detector.NewFinderPatternInfo(
+				bl.(*detector.FinderPattern), tl.(*detector.FinderPattern), tr.(*detector.FinderPattern)))
+	}
+
+	return result, nil
+}
---
+++ multi/qrcode/detector/multi_finder_pattern_finder_test.go
@@ -0,0 +1,217 @@
+package detector
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+	"unsafe"
+
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/qrcode/detector"
+)
+
+func TestModuleSizeComparator(t *testing.T) {
+	patterns := []*detector.FinderPattern{
+		detector.NewFinderPattern1(1, 1, 5),
+		detector.NewFinderPattern1(1, 1, 3),
+		detector.NewFinderPattern1(1, 1, 1),
+		detector.NewFinderPattern1(1, 1, 4),
+		detector.NewFinderPattern1(1, 1, 2),
+	}
+	sort.Slice(patterns, ModuleSizeComparator(patterns))
+
+	expects := []float64{5, 4, 3, 2, 1}
+	for i := 0; i < 5; i++ {
+		if ems := patterns[i].GetEstimatedModuleSize(); ems != expects[i] {
+			t.Fatalf("patterns[%v].GetEstimatedModuleSize = %v, expected %v", i, ems, expects[i])
+		}
+	}
+}
+
+func injectPossibleCenters(finder *MultiFinderPatternFinder, patterns []*detector.FinderPattern) {
+	v := reflect.ValueOf(finder).Elem().FieldByName("possibleCenters")
+	p := (*[]*detector.FinderPattern)(unsafe.Pointer(v.UnsafeAddr()))
+	*p = patterns
+}
+
+func compResultPoint(p gozxing.ResultPoint, x, y float64) bool {
+	return p.GetX() == x && p.GetY() == y
+}
+
+func TestMultiFinderPatternFinder_selectMultipleBestPatterns(t *testing.T) {
+
+	finder := &MultiFinderPatternFinder{&detector.FinderPatternFinder{}}
+	injectPossibleCenters(finder, []*detector.FinderPattern{
+		detector.NewFinderPattern1(0, 0, 1),
+		detector.NewFinderPattern1(0, 0, 2),
+	})
+
+	_, e := finder.selectMultipleBestPatterns()
+	if _, ok := e.(gozxing.NotFoundException); !ok {
+		t.Fatalf("selectMultipleBestPatterns must be NotFoundException, %T", e)
+	}
+
+	ptns := []*detector.FinderPattern{
+		detector.NewFinderPattern1(10, 10, 10),
+		detector.NewFinderPattern1(50, 50, 11),
+		detector.NewFinderPattern1(10, 50, 12),
+	}
+	injectPossibleCenters(finder, ptns)
+
+	r, e := finder.selectMultipleBestPatterns()
+	if e != nil {
+		t.Fatalf("selectMultipleBestPatterns returns error: %v", e)
+	}
+	if !reflect.DeepEqual(r, [][]*detector.FinderPattern{ptns}) {
+		t.Fatalf("bestPatterns = %v, expect %v", r, ptns)
+	}
+
+	injectPossibleCenters(finder, []*detector.FinderPattern{
+		// estimatedModuleSize missmatch.
+		detector.NewFinderPattern1(100, 100, 5.59),
+		detector.NewFinderPattern1(100, 200, 5.58),
+		detector.NewFinderPattern1(200, 100, 15.57),
+		detector.NewFinderPattern1(200, 200, 15.56),
+	})
+	_, e = finder.selectMultipleBestPatterns()
+	if _, ok := e.(gozxing.NotFoundException); !ok {
+		t.Fatalf("selectMultipleBestPatterns must be NotFoundException, %T", e)
+	}
+
+	injectPossibleCenters(finder, []*detector.FinderPattern{
+		detector.NewFinderPattern1(100, 100, 5.59),
+		detector.NewFinderPattern1(100, 130, 5.58),
+		detector.NewFinderPattern1(130, 100, 5.57), // 0-1-2: failed on check the size
+		detector.NewFinderPattern1(100, 200, 5.56), // 0-1-3: failed on difference of the edge lengths
+		detector.NewFinderPattern1(197, 75, 5.55),  // 0-3-4: failed on angle at topleft
+		detector.NewFinderPattern1(200, 100, 5.54), // 0-3-5: OK
+		detector.NewFinderPattern1(170, 10, 5.53),  // 2-4-6: OK
+	})
+	// expected results: [(100,200),(100,100),(200,100)] and [(130,100),(197,75),(170,10)]
+	r, e = finder.selectMultipleBestPatterns()
+	if e != nil {
+		t.Fatalf("selectMultipleBestPatterns returns error: %v", e)
+	}
+	if len(r) != 2 {
+		t.Fatalf("patterns length = %v, expect 2", len(r))
+	}
+	bl1, tl1, tr1 := gozxing.ResultPoint_OrderBestPatterns(r[0][0], r[0][1], r[0][2])
+	bl2, tl2, tr2 := gozxing.ResultPoint_OrderBestPatterns(r[1][0], r[1][1], r[1][2])
+	if tl1.GetX() == 197 {
+		bl1, tl1, tr1, bl2, tl2, tr2 = bl2, tl2, tr2, bl1, tl1, tr1
+	}
+	if p := bl1; !compResultPoint(p, 100, 200) {
+		t.Fatalf("result[0] BottomLeft = (%v,%v), expect (100,200)", p.GetX(), p.GetY())
+	}
+	if p := tl1; !compResultPoint(p, 100, 100) {
+		t.Fatalf("result[0] BottomLeft = (%v,%v), expect (100,100)", p.GetX(), p.GetY())
+	}
+	if p := tr1; !compResultPoint(p, 200, 100) {
+		t.Fatalf("result[0] BottomLeft = (%v,%v), expect (200,100)", p.GetX(), p.GetY())
+	}
+	if p := bl2; !compResultPoint(p, 170, 10) {
+		t.Fatalf("result[0] BottomLeft = (%v,%v), expect (170,10)", p.GetX(), p.GetY())
+	}
+	if p := tl2; !compResultPoint(p, 197, 75) {
+		t.Fatalf("result[0] BottomLeft = (%v,%v), expect (197,75)", p.GetX(), p.GetY())
+	}
+	if p := tr2; !compResultPoint(p, 130, 100) {
+		t.Fatalf("result[0] BottomLeft = (%v,%v), expect (130,100)", p.GetX(), p.GetY())
+	}
+}
+
+func containsPatternInfo(patterns []*detector.FinderPatternInfo, tlx, tly, blx, bly, trx, try float64) bool {
+	for _, p := range patterns {
+		if compResultPoint(p.GetTopLeft(), tlx, tly) &&
+			compResultPoint(p.GetBottomLeft(), blx, bly) &&
+			compResultPoint(p.GetTopRight(), trx, try) {
+			return true
+		}
+	}
+	return false
+}
+
+func TestMultiFinderPatternFinder_FindMulti(t *testing.T) {
+	img, _ := gozxing.NewBitMatrix(100, 100)
+	finder := NewMultiFinderPatternFinder(img, nil)
+	_, e := finder.FindMulti(nil)
+	if _, ok := e.(gozxing.NotFoundException); !ok {
+		t.Fatalf("FindMulti must be NotFoundException, %T", e)
+	}
+
+	img, _ = gozxing.ParseStringToBitMatrix(""+
+		"##############      ##  ##  ##############        ##############      ##  ##  ##############\n"+
+		"##          ##          ##  ##          ##        ##          ##          ##  ##          ##\n"+
+		"##  ######  ##  ##  ##      ##  ######  ##        ##  ######  ##  ##  ##      ##  ######  ##\n"+
+		"##  ######  ##          ##  ##  ######  ##        ##  ######  ##          ##  ##  ######  ##\n"+
+		"##  ######  ##    ##  ####  ##  ######  ##        ##  ######  ##    ##  ####  ##  ######  ##\n"+
+		"##          ##    ######    ##          ##        ##          ##    ######    ##          ##\n"+
+		"##############  ##  ##  ##  ##############        ##############  ##  ##  ##  ##############\n"+
+		"                ##  ##                                            ##  ##                    \n"+
+		"######  ##########  ##  ######      ##            ######  ##########  ##  ######      ##    \n"+
+		"  ##  ##        ########  ##  ##      ####          ##  ##        ########  ##  ##      ####\n"+
+		"##    ####  ##  ########  ######  ########        ##    ####  ##  ########  ######  ########\n"+
+		"    ####  ##  ####    ######  ####    ##              ####  ##  ####    ######  ####    ##  \n"+
+		"        ##  ##    ##  ##  ######                          ##  ##    ##  ##  ######          \n"+
+		"                ##  ##      ####    ######                        ##  ##      ####    ######\n"+
+		"##############  ##  ##  ##      ##  ######        ##############  ##  ##  ##      ##  ######\n"+
+		"##          ##  ######      ######    ####        ##          ##  ######      ######    ####\n"+
+		"##  ######  ##  ####    ##  ##        ####        ##  ######  ##  ####    ##  ##        ####\n"+
+		"##  ######  ##    ######  ##  ##    ####          ##  ######  ##    ######  ##  ##    ####  \n"+
+		"##  ######  ##  ########  ####  ##  ##  ##        ##  ######  ##  ########  ####  ##  ##  ##\n"+
+		"##          ##  ##  ########    ##    ##          ##          ##  ##  ########    ##    ##  \n"+
+		"##############  ########  ######      ####        ##############  ########  ######      ####\n"+
+		"                                                                                            \n"+
+		"                                                                                            \n"+
+		"                                                                                            \n"+
+		"                                                                                            \n"+
+		"##############      ##  ##  ##############        ##############      ##  ##  ##############\n"+
+		"##          ##          ##  ##          ##        ##          ##          ##  ##          ##\n"+
+		"##  ######  ##  ##  ##      ##  ######  ##        ##  ######  ##  ##  ##      ##  ######  ##\n"+
+		"##  ######  ##          ##  ##  ######  ##        ##  ######  ##          ##  ##  ######  ##\n"+
+		"##  ######  ##    ##  ####  ##  ######  ##        ##  ######  ##    ##  ####  ##  ######  ##\n"+
+		"##          ##    ######    ##          ##        ##          ##    ######    ##          ##\n"+
+		"##############  ##  ##  ##  ##############        ##############  ##  ##  ##  ##############\n"+
+		"                ##  ##                                            ##  ##                    \n"+
+		"######  ##########  ##  ######      ##            ######  ##########  ##  ######      ##    \n"+
+		"  ##  ##        ########  ##  ##      ####          ##  ##        ########  ##  ##      ####\n"+
+		"##    ####  ##  ########  ######  ########        ##    ####  ##  ########  ######  ########\n"+
+		"    ####  ##  ####    ######  ####    ##              ####  ##  ####    ######  ####    ##  \n"+
+		"        ##  ##    ##  ##  ######                          ##  ##    ##  ##  ######          \n"+
+		"                ##  ##      ####    ######                        ##  ##      ####    ######\n"+
+		"##############  ##  ##  ##      ##  ######        ##############  ##  ##  ##      ##  ######\n"+
+		"##          ##  ######      ######    ####        ##          ##  ######      ######    ####\n"+
+		"##  ######  ##  ####    ##  ##        ####        ##  ######  ##  ####    ##  ##        ####\n"+
+		"##  ######  ##    ######  ##  ##    ####          ##  ######  ##    ######  ##  ##    ####  \n"+
+		"##  ######  ##  ########  ####  ##  ##  ##        ##  ######  ##  ########  ####  ##  ##  ##\n"+
+		"##          ##  ##  ########    ##    ##          ##          ##  ##  ########    ##    ##  \n"+
+		"##############  ########  ######      ####        ##############  ########  ######      ####\n",
+		"##", "  ")
+
+	finder = NewMultiFinderPatternFinder(img, nil)
+
+	hint := make(map[gozxing.DecodeHintType]interface{})
+	hint[gozxing.DecodeHintType_TRY_HARDER] = true
+
+	r, e := finder.FindMulti(hint)
+	if e != nil {
+		t.Fatalf("FindMulti returns error: %v", e)
+	}
+
+	// top left
+	if !containsPatternInfo(r, 3.5, 3.5, 3.5, 17.5, 17.5, 3.5) {
+		t.Fatalf("result must contains: {(3.5,3.5), (3.5,17.5), (17.5,3.5)}")
+	}
+	// top right
+	if !containsPatternInfo(r, 28.5, 3.5, 28.5, 17.5, 42.5, 3.5) {
+		t.Fatalf("result must contains: {(28.5,3.5), (28.5,17.5), (42.5,3.5)}")
+	}
+	// bottom left
+	if !containsPatternInfo(r, 3.5, 28.5, 3.5, 42.5, 17.5, 28.5) {
+		t.Fatalf("result must contains: {(3.5,28.5), (3.5,42.5), (17.5,28.5)}")
+	}
+	// bottom right
+	if !containsPatternInfo(r, 28.5, 28.5, 28.5, 42.5, 42.5, 28.5) {
+		t.Fatalf("result must contains: {(28.5,28.5), (28.5,42.5), (42.5,28.5)}")
+	}
+}
